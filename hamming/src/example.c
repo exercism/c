89655e56538490dcf1ceb37063aa079b472ba59d
Alan Shen <sunzenshen@gmail.com>
2014-09-25 22:51:43 -0600

    Hamming example solution

---
+++ hamming/src/example.c
@@ -0,0 +1,13 @@
+#include "hamming.h"
+
+int compute(char *lhs, int lhs_len, char *rhs, int rhs_len) {
+    int count = 0;
+    int i = 0;
+    for (i=0; i<lhs_len && i<rhs_len; ++i) {
+        if(lhs[i] != rhs[i]) {
+            ++count;
+        }
+    }
+    return count;
+}
+
