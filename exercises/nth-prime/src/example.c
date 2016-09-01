41cd517bcfc6f0f2c4dbae85b0138734ddefb5a7
Barrett Brown <barrettb@ieee.org>
2016-09-01 16:04:57 -0400

    Add nth-prime exercise

---
+++ exercises/nth-prime/src/example.c
@@ -0,0 +1,32 @@
+#include "nth_prime.h"
+#include <stdbool.h>
+#include <math.h>
+
+static bool is_prime(uint32_t n)
+{
+  for(uint32_t i = (uint32_t)sqrt(n); i > 1; i--) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+uint32_t nth(uint32_t n)
+{
+  uint32_t candidate = 1;
+  uint32_t count = 0;
+
+  if(n < 1) {
+    return 0;
+  }
+
+  while(count < n) {
+    if(is_prime(++candidate)) {
+      count++;
+    }
+  }
+
+  return candidate;
+}
