97b7b07086755fbfd046fac116b8318a09933e48
Ryan Hartlage <ryanplusplus@gmail.com>
2016-09-02 14:03:37 -0400

    Use NULL instead of empty string when no name is provided (#46)
    * Use NULL instead of empty string when no name is provided
    
    * Added missing stddef.h include for NULL
    
    * Added missing include
    
    * Simplified exercise by removing buffer length check; Added stub

--- exercises/hello-world/src/example.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-void hello(char* buffer, unsigned int buffer_length, const char* name){
-    
-    if (name[0] == '\0') name = "World";
-    
-    snprintf(buffer, buffer_length, "Hello, %s!", name);
-}
