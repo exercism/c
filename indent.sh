47c9f02dc89207dd8f79bb53b31420cb1abf26d6
Ryan Hartlage <ryanplusplus@gmail.com>
2016-09-05 10:17:54 -0400

    Added indent script

---
+++ indent.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+for f in $(find exercises/ -not -path '*/vendor/*' -name '*.c' -or -not -path '*/vendor/*' -name '*.h'); do
+  indent -nbad -bap -nbc -bbo -hnl -br -brs -c33 -cd33 -ncdb -ce -ci4 -cli0 -d0 -di1 -nfc1 -i3 -nut -ip0 -l80 -lp -npcs -nprs -npsl -sai -saf -saw -ncs -nsc -sob -nfca -cp33 -ss -il1 $f
+  rm $f~
+done
