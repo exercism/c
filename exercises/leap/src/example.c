e72023e3f5951f473e228395c188103ee7a55acc
Patrick Jackson <PatrickSJackson@gmail.com>
2016-09-12 18:39:24 -0700

    Fixing leap solution.

--- exercises/leap/src/example.c
+++ exercises/leap/src/example.c
@@ -3,14 +3,13 @@
 bool is_leap_year(int year)
 {
    if (year % 4 == 0) {
-
       if (year % 400 == 0) {
          return true;
       }
       if (year % 100 == 0) {
          return false;
       }
-
+      return true;
    }
    return false;
 }
