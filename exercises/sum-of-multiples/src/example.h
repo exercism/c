0f77e7d3a4143f296056eff2996931bf821619be
Barrett Brown <barrettb@ieee.org>
2016-09-23 10:02:14 -0400

    Add sum-of-multiples exercise

---
+++ exercises/sum-of-multiples/src/example.h
@@ -0,0 +1,8 @@
+#ifndef _SUM_OF_MULTIPLES_H
+#define _SUM_OF_MULTIPLES_H
+
+#include <stddef.h>
+
+unsigned int sum_of_multiples_to(const unsigned int *multiples, const size_t number_of_multiples, const int up_to);
+
+#endif
