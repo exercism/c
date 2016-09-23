2f6f990568c5c89fd07ebef624aa020c432ecf60
Ryan Hartlage <ryanplusplus@gmail.com>
2016-09-23 19:46:38 -0400

    Apply correct style

--- exercises/hamming/src/example.c
+++ exercises/hamming/src/example.c
@@ -2,12 +2,12 @@
 #include <string.h>
 #include "hamming.h"
 
-int compute(const char *lhs, const char *rhs)
+size_t compute(const char *lhs, const char *rhs)
 {
    size_t lhs_len = strlen(lhs);
    size_t rhs_len = strlen(rhs);
-   int count = 0;
-   int i = 0;
+   size_t count = 0;
+   size_t i = 0;
    for (i = 0; i < lhs_len && i < rhs_len; ++i) {
       if (lhs[i] != rhs[i]) {
          ++count;
--- exercises/hamming/src/example.h
+++ exercises/hamming/src/example.h
@@ -1,6 +1,6 @@
 #ifndef _HAMMING_H
 #define _HAMMING_H
 
-int compute(const char *lhs, const char *rhs);
+size_t compute(const char *lhs, const char *rhs);
 
 #endif
