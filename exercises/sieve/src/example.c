e852b0b50ff648d31c2a93de4c3463a02863786b
steven_root <Steven.Root@ge.com>
2016-11-18 18:15:38 -0500

    modified to adjust the index more easily and call sqrt only 1 time

--- exercises/sieve/src/example.c
+++ exercises/sieve/src/example.c
@@ -16,27 +16,28 @@ unsigned int sieve(const unsigned int limit, primesArray_t primes)
       numberIsPrime = malloc(limit + 1);
       memset(numberIsPrime, 1, limit + 1);
 
+      unsigned int maxFactor = sqrt(limit) + 1;
+
       // mark 0 and 1 as not prime
       for (unsigned int i = 0; i < 2; i++) {
          numberIsPrime[i] = 0;
       }
 
       // mark the remaining numbers in the array according to the algo.
-      for (unsigned int i = 2; i < sqrt(limit) + 1; i++) {
+      for (unsigned int index = 2; index < maxFactor;) {
          // mark all of multiples that can't be prime
-         for (unsigned int nonPrimeIndex = (2 * i); nonPrimeIndex < (limit + 1);
-              nonPrimeIndex += i) {
+         for (unsigned int nonPrimeIndex = (2 * index);
+              nonPrimeIndex < (limit + 1); nonPrimeIndex += index) {
             numberIsPrime[nonPrimeIndex] = 0;
          }
 
-         // adjust the index i - normally bad practice within a loop but ok here as always making equivalent or larger
-         for (unsigned int newStartIndex = i + 1;
-              newStartIndex < sqrt(limit) + 1; newStartIndex++) {
-            if (numberIsPrime[newStartIndex]) {
-               i = newStartIndex - 1;   // need to account for the ++ of the outer for loop
+         // adjust the index
+         do {
+            index++;
+            if (numberIsPrime[index]) {
                break;
             }
-         }
+         } while (index <= maxFactor);
       }
       // collect and count the primes found
       for (unsigned int i = 1; i < limit + 1; i++) {
