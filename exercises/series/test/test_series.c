40cc954b73134c083c519a837c9815b6f89ede54
steven_root <Steven.Root@ge.com>
2016-11-18 17:48:03 -0500

    changed DERP name

--- exercises/series/test/test_series.c
+++ exercises/series/test/test_series.c
@@ -30,17 +30,17 @@ static void testSolution(SeriesResults_t * expectedSolution,
    testSolution(&expected, actual);\
    }\
 
-#define DERP(...) __VA_ARGS__
+#define ASSEMBLE_STRING_ARRAY(...) __VA_ARGS__
 
 // Test cases
 #define SERIES_TEST_CASES(ENTRY) \
-   ENTRY(test_slices_of_one, "01234", 1, 5, { DERP("0","1","2","3","4") })\
-   ENTRY(test_slices_of_two, "97867564", 2, 7, { DERP("97","78","86","67","75","56", "64") })\
-   ENTRY(test_slices_of_three, "97867564", 3, 6, { DERP("978","786","867","675","756","564") })\
-   ENTRY(test_slices_of_four, "01234", 4, 2, { DERP("0123","1234") })\
-   ENTRY(test_slices_of_five, "01234", 5, 1, { DERP("01234")})\
-   ENTRY(test_overly_long_slice, "012", 4, 0, {DERP("")})\
-   ENTRY(test_overly_short_slice, "01234", 0, 0, {DERP("")})\
+   ENTRY(test_slices_of_one, "01234", 1, 5, { ASSEMBLE_STRING_ARRAY("0","1","2","3","4") })\
+   ENTRY(test_slices_of_two, "97867564", 2, 7, { ASSEMBLE_STRING_ARRAY("97","78","86","67","75","56", "64") })\
+   ENTRY(test_slices_of_three, "97867564", 3, 6, { ASSEMBLE_STRING_ARRAY("978","786","867","675","756","564") })\
+   ENTRY(test_slices_of_four, "01234", 4, 2, { ASSEMBLE_STRING_ARRAY("0123","1234") })\
+   ENTRY(test_slices_of_five, "01234", 5, 1, { ASSEMBLE_STRING_ARRAY("01234")})\
+   ENTRY(test_overly_long_slice, "012", 4, 0, {ASSEMBLE_STRING_ARRAY("")})\
+   ENTRY(test_overly_short_slice, "01234", 0, 0, {ASSEMBLE_STRING_ARRAY("")})\
 
 SERIES_TEST_CASES(EXPAND_SERIES_TEST_CASES_AS_TEST)
 
