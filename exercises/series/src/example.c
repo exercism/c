eaf49ebcf826e3ebadfb9c8655769930e9be0eca
steven_root <Steven.Root@ge.com>
2016-11-29 17:48:12 -0500

    ugh - forgot example

---
+++ exercises/series/src/example.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "series.h"
+
+SeriesResults_t series(char *inputText, unsigned int substringLength)
+{
+   int substringCount = strlen(inputText) - (substringLength - 1);
+   SeriesResults_t results;
+
+   memset(&results, 0, sizeof(results));
+   if ((substringCount > 0) && (substringLength > 0)) {
+      results.substringCount = substringCount;
+      results.substring = malloc(sizeof(char *) * substringCount);
+      for (int index = 0; index < substringCount; index++) {
+         results.substring[index] =
+             malloc(sizeof(char) * (substringLength + 1));
+         sprintf(results.substring[index], "%.*s", substringLength,
+                 &inputText[index]);
+      }
+   }
+   return results;
+}
