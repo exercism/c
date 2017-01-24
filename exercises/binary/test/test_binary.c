975f6a0c6921a63417a5ef0972e02016c200da75
DeathSec <DeathSec@users.noreply.github.com>
2017-01-25 00:44:22 +0530

    update test_binary.c

--- exercises/binary/test/test_binary.c
+++ exercises/binary/test/test_binary.c
@@ -8,68 +8,67 @@ void b1_is_decimal_1(void)
 
 void b10_is_decimal_2(void)
 {
-   int r = (2 == convert("10"));
-   TEST_ASSERT(r);
+   int result = convert("10");
+   TEST_ASSERT_EQUAL(2, result);
 }
 
 void b11_is_decimal_3(void)
 {
-   int r = (3 == convert("11"));
-   TEST_ASSERT(r);
+   int result = convert("11");
+   TEST_ASSERT_EQUAL(3, result);
 }
 
 void b100_is_decimal_4(void)
 {
-   int r = (4 == convert("100"));
-   TEST_ASSERT(r);
+   int result = convert("100");
+   TEST_ASSERT_EQUAL(4, result);
 }
 
 void b101_is_decimal_5(void)
 {
-   int r = (5 == convert("101"));
-   TEST_ASSERT(r);
+   int result = convert("101");
+   TEST_ASSERT_EQUAL(5, result);
 }
 
 void b110_is_decimal_6(void)
 {
-   int r = (6 == convert("110"));
-   TEST_ASSERT(r);
+   int result = convert("110");
+   TEST_ASSERT_EQUAL(6, result);
 }
 
 void b111_is_decimal_7(void)
 {
-   int r = (7 == convert("111"));
-   TEST_ASSERT(r);
+   int result = convert("111");
+   TEST_ASSERT_EQUAL(7, result);
 }
 
 void b1000_is_decimal_8(void)
 {
-   int r = (8 == convert("1000"));
-   TEST_ASSERT(r);
+   int result = convert("1000");
+   TEST_ASSERT_EQUAL(8, result);
 }
 
 void b1001_is_decimal_9(void)
 {
-   int r = (9 == convert("1001"));
-   TEST_ASSERT(r);
+   int result = convert("1001");
+   TEST_ASSERT_EQUAL(9, result);
 }
 
 void b10001101000_is_decimal_1128(void)
 {
-   int r = (1128 == convert("10001101000"));
-   TEST_ASSERT(r);
+   int result = convert("10001101000");
+   TEST_ASSERT_EQUAL(1128, result);
 }
 
 void invalid_entry_is_decimal_0(void)
 {
-   int r = (0 == convert("invalid entry!"));
-   TEST_ASSERT(r);
+   int result = convert("invalid entry");
+   TEST_ASSERT_EQUAL(0, result);
 }
 
 int main(void)
 {
    UnityBegin("test/test_binary.c");
-
    RUN_TEST(b1_is_decimal_1);
    RUN_TEST(b10_is_decimal_2);
    RUN_TEST(b11_is_decimal_3);
