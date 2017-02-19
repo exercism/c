3c1014578e175d04f11e16ec797535cb2765f3a3
Bernard Corentin <corentin.bernard@etudiant.univ-lille1.fr>
2017-02-19 19:00:29 +0100

    changed Test method

--- exercises/space-age/test/test_space_age.c
+++ exercises/space-age/test/test_space_age.c
@@ -4,42 +4,42 @@
 
 void test_convert_earth_year(void)
 {
-   TEST_ASSERT_EQUAL_FLOAT(31.69, convert_earth_age(1000000000));
+   TEST_ASSERT_FLOAT_WITHIN(0.02,31.69, convert_earth_age(1000000000));
 }
 
 void test_convert_mercury_year(void)
 {
-   TEST_ASSERT_EQUAL_FLOAT(280.88, convert_mercury_age(2134835688));
+   TEST_ASSERT_FLOAT_WITHIN(0.02,280.88, convert_mercury_age(2134835688));
 }
 
 void test_convert_venus_year(void)
 {
-   TEST_ASSERT_EQUAL_FLOAT(9.78, convert_venus_age(189839836));
+   TEST_ASSERT_FLOAT_WITHIN(0.02,9.78, convert_venus_age(189839836));
 }
 
 void test_convert_mars_year(void)
 {
-   TEST_ASSERT_EQUAL_FLOAT(39.25, convert_mars_age(2329871239));
+   TEST_ASSERT_FLOAT_WITHIN(0.02,39.25, convert_mars_age(2329871239));
 }
 
 void test_convert_jupiter_year(void)
 {
-   TEST_ASSERT_EQUAL_FLOAT(2.41, convert_jupiter_age(901876382));
+   TEST_ASSERT_FLOAT_WITHIN(0.02,2.41, convert_jupiter_age(901876382));
 }
 
 void test_convert_saturn_year(void)
 {
-   TEST_ASSERT_EQUAL_FLOAT(3.23, convert_saturn_age(3000000000));
+   TEST_ASSERT_FLOAT_WITHIN(0.02,3.23, convert_saturn_age(3000000000));
 }
 
 void test_convert_uranus_year(void)
 {
-   TEST_ASSERT_EQUAL_FLOAT(1.21, convert_uranus_age(3210123456));
+   TEST_ASSERT_FLOAT_WITHIN(0.02,1.21, convert_uranus_age(3210123456));
 }
 
 void test_convert_neptune_year(void)
 {
-   TEST_ASSERT_EQUAL_FLOAT(1.58, convert_neptune_age(8210123456));
+   TEST_ASSERT_FLOAT_WITHIN(0.02,1.58, convert_neptune_age(8210123456));
 }
 
 int main(void)
