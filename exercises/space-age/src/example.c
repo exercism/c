6a5e2857af3e0adf0acc4956a2ca8e2d3bd0d0d8
Bernard Corentin <corentin.bernard@etudiant.univ-lille1.fr>
2017-02-20 12:32:26 +0100

    correcting indentation

--- exercises/space-age/src/example.c
+++ exercises/space-age/src/example.c
@@ -1,6 +1,5 @@
 #include "space_age.h"
 
-
 /* Number of seconds in a Earth year */
 #define SECONDS_ON_EARTH_YEAR 31557600
 
@@ -15,40 +14,40 @@
 
 float convert_earth_age(const long input)
 {
-   return input/SECONDS_ON_EARTH_YEAR;
+   return input / SECONDS_ON_EARTH_YEAR;
 }
 
 float convert_mercury_age(const long input)
 {
-   return convert_earth_age(input)/MERCURY_YEAR;
+   return convert_earth_age(input) / MERCURY_YEAR;
 }
 
 float convert_venus_age(const long input)
 {
-   return convert_earth_age(input)/VENUS_YEAR;
+   return convert_earth_age(input) / VENUS_YEAR;
 }
 
 float convert_mars_age(const long input)
 {
-   return convert_earth_age(input)/MARS_YEAR;
+   return convert_earth_age(input) / MARS_YEAR;
 }
 
 float convert_jupiter_age(const long input)
 {
-   return convert_earth_age(input)/JUPITER_YEAR;
+   return convert_earth_age(input) / JUPITER_YEAR;
 }
 
 float convert_saturn_age(const long input)
 {
-   return convert_earth_age(input)/SATURN_YEAR;
+   return convert_earth_age(input) / SATURN_YEAR;
 }
 
 float convert_uranus_age(const long input)
 {
-   return convert_earth_age(input)/URANUS_YEAR;
+   return convert_earth_age(input) / URANUS_YEAR;
 }
 
 float convert_neptune_age(const long input)
 {
-   return convert_earth_age(input)/NEPTUNE_YEAR;
+   return convert_earth_age(input) / NEPTUNE_YEAR;
 }
--- exercises/space-age/src/example.h
+++ exercises/space-age/src/example.h
@@ -1,7 +1,6 @@
 #ifndef SPACE_AGE_H
 #define SPACE_AGE_H
 
-
 float convert_mercury_age(const long input);
 float convert_venus_age(const long input);
 float convert_earth_age(const long input);
