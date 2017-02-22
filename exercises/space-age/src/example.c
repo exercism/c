4177d88d1e76ff9244e9dd8b09458131b55080e1
Bernard Corentin <corentin.bernard@etudiant.univ-lille1.fr>
2017-02-22 19:58:23 +0100

    correction for simpler calculation at run-time

--- exercises/space-age/src/example.c
+++ exercises/space-age/src/example.c
@@ -4,13 +4,13 @@
 #define SECONDS_ON_EARTH_YEAR 31557600
 
 /* Orbital period of each planet, in Earth years */
-#define MERCURY_YEAR 0.2408467
-#define VENUS_YEAR 0.61519726
-#define MARS_YEAR 1.8808158
-#define JUPITER_YEAR 11.862615
-#define SATURN_YEAR 29.447498
-#define URANUS_YEAR 84.016846
-#define NEPTUNE_YEAR 164.79132
+#define MERCURY_YEAR (0.2408467 * SECONDS_ON_EARTH_YEAR)
+#define VENUS_YEAR (0.61519726 * SECONDS_ON_EARTH_YEAR)
+#define MARS_YEAR (1.8808158 * SECONDS_ON_EARTH_YEAR)
+#define JUPITER_YEAR (11.862615 * SECONDS_ON_EARTH_YEAR)
+#define SATURN_YEAR (29.447498 * SECONDS_ON_EARTH_YEAR)
+#define URANUS_YEAR (84.016846 * SECONDS_ON_EARTH_YEAR)
+#define NEPTUNE_YEAR (164.79132 * SECONDS_ON_EARTH_YEAR)
 
 float convert_earth_age(const long input)
 {
@@ -19,35 +19,35 @@ float convert_earth_age(const long input)
 
 float convert_mercury_age(const long input)
 {
-   return convert_earth_age(input) / MERCURY_YEAR;
+   return input / MERCURY_YEAR;
 }
 
 float convert_venus_age(const long input)
 {
-   return convert_earth_age(input) / VENUS_YEAR;
+   return input / VENUS_YEAR;
 }
 
 float convert_mars_age(const long input)
 {
-   return convert_earth_age(input) / MARS_YEAR;
+   return input / MARS_YEAR;
 }
 
 float convert_jupiter_age(const long input)
 {
-   return convert_earth_age(input) / JUPITER_YEAR;
+   return input / JUPITER_YEAR;
 }
 
 float convert_saturn_age(const long input)
 {
-   return convert_earth_age(input) / SATURN_YEAR;
+   return input / SATURN_YEAR;
 }
 
 float convert_uranus_age(const long input)
 {
-   return convert_earth_age(input) / URANUS_YEAR;
+   return input / URANUS_YEAR;
 }
 
 float convert_neptune_age(const long input)
 {
-   return convert_earth_age(input) / NEPTUNE_YEAR;
+   return input / NEPTUNE_YEAR;
 }
