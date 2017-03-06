37399f8aee4af93f17af31e47c35166e39a9cdf1
Peter Tseng <pht24@cornell.edu>
2017-03-05 19:32:38 -0800

    react: Add to track

---
+++ exercises/react/src/example.c
@@ -0,0 +1,211 @@
+#include <stdlib.h>
+#include "react.h"
+
+enum cell_kind {
+   kind_input,
+   kind_compute1,
+   kind_compute2,
+};
+
+struct child {
+   struct cell *cell;
+   struct child *next;
+};
+
+struct cb {
+   callback_id id;
+   callback f;
+   void *obj;
+   struct cb *next;
+};
+
+struct reactor {
+   struct child *first;
+   struct child *last;
+};
+
+struct cell {
+   struct reactor *reactor;
+   int value;
+   enum cell_kind kind;
+   struct cell *input1;
+   struct cell *input2;
+   compute1 compute1;
+   compute2 compute2;
+   struct cb *cb;
+   int callbacks_issued;
+};
+
+struct reactor *create_reactor()
+{
+   struct reactor *r = calloc(1, sizeof(struct reactor));
+   return r;
+}
+
+void free_reactor(struct reactor *r)
+{
+   struct child *child = r->first;
+   while (child) {
+      struct cb *cb = child->cell->cb;
+      while (cb) {
+         struct cb *next_cb = cb->next;
+         free(cb);
+         cb = next_cb;
+      }
+      free(child->cell);
+
+      struct child *next = child->next;
+      free(child);
+      child = next;
+   }
+   free(r);
+}
+
+#define SUCCESS 1
+#define FAIL 0
+
+static int add_child(struct reactor *r, struct cell *cell)
+{
+   struct child *child = calloc(1, sizeof(struct child));
+   if (!child) {
+      return FAIL;
+   }
+   child->cell = cell;
+   if (!r->first) {
+      r->first = child;
+   } else {
+      r->last->next = child;
+   }
+   r->last = child;
+   return SUCCESS;
+}
+
+struct cell *create_input_cell(struct reactor *r, int initial_value)
+{
+   struct cell *c = calloc(1, sizeof(struct cell));
+   if (!c) {
+      return NULL;
+   }
+   if (add_child(r, c) != SUCCESS) {
+      free(c);
+      return NULL;
+   }
+   c->reactor = r;
+   c->kind = kind_input;
+   c->value = initial_value;
+   return c;
+}
+
+struct cell *create_compute1_cell(struct reactor *r, struct cell *input,
+                                  compute1 compute)
+{
+   struct cell *c = calloc(1, sizeof(struct cell));
+   if (!c) {
+      return NULL;
+   }
+   if (add_child(r, c) != SUCCESS) {
+      free(c);
+      return NULL;
+   }
+   c->reactor = r;
+   c->kind = kind_compute1;
+   c->input1 = input;
+   c->compute1 = compute;
+   c->value = compute(cell_value(input));
+   return c;
+}
+
+struct cell *create_compute2_cell(struct reactor *r, struct cell *input1,
+                                  struct cell *input2, compute2 compute)
+{
+   struct cell *c = calloc(1, sizeof(struct cell));
+   if (!c) {
+      return NULL;
+   }
+   if (add_child(r, c) != SUCCESS) {
+      free(c);
+      return NULL;
+   }
+   c->reactor = r;
+   c->kind = kind_compute2;
+   c->input1 = input1;
+   c->input2 = input2;
+   c->compute2 = compute;
+   c->value = compute(cell_value(input1), cell_value(input2));
+   return c;
+}
+
+int cell_value(struct cell *c)
+{
+   return c->value;
+}
+
+static void propagate(struct cell *c)
+{
+   int new_value;
+   switch (c->kind) {
+   case kind_compute1:
+      new_value = c->compute1(cell_value(c->input1));
+      break;
+   case kind_compute2:
+      new_value = c->compute2(cell_value(c->input1), cell_value(c->input2));
+      break;
+   default:
+      return;
+   }
+
+   if (new_value != c->value) {
+      c->value = new_value;
+      for (struct cb * cb = c->cb; cb; cb = cb->next) {
+         cb->f(cb->obj, new_value);
+      }
+   }
+
+}
+
+void cell_value_set(struct cell *c, int new_value)
+{
+   c->value = new_value;
+   struct reactor *r = c->reactor;
+
+   // We take the very naive route of updating all cells.
+   // Traversing the tree and only updating the cells dependent on the just-changed cell is possible,
+   // but requires much more memory management to make each cell aware of its dependents.
+   for (struct child * child = r->first; child; child = child->next) {
+      propagate(child->cell);
+   }
+}
+
+callback_id add_callback(struct cell *c, void *obj, callback f)
+{
+   struct cb *cb = calloc(1, sizeof(struct cb));
+   if (!cb) {
+      return -1;
+   }
+   cb->id = c->callbacks_issued++;
+   cb->next = c->cb;
+   cb->obj = obj;
+   cb->f = f;
+   c->cb = cb;
+   return cb->id;
+}
+
+void remove_callback(struct cell *c, callback_id to_remove)
+{
+   if (!c->cb) {
+      return;
+   } else if (c->cb->id == to_remove) {
+      struct cb *to_free = c->cb;
+      c->cb = c->cb->next;
+      free(to_free);
+      return;
+   }
+   for (struct cb * prev = c->cb, *cb = c->cb->next; cb;
+        prev = prev->next, cb = cb->next) {
+      if (cb->id == to_remove) {
+         prev->next = cb->next;
+         free(cb);
+         return;
+      }
+   }
+}
---
+++ exercises/react/test/test_react.c
@@ -0,0 +1,289 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "vendor/unity.h"
+#include "../src/react.h"
+
+void test_input_cells_have_value(void)
+{
+   struct reactor *r = create_reactor();
+   struct cell *input = create_input_cell(r, 2);
+
+   TEST_ASSERT_EQUAL_INT(2, cell_value(input));
+
+   free_reactor(r);
+}
+
+static int plus1(int x)
+{
+   return x + 1;
+}
+
+void test_compute_cells_calculate_initial_value(void)
+{
+   struct reactor *r = create_reactor();
+   struct cell *input = create_input_cell(r, 1);
+   struct cell *output = create_compute1_cell(r, input, plus1);
+
+   TEST_ASSERT_EQUAL_INT(2, cell_value(output));
+
+   free_reactor(r);
+}
+
+static int concat_digits(int a, int b)
+{
+   return b * 10 + a;
+}
+
+void test_compute_cells_take_inputs_in_the_right_order(void)
+{
+   struct reactor *r = create_reactor();
+   struct cell *one = create_input_cell(r, 1);
+   struct cell *two = create_input_cell(r, 2);
+   struct cell *output = create_compute2_cell(r, one, two, concat_digits);
+
+   TEST_ASSERT_EQUAL_INT(21, cell_value(output));
+
+   free_reactor(r);
+}
+
+void test_compute_cells_update_value_when_dependencies_are_changed(void)
+{
+   struct reactor *r = create_reactor();
+   struct cell *input = create_input_cell(r, 1);
+   struct cell *output = create_compute1_cell(r, input, plus1);
+
+   cell_value_set(input, 3);
+   TEST_ASSERT_EQUAL_INT(4, cell_value(output));
+
+   free_reactor(r);
+}
+
+static int times2(int x)
+{
+   return x * 2;
+}
+
+static int times30(int x)
+{
+   return x * 30;
+}
+
+static int plus(int x, int y)
+{
+   return x + y;
+}
+
+void test_compute_cells_can_depend_on_other_compute_cells(void)
+{
+   struct reactor *r = create_reactor();
+   struct cell *input = create_input_cell(r, 1);
+   struct cell *times_two = create_compute1_cell(r, input, times2);
+   struct cell *times_thirty = create_compute1_cell(r, input, times30);
+   struct cell *output = create_compute2_cell(r, times_two, times_thirty, plus);
+
+   TEST_ASSERT_EQUAL_INT(32, cell_value(output));
+   cell_value_set(input, 3);
+   TEST_ASSERT_EQUAL_INT(96, cell_value(output));
+
+   free_reactor(r);
+}
+
+struct cbinfo {
+   int last_value;
+   int times_called;
+};
+
+static void cb_record_last_value(void *obj, int v)
+{
+   struct cbinfo *cbinfo = (struct cbinfo *)obj;
+   cbinfo->last_value = v;
+   ++cbinfo->times_called;
+}
+
+void test_compute_cells_fire_callbacks(void)
+{
+   struct reactor *r = create_reactor();
+   struct cell *input = create_input_cell(r, 1);
+   struct cell *output = create_compute1_cell(r, input, plus1);
+
+   struct cbinfo cbinfo = { -1, 0 };
+   add_callback(output, &cbinfo, cb_record_last_value);
+
+   cell_value_set(input, 3);
+   TEST_ASSERT_EQUAL_INT(1, cbinfo.times_called);
+   TEST_ASSERT_EQUAL_INT(4, cbinfo.last_value);
+
+   free_reactor(r);
+}
+
+static void cb_noop(void *obj __attribute__ ((unused)), int v
+                    __attribute__ ((unused)))
+{
+}
+
+void test_compute_cells_dont_access_callback_obj(void)
+{
+   struct reactor *r = create_reactor();
+   struct cell *input = create_input_cell(r, 1);
+   struct cell *output = create_compute1_cell(r, input, plus1);
+
+   add_callback(output, NULL, cb_noop);
+
+   cell_value_set(input, 3);
+
+   free_reactor(r);
+}
+
+static int big_if_three(int x)
+{
+   return x < 3 ? 111 : 222;
+}
+
+void test_callbacks_only_fire_on_change(void)
+{
+   struct reactor *r = create_reactor();
+   struct cell *input = create_input_cell(r, 1);
+   struct cell *output = create_compute1_cell(r, input, big_if_three);
+
+   struct cbinfo cbinfo = { -1, 0 };
+   add_callback(output, &cbinfo, cb_record_last_value);
+
+   cell_value_set(input, 2);
+   TEST_ASSERT_EQUAL_INT(0, cbinfo.times_called);
+
+   cell_value_set(input, 4);
+   TEST_ASSERT_EQUAL_INT(1, cbinfo.times_called);
+   TEST_ASSERT_EQUAL_INT(222, cbinfo.last_value);
+
+   free_reactor(r);
+}
+
+void test_callbacks_can_be_added_and_removed(void)
+{
+   struct reactor *r = create_reactor();
+   struct cell *input = create_input_cell(r, 11);
+   struct cell *output = create_compute1_cell(r, input, plus1);
+
+   struct cbinfo cbinfo1 = { -1, 0 };
+   callback_id cb1 = add_callback(output, &cbinfo1, cb_record_last_value);
+   struct cbinfo cbinfo2 = { -1, 0 };
+   add_callback(output, &cbinfo2, cb_record_last_value);
+
+   cell_value_set(input, 31);
+
+   remove_callback(output, cb1);
+   struct cbinfo cbinfo3 = { -1, 0 };
+   add_callback(output, &cbinfo3, cb_record_last_value);
+
+   cell_value_set(input, 41);
+
+   TEST_ASSERT_EQUAL_INT(1, cbinfo1.times_called);
+   TEST_ASSERT_EQUAL_INT(32, cbinfo1.last_value);
+   TEST_ASSERT_EQUAL_INT(2, cbinfo2.times_called);
+   TEST_ASSERT_EQUAL_INT(42, cbinfo2.last_value);
+   TEST_ASSERT_EQUAL_INT(1, cbinfo3.times_called);
+   TEST_ASSERT_EQUAL_INT(42, cbinfo3.last_value);
+
+   free_reactor(r);
+}
+
+void test_removing_a_callback_multiple_times(void)
+{
+   struct reactor *r = create_reactor();
+   struct cell *input = create_input_cell(r, 11);
+   struct cell *output = create_compute1_cell(r, input, plus1);
+
+   struct cbinfo cbinfo1 = { -1, 0 };
+   callback_id cb1 = add_callback(output, &cbinfo1, cb_record_last_value);
+   struct cbinfo cbinfo2 = { -1, 0 };
+   add_callback(output, &cbinfo2, cb_record_last_value);
+   for (int i = 0; i < 10; ++i) {
+      remove_callback(output, cb1);
+   }
+
+   cell_value_set(input, 2);
+
+   TEST_ASSERT_EQUAL_INT(0, cbinfo1.times_called);
+   TEST_ASSERT_EQUAL_INT(1, cbinfo2.times_called);
+   TEST_ASSERT_EQUAL_INT(3, cbinfo2.last_value);
+
+   free_reactor(r);
+}
+
+static int minus1(int x)
+{
+   return x - 1;
+}
+
+static int times(int x, int y)
+{
+   return x * y;
+}
+
+void test_callbacks_only_called_once_even_if_multiple_inputs_change(void)
+{
+   struct reactor *r = create_reactor();
+   struct cell *input = create_input_cell(r, 1);
+   struct cell *plus_one = create_compute1_cell(r, input, plus1);
+   struct cell *minus_one1 = create_compute1_cell(r, input, minus1);
+   struct cell *minus_one2 = create_compute1_cell(r, minus_one1, minus1);
+   struct cell *output = create_compute2_cell(r, plus_one, minus_one2, times);
+
+   struct cbinfo cbinfo = { -1, 0 };
+   add_callback(output, &cbinfo, cb_record_last_value);
+
+   cell_value_set(input, 4);
+
+   TEST_ASSERT_EQUAL_INT(1, cbinfo.times_called);
+   TEST_ASSERT_EQUAL_INT(10, cbinfo.last_value);
+
+   free_reactor(r);
+}
+
+static int minus(int x, int y)
+{
+   return x - y;
+}
+
+void test_callbacks_not_called_if_inputs_change_but_output_doesnt(void)
+{
+   struct reactor *r = create_reactor();
+   struct cell *input = create_input_cell(r, 1);
+   struct cell *plus_one = create_compute1_cell(r, input, plus1);
+   struct cell *minus_one = create_compute1_cell(r, input, minus1);
+   struct cell *always_two =
+       create_compute2_cell(r, plus_one, minus_one, minus);
+
+   struct cbinfo cbinfo = { -1, 0 };
+   add_callback(always_two, &cbinfo, cb_record_last_value);
+
+   for (int i = 0; i < 10; ++i) {
+      cell_value_set(input, i);
+   }
+
+   TEST_ASSERT_EQUAL_INT(0, cbinfo.times_called);
+
+   free_reactor(r);
+}
+
+int main(void)
+{
+   UnityBegin("test/test_react.c");
+
+   RUN_TEST(test_input_cells_have_value);
+   RUN_TEST(test_compute_cells_calculate_initial_value);
+   RUN_TEST(test_compute_cells_take_inputs_in_the_right_order);
+   RUN_TEST(test_compute_cells_update_value_when_dependencies_are_changed);
+   RUN_TEST(test_compute_cells_can_depend_on_other_compute_cells);
+   RUN_TEST(test_compute_cells_fire_callbacks);
+   RUN_TEST(test_compute_cells_dont_access_callback_obj);
+   RUN_TEST(test_callbacks_only_fire_on_change);
+   RUN_TEST(test_callbacks_can_be_added_and_removed);
+   RUN_TEST(test_removing_a_callback_multiple_times);
+   RUN_TEST(test_callbacks_only_called_once_even_if_multiple_inputs_change);
+   RUN_TEST(test_callbacks_not_called_if_inputs_change_but_output_doesnt);
+
+   UnityEnd();
+
+   return 0;
+}
