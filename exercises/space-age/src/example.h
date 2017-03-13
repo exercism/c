c32aaa333dd24c1cd34adbe26a784913200f12e8
Corentin Bernard <corentin.bernard59@gmail.com>
2017-03-13 14:40:24 +0100

    Improved code for space age (#125)
    * Improved code for space age
    
    * Correct space age (#1)
    
    change define for enum
    
    * removed switch

--- exercises/space-age/src/example.h
+++ exercises/space-age/src/example.h
@@ -1,13 +1,17 @@
 #ifndef SPACE_AGE_H
 #define SPACE_AGE_H
 
-float convert_mercury_age(const long input);
-float convert_venus_age(const long input);
-float convert_earth_age(const long input);
-float convert_mars_age(const long input);
-float convert_jupiter_age(const long input);
-float convert_saturn_age(const long input);
-float convert_uranus_age(const long input);
-float convert_neptune_age(const long input);
+typedef enum planets {
+   mercury,
+   venus,
+   earth,
+   mars,
+   jupiter,
+   saturn,
+   uranus,
+   neptune,
+} Planets;
+
+float convert_planet_age(Planets planet, const long input);
 
 #endif
