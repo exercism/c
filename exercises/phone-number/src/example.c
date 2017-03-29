8aa80edf66165e917b689ae8abdee544a39a393a
toby <wolf99@users.noreply.github.com>
2017-03-29 01:27:23 +0100

    Add missing tests and adjust example to suit changes (#137)
    Add test for lengths greater than 11
    
    Fix example to not treat all longer numbers as 11 length
    
    Add 'cleans numbers with multiple spaces' test
    
    Addmissing tests, adjust example to suit
    
    Run indent
    
    Run indent. Again

--- exercises/phone-number/src/example.c
+++ exercises/phone-number/src/example.c
@@ -14,6 +14,7 @@
 #define EXTENSION_OFFSET (AREA_CODE_LENGTH + EXCHANGE_LENGTH)
 
 #define INVALID_NUMBER_RESULT    "0000000000"
+#define VALID_NON_DIGIT_CHARS    " .-()"
 
 static void remove_leading_digit(char *phone_number)
 {
@@ -25,14 +26,17 @@ static void remove_leading_digit(char *phone_number)
 char *phone_number_clean(const char *input)
 {
    char *output = calloc(VALID_NUMBER_LENGTH + 2, sizeof(char));
-
    size_t j = 0;
+
    for (size_t i = 0; i < strlen(input); i++) {
       if (isdigit(input[i])) {
-         if (j > VALID_NUMBER_LENGTH) {
+         if (j > VALID_NUMBER_LENGTH + 1) {
             break;
          }
          output[j++] = input[i];
+      } else if (strchr(VALID_NON_DIGIT_CHARS, input[i]) == NULL) {
+         strcpy(output, INVALID_NUMBER_RESULT);
+         return output;
       }
    }
 
@@ -45,7 +49,6 @@ char *phone_number_clean(const char *input)
          strcpy(output, INVALID_NUMBER_RESULT);
       }
    }
-
    return output;
 }
 
@@ -53,7 +56,6 @@ char *phone_number_get_area_code(const char *input)
 {
    char *output = phone_number_clean(input);
    output[3] = '\0';
-
    return output;
 }
 
@@ -64,8 +66,6 @@ char *phone_number_format(const char *input)
 
    sprintf(output, "(%.3s) %.3s-%.4s", cleaned_input,
            &cleaned_input[AREA_CODE_LENGTH], &cleaned_input[EXTENSION_OFFSET]);
-
    free(cleaned_input);
-
    return output;
 }
--- exercises/phone-number/test/test_phone_number.c
+++ exercises/phone-number/test/test_phone_number.c
@@ -27,6 +27,22 @@ void test_cleans_numbers_with_dots(void)
    test_phone_number_clean(input, expected);
 }
 
+void test_cleans_numbers_with_multiple_spaces(void)
+{
+   const char input[] = "123 456   7890  ";
+   const char expected[] = "1234567890";
+
+   test_phone_number_clean(input, expected);
+}
+
+void test_invalid_when_9_digits(void)
+{
+   const char input[] = "123456789";
+   const char expected[] = "0000000000";
+
+   test_phone_number_clean(input, expected);
+}
+
 void test_valid_when_11_digits_and_first_digit_is_1(void)
 {
    const char input[] = "11234567890";
@@ -35,7 +51,7 @@ void test_valid_when_11_digits_and_first_digit_is_1(void)
    test_phone_number_clean(input, expected);
 }
 
-void test_invalid_when_11_digits(void)
+void test_invalid_when_11_digits_and_first_digit_not_1(void)
 {
    const char input[] = "21234567890";
    const char expected[] = "0000000000";
@@ -43,9 +59,33 @@ void test_invalid_when_11_digits(void)
    test_phone_number_clean(input, expected);
 }
 
-void test_invalid_when_9_digits(void)
+void test_invalid_when_more_than_11_digits(void)
 {
-   const char input[] = "123456789";
+   const char input[] = "121234567890";
+   const char expected[] = "0000000000";
+
+   test_phone_number_clean(input, expected);
+}
+
+void test_invalid_with_letters(void)
+{
+   const char input[] = "123-abc-7890";
+   const char expected[] = "0000000000";
+
+   test_phone_number_clean(input, expected);
+}
+
+void test_invalid_with_punctuations(void)
+{
+   const char input[] = "123-@:!-7890";
+   const char expected[] = "0000000000";
+
+   test_phone_number_clean(input, expected);
+}
+
+void test_invalid_with_right_number_of_digits_but_letters_mixed_in(void)
+{
+   const char input[] = "1a2b3c4d5e6f7g8h9i0j";
    const char expected[] = "0000000000";
 
    test_phone_number_clean(input, expected);
@@ -96,9 +136,14 @@ int main(void)
 
    RUN_TEST(test_cleans_parens_dashes_and_spaces_from_the_number);
    RUN_TEST(test_cleans_numbers_with_dots);
-   RUN_TEST(test_valid_when_11_digits_and_first_digit_is_1);
-   RUN_TEST(test_invalid_when_11_digits);
+   RUN_TEST(test_cleans_numbers_with_multiple_spaces);
    RUN_TEST(test_invalid_when_9_digits);
+   RUN_TEST(test_valid_when_11_digits_and_first_digit_is_1);
+   RUN_TEST(test_invalid_when_11_digits_and_first_digit_not_1);
+   RUN_TEST(test_invalid_when_more_than_11_digits);
+   RUN_TEST(test_invalid_with_letters);
+   RUN_TEST(test_invalid_with_punctuations);
+   RUN_TEST(test_invalid_with_right_number_of_digits_but_letters_mixed_in);
    RUN_TEST(test_returns_area_code);
    RUN_TEST(test_formats_a_number);
    RUN_TEST(test_cleans_number_before_formatting);
