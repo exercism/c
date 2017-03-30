0a0c2a1e3875cb8f65a70022a3ebc8f44e66cb0d
DeathSec <DeathSec@users.noreply.github.com>
2017-03-30 05:44:50 +0530

    Adding the new pascals triangle (#135)
    * Adding the new pascals triangle
    
    * Little indent in config.json
    
    * Update test_pascals_triangle.c
    
    * Following the Codebase...
    
    * Avoiding NULL Test and changing input to int type instead of char*
    
    * Removing unwanted headers!

---
+++ exercises/pascals-triangle/src/example.h
@@ -0,0 +1,8 @@
+#ifndef PASCALS_TRIANGLE_H
+#define PASCALS_TRIANGLE_H
+#include <stdlib.h>
+
+void free_triangle(size_t **, size_t);
+size_t **create_triangle(int);
+
+#endif
---
+++ exercises/pascals-triangle/test/test_pascals_triangle.c
@@ -0,0 +1,87 @@
+#include <stdbool.h>
+#include "../src/pascals_triangle.h"
+#include "vendor/unity.h"
+
+static bool check(size_t count, size_t expected[][count], size_t ** result)
+{
+   size_t i, j;
+   for (i = 0; i < count; i++) {
+      for (j = 0; j < count; j++) {
+         if (expected[i][j] != result[i][j]) {
+            return 0;
+         }
+      }
+   }
+   return 1;
+}
+
+void test_no_rows(void)
+{
+   size_t expected[1][1] = { {0} };
+   size_t **r = create_triangle(0);
+   TEST_ASSERT_TRUE(check(1, expected, r));
+   free_triangle(r, 1);
+}
+
+void test_single_row(void)
+{
+   size_t expected[1][1] = {
+      {1}
+   };
+   size_t **r = create_triangle(1);
+   TEST_ASSERT_TRUE(check(1, expected, r));
+   free_triangle(r, 1);
+}
+
+void test_two_rows(void)
+{
+   size_t expected[2][2] = {
+      {1, 0},
+      {1, 1}
+   };
+   size_t **r = create_triangle(2);
+   TEST_ASSERT_TRUE(check(2, expected, r));
+   free_triangle(r, 2);
+}
+
+void test_three_rows(void)
+{
+   size_t expected[3][3] = {
+      {1, 0, 0},
+      {1, 1, 0},
+      {1, 2, 1}
+   };
+   size_t **r = create_triangle(3);
+   TEST_ASSERT_TRUE(check(3, expected, r));
+   free_triangle(r, 3);
+}
+
+void test_four_rows(void)
+{
+   size_t expected[4][4] = {
+      {1, 0, 0, 0},
+      {1, 1, 0, 0},
+      {1, 2, 1, 0},
+      {1, 3, 3, 1}
+   };
+   size_t **r = create_triangle(4);
+   TEST_ASSERT_TRUE(check(4, expected, r));
+   free_triangle(r, 4);
+}
+
+void test_negative_rows(void)
+{
+   TEST_ASSERT_TRUE((create_triangle(-1) == NULL));
+}
+
+int main(void)
+{
+   UnityBegin("test/test_pascals_triangle.c");
+   RUN_TEST(test_no_rows);
+   RUN_TEST(test_single_row);
+   RUN_TEST(test_two_rows);
+   RUN_TEST(test_three_rows);
+   RUN_TEST(test_four_rows);
+   RUN_TEST(test_negative_rows);
+   UnityEnd();
+}
