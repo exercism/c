baf69863edb4d613a20432143f0b7184d01d6e9d
Mike Finch <mikefinch87@gmail.com>
2017-09-16 22:35:59 -0400

    Added message output to indent.sh

--- indent.sh
+++ indent.sh
@@ -6,10 +6,13 @@
 INDENT=indent
 
 if [ -x "$(command -v gindent)" ]; then
-  INDENT=gindent
+   INDENT=gindent
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+   echo "***********************************************\n*  On OSX install gnu-indent using home brew  *\n***********************************************"
 fi
 
+
 for f in $(find exercises/ -not -path '*/vendor/*' -name '*.c' -or -not -path '*/vendor/*' -name '*.h'); do
-  $INDENT -nbad -bap -nbc -bbo -hnl -br -brs -c33 -cd33 -ncdb -ce -ci4 -cli0 -d0 -di1 -nfc1 -i3 -nut -ip0 -l80 -lp -npcs -nprs -npsl -sai -saf -saw -ncs -nsc -sob -nfca -cp33 -ss -il1 $f
-  rm $f~ 2> /dev/null
+$INDENT -nbad -bap -nbc -bbo -hnl -br -brs -c33 -cd33 -ncdb -ce -ci4 -cli0 -d0 -di1 -nfc1 -i3 -nut -ip0 -l80 -lp -npcs -nprs -npsl -sai -saf -saw -ncs -nsc -sob -nfca -cp33 -ss -il1 $f
+rm $f~ 2> /dev/null
 done
