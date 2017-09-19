4900f60050fa21df1a519b0cf0f5231e76d237e3
Mike Finch <mikefinch87@gmail.com>
2017-09-18 21:11:13 -0400

    Removed strlen from within loop on hamming example.c

--- exercises/hamming/src/example.c
+++ exercises/hamming/src/example.c
@@ -7,7 +7,8 @@ size_t compute(const char *lhs, const char *rhs)
    size_t count = -1;
    if (lhs && rhs && (strlen(lhs) == strlen(rhs))) {
       count = 0;
-      for (size_t i = 0; i < strlen(lhs); ++i) {
+      size_t length = strlen(lhs);
+      for (size_t i = 0; i < length; ++i) {
          if (lhs[i] != rhs[i]) {
             ++count;
          }
