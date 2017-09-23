16d3dae1b7584f3a1cc3b76410e24a8e8e89acee
Ryan Hartlage <ryanplusplus@gmail.com>
2017-09-23 19:16:24 -0400

    Simplify Hello World and sync it with canonical JSON

--- exercises/hello-world/src/hello_world.h
+++ exercises/hello-world/src/hello_world.h
@@ -1,6 +1,6 @@
 #ifndef HELLO_WORLD_H
 #define HELLO_WORLD_H
 
-void hello(char *buffer, const char *name);
+const char *hello(void);
 
 #endif
--- exercises/hello-world/test/test_hello_world.c
+++ exercises/hello-world/test/test_hello_world.c
@@ -2,35 +2,16 @@
 #include "vendor/unity.h"
 #include "../src/hello_world.h"
 
-#define BUFFER_LENGTH 128
-
-char buffer[BUFFER_LENGTH];
-
-void test_hello_no_name(void)
+void test_hello(void)
 {
-   hello(buffer, NULL);
-   TEST_ASSERT_EQUAL_STRING("Hello, World!", buffer);
-}
-
-void test_hello_alice(void)
-{
-   hello(buffer, "Alice");
-   TEST_ASSERT_EQUAL_STRING("Hello, Alice!", buffer);
-}
-
-void test_hello_bob(void)
-{
-   hello(buffer, "Bob");
-   TEST_ASSERT_EQUAL_STRING("Hello, Bob!", buffer);
+   TEST_ASSERT_EQUAL_STRING("Hello, World!", hello());
 }
 
 int main(void)
 {
    UnityBegin("test/test_hello_world.c");
 
-   RUN_TEST(test_hello_no_name);
-   RUN_TEST(test_hello_alice);
-   RUN_TEST(test_hello_bob);
+   RUN_TEST(test_hello);
 
    UnityEnd();
 
