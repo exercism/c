908df7e3b52bbba64191d93413153ab83e887930
toby <wolf99@users.noreply.github.com>
2017-10-04 23:19:29 +0100

    Update snake case for allergen exercise (#189)

--- exercises/allergies/src/allergies.h
+++ exercises/allergies/src/allergies.h
@@ -2,20 +2,20 @@
 #define ALLERGIES_H
 
 typedef enum {
-   Allergen_Eggs = 0,
-   Allergen_Peanuts,
-   Allergen_Shellfish,
-   Allergen_Strawberries,
-   Allergen_Tomatoes,
-   Allergen_Chocolate,
-   Allergen_Pollen,
-   Allergen_Cats,
-   Allergen_Count
-} Allergen_t;
+   allergen_eggs = 0,
+   allergen_peanuts,
+   allergen_shellfish,
+   allergen_strawberries,
+   allergen_tomatoes,
+   allergen_chocolate,
+   allergen_pollen,
+   allergen_cats,
+   allergen_count
+} allergen_t;
 
 typedef struct {
    int count;
-   Allergen_t *allergens;
-} Allergen_List_t;
+   allergen_t *allergens;
+} allergen_list_t;
 
 #endif
