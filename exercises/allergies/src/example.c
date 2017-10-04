2bfd09791b2ffb7b0113fed62662f2fd4e852460
toby <wolf99@users.noreply.github.com>
2017-10-04 12:36:29 +0100

    Update snake case for allergen exercise

--- exercises/allergies/src/example.c
+++ exercises/allergies/src/example.c
@@ -12,17 +12,17 @@ static const unsigned int scores[] = {
    128
 };
 
-bool is_allergic_to(Allergen_t allergen, unsigned int score)
+bool is_allergic_to(allergen_t allergen, unsigned int score)
 {
    return ((score & scores[allergen]) == scores[allergen]);
 }
 
-void get_allergens(unsigned int score, Allergen_List_t * list)
+void get_allergens(unsigned int score, allergen_list_t * list)
 {
-   list->allergens = calloc(Allergen_Count, sizeof(Allergen_t));
+   list->allergens = calloc(allergen_count, sizeof(allergen_t));
    list->count = 0;
 
-   for (Allergen_t allergen = 0; allergen < Allergen_Count; allergen++) {
+   for (allergen_t allergen = 0; allergen < allergen_count; allergen++) {
       if (is_allergic_to(allergen, score)) {
          score -= scores[allergen];
          list->allergens[list->count] = allergen;
