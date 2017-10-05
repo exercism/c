5a13aa8f4e24640fe75d6473273d9e5ff7426a9a
toby <wolf99@users.noreply.github.com>
2017-10-06 00:27:31 +0100

    Update exercise to use snake case (#192)

--- exercises/isogram/test/test_isogram.c
+++ exercises/isogram/test/test_isogram.c
@@ -4,47 +4,47 @@
 
 void test_empty_string(void)
 {
-   TEST_ASSERT_TRUE(isIsogram(""));
+   TEST_ASSERT_TRUE(is_isogram(""));
 }
 
 void test_lower_case_only(void)
 {
-   TEST_ASSERT_TRUE(isIsogram("isogram"));
+   TEST_ASSERT_TRUE(is_isogram("isogram"));
 }
 
 void test_duplicated_letter(void)
 {
-   TEST_ASSERT_FALSE(isIsogram("eleven"));
+   TEST_ASSERT_FALSE(is_isogram("eleven"));
 }
 
 void test_longest_known_isogram(void)
 {
-   TEST_ASSERT_TRUE(isIsogram("subdermatoglyphic"));
+   TEST_ASSERT_TRUE(is_isogram("subdermatoglyphic"));
 }
 
 void test_duplicated_letter_mixed_case(void)
 {
-   TEST_ASSERT_FALSE(isIsogram("Alphabet"));
+   TEST_ASSERT_FALSE(is_isogram("Alphabet"));
 }
 
 void test_non_letter_char(void)
 {
-   TEST_ASSERT_TRUE(isIsogram("thumbscrew-japingly"));
+   TEST_ASSERT_TRUE(is_isogram("thumbscrew-japingly"));
 }
 
 void test_duplicated_non_letter_char(void)
 {
-   TEST_ASSERT_TRUE(isIsogram("Hjelmqvist-Gryb-Zock-Pfund-Wax"));
+   TEST_ASSERT_TRUE(is_isogram("Hjelmqvist-Gryb-Zock-Pfund-Wax"));
 }
 
 void test_multiple_whitespace(void)
 {
-   TEST_ASSERT_TRUE(isIsogram("Emily Jung Schwartzkopf"));
+   TEST_ASSERT_TRUE(is_isogram("Emily Jung Schwartzkopf"));
 }
 
 void test_duplicated_letter_within_word(void)
 {
-   TEST_ASSERT_FALSE(isIsogram("accentor"));
+   TEST_ASSERT_FALSE(is_isogram("accentor"));
 }
 
 int main(void)
