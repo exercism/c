d6dbe3e13fe1a191989d8a45a5bc6a79f054c66d
toby <wolf99@users.noreply.github.com>
2017-10-12 10:25:51 +0100

    Update palindrome product exercise to snake case
    Also took the chance to make the type names more relevant to it's actual contents and add new lines between test cases and `UnityBegin()` & `UnityEnd()`

--- exercises/palindrome-products/src/example.h
+++ exercises/palindrome-products/src/example.h
@@ -2,11 +2,11 @@
 #define PALINDROME_PRODUCTS_H
 
 typedef struct {
-   int small_palind;
-   int larg_palind;
-} Pair;
+   int smallest;
+   int largest;
+} product_t;
 
-/* Return the smallest and biggest palindrome in the range of min and max */
-Pair getPalindromeProduct(int min, int max);
+/* Return the smallest and largest palindrome products in the range of min and max */
+product_t getPalindromeProduct(int min, int max);
 
 #endif
