ae368a0e8bf7e5e3bee1f39e12ffc10831a9b10a
toby <wolf99@users.noreply.github.com>
2017-10-13 00:23:48 +0100

    Update nucleotide count to snake case (#196)

--- exercises/nucleotide-count/src/example.c
+++ exercises/nucleotide-count/src/example.c
@@ -8,26 +8,26 @@ char *count(const char *dna_strand)
 {
    bool invalid_char = false;
    size_t index;
-   size_t nucleotide_A_count = 0;
-   size_t nucleotide_C_count = 0;
-   size_t nucleotide_G_count = 0;
-   size_t nucleotide_T_count = 0;
+   size_t nucleotide_a_count = 0;
+   size_t nucleotide_c_count = 0;
+   size_t nucleotide_g_count = 0;
+   size_t nucleotide_t_count = 0;
    char *count_results = calloc(1, 50);
 
    for (index = 0; (index < strlen(dna_strand)) && (invalid_char == false);
         index++) {
       switch (dna_strand[index]) {
       case 'A':
-         nucleotide_A_count++;
+         nucleotide_a_count++;
          break;
       case 'C':
-         nucleotide_C_count++;
+         nucleotide_c_count++;
          break;
       case 'G':
-         nucleotide_G_count++;
+         nucleotide_g_count++;
          break;
       case 'T':
-         nucleotide_T_count++;
+         nucleotide_t_count++;
          break;
       default:
          invalid_char = true;
@@ -36,8 +36,8 @@ char *count(const char *dna_strand)
    }
 
    if (!invalid_char) {
-      sprintf(count_results, "A:%zd C:%zd G:%zd T:%zd", nucleotide_A_count,
-              nucleotide_C_count, nucleotide_G_count, nucleotide_T_count);
+      sprintf(count_results, "A:%zd C:%zd G:%zd T:%zd", nucleotide_a_count,
+              nucleotide_c_count, nucleotide_g_count, nucleotide_t_count);
    }
    return count_results;
 }
