43505c275043540af966a7b727721b8f7b9a3166
Mike Finch <Gamecock@users.noreply.github.com>
2017-10-15 12:43:44 -0400

    Additional test cases for Sum of Multiples (#206)

--- exercises/sum-of-multiples/src/example.c
+++ exercises/sum-of-multiples/src/example.c
@@ -6,10 +6,12 @@ unsigned int sum_of_multiples(const unsigned int *multiples,
                               const unsigned int upper_bound)
 {
    unsigned int sum = 0;
+   if (NULL == multiples)
+      return sum;
 
    for (unsigned int current = 1; current < upper_bound; current++) {
       for (size_t i = 0; i < number_of_multiples; i++) {
-         if ((current % multiples[i]) == 0) {
+         if (0 != multiples[i] && (current % multiples[i]) == 0) {
             sum += current;
             break;
          }
