41978f89edb48e44bbc2837fffa2dc51804fb3a3
toby <wolf99@users.noreply.github.com>
2017-10-16 12:21:19 +0100

    Update beer song exercise to snake case

--- exercises/beer-song/src/example.c
+++ exercises/beer-song/src/example.c
@@ -2,51 +2,51 @@
 #include <stdio.h>
 #include <string.h>
 
-static unsigned int getVerse(char *buffer, unsigned int verseNumber)
+static unsigned int get_verse(char *buffer, unsigned int verse_number)
 {
-   unsigned int bytesWritten = 0;
+   unsigned int bytes_written = 0;
 
-   if (verseNumber == 0) {
-      bytesWritten =
+   if (verse_number == 0) {
+      bytes_written =
           sprintf(buffer,
                   "No more bottles of beer on the wall, no more bottles of beer.\n"
                   "Go to the store and buy some more, 99 bottles of beer on the wall.\n");
-   } else if (verseNumber == 1) {
-      bytesWritten =
+   } else if (verse_number == 1) {
+      bytes_written =
           sprintf(buffer,
                   "1 bottle of beer on the wall, 1 bottle of beer.\n"
                   "Take it down and pass it around, no more bottles of beer on the wall.\n");
-   } else if (verseNumber == 2) {
-      bytesWritten =
+   } else if (verse_number == 2) {
+      bytes_written =
           sprintf(buffer,
                   "2 bottles of beer on the wall, 2 bottles of beer.\n"
                   "Take one down and pass it around, 1 bottle of beer on the wall.\n");
-   } else if (verseNumber <= 99) {
-      bytesWritten =
+   } else if (verse_number <= 99) {
+      bytes_written =
           sprintf(buffer,
                   "%d bottles of beer on the wall, %d bottles of beer.\n"
                   "Take one down and pass it around, %d bottles of beer on the wall.\n",
-                  verseNumber, verseNumber, verseNumber - 1);
+                  verse_number, verse_number, verse_number - 1);
    }
 
-   return bytesWritten;
+   return bytes_written;
 }
 
-void verse(char *buffer, unsigned int verseNumber)
+void verse(char *buffer, unsigned int verse_number)
 {
-   (void)getVerse(buffer, verseNumber);
+   (void)get_verse(buffer, verse_number);
 }
 
-void sing(char *buffer, unsigned int startingVerseNumber,
-          unsigned int endingVerseNumber)
+void sing(char *buffer, unsigned int starting_verse_number,
+          unsigned int ending_verse_number)
 {
-   char *currentPosition = buffer;
+   char *current_position = buffer;
 
-   for (unsigned int i = startingVerseNumber + 1; i > endingVerseNumber; i--) {
-      currentPosition += getVerse(currentPosition, i - 1);
-      strcpy(currentPosition, "\n");
-      currentPosition++;
+   for (unsigned int i = starting_verse_number + 1; i > ending_verse_number; i--) {
+      current_position += get_verse(current_position, i - 1);
+      strcpy(current_position, "\n");
+      current_position++;
    }
 
-   strcpy(currentPosition - 1, "\0");
+   strcpy(current_position - 1, "\0");
 }
--- exercises/beer-song/src/example.h
+++ exercises/beer-song/src/example.h
@@ -1,8 +1,8 @@
 #ifndef BEER_SONG_H
 #define BEER_SONG_H
 
-void verse(char *buffer, unsigned int verseNumber);
-void sing(char *buffer, unsigned int startingVerseNumber,
-          unsigned int endingVerseNumber);
+void verse(char *buffer, unsigned int verse_number);
+void sing(char *buffer, unsigned int starting_verse_number,
+          unsigned int ending_verse_number);
 
 #endif
