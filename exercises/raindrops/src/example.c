19d0347148f11f14e89a2d959de4b50572abcafe
Julian Minde <mindejulian@me.com>
2017-10-18 20:54:47 +0200

    Fixed indents and removed unused defs

--- exercises/raindrops/src/example.c
+++ exercises/raindrops/src/example.c
@@ -2,28 +2,24 @@
 #include "stdio.h"
 #include "string.h"
 
-#define PLING_FACTOR 3
-#define PLANG_FACTOR 5
-#define PLONG_FACTOR 7
-
 char *convert(int drops)
 {
-   static char result[100];
-   memset(result,'\0',100);
-  
-   if(drops % 3 == 0) {
+   static char     result[100];
+   memset(result, '\0', 100);
+
+   if (drops % 3 == 0) {
       strcat(result, "Pling");
    }
-   if(drops % 5 == 0) {
+   if (drops % 5 == 0) {
       strcat(result, "Plang");
    }
-   if(drops % 7 == 0) {
+   if (drops % 7 == 0) {
       strcat(result, "Plong");
    }
-   if(strlen(result) == 0) {
+   if (strlen(result) == 0) {
       char drops_string[12] = "";
       sprintf(drops_string, "%d", drops);
       strcat(result, drops_string);
    }
-   return(result);
+   return (result);
 }
