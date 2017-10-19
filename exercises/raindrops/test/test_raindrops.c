109fcd3dafb737b9e20e9a82ad0a469324ffe446
Julian Minde <mindejulian@me.com>
2017-10-19 11:19:01 +0200

    Uncommented TEST_IGNORE

--- exercises/raindrops/test/test_raindrops.c
+++ exercises/raindrops/test/test_raindrops.c
@@ -26,91 +26,91 @@ void test_one_yields_itself(void)
 
 void test_three_yields_pling(void)
 {
-   //TEST_IGNORE();               // delete this line to run test
+   TEST_IGNORE();               // delete this line to run test
    convert_drops(3, "Pling");
 }
 
 void test_five_yields_plang(void)
 {
-   //TEST_IGNORE();
+   TEST_IGNORE();
    convert_drops(5, "Plang");
 }
 
 void test_seven_yields_plong(void)
 {
-   //TEST_IGNORE();
+   TEST_IGNORE();
    convert_drops(7, "Plong");
 }
 
 void test_six_yields_pling(void)
 {
-   //TEST_IGNORE();
+   TEST_IGNORE();
    convert_drops(6, "Pling");
 }
 
 void test_nine_yields_pling(void)
 {
-   //TEST_IGNORE();
+   TEST_IGNORE();
    convert_drops(9, "Pling");
 }
 
 void test_ten_yields_plang(void)
 {
-   //TEST_IGNORE();
+   TEST_IGNORE();
    convert_drops(10, "Plang");
 }
 
 void test_fourteen_yields_plong(void)
 {
-   //TEST_IGNORE();
+   TEST_IGNORE();
    convert_drops(14, "Plong");
 }
 
 void test_fifteen_yields_plingplang(void)
 {
-   //TEST_IGNORE();
+   TEST_IGNORE();
    convert_drops(15, "PlingPlang");
 }
 
 void test_twenty_one_yields_plingplong(void)
 {
-   //TEST_IGNORE();
+   TEST_IGNORE();
    convert_drops(21, "PlingPlong");
 }
 
 void test_twenty_five_yields_plang(void)
 {
-   //TEST_IGNORE();
+   TEST_IGNORE();
    convert_drops(25, "Plang");
 }
 
 void test_thirty_five_yields_plangplong(void)
 {
-   //TEST_IGNORE();
+   TEST_IGNORE();
    convert_drops(35, "PlangPlong");
 }
 
 void test_forty_nine_yields_plong(void)
 {
-   //TEST_IGNORE();
+   TEST_IGNORE();
    convert_drops(49, "Plong");
 }
 
 void test_fifty_two_yields_itself(void)
 {
-   //TEST_IGNORE();
+   TEST_IGNORE();
    convert_drops(52, "52");
 }
 
 void test_one_hundred_five_yields_plingplangplong(void)
 {
-   //TEST_IGNORE();
+   TEST_IGNORE();
    convert_drops(105, "PlingPlangPlong");
 }
 
 void test_big_prime_yields_itself(void)
 {
-   //TEST_IGNORE();
+   TEST_IGNORE();
    convert_drops(12121, "12121");
 }
 
