3de49400b6b64404446d08824fecf8da3f7c0e72
Barrett Brown <barrettb@ieee.org>
2017-10-27 12:47:51 -0400

    Run indent script

--- exercises/queen-attack/src/example.c
+++ exercises/queen-attack/src/example.c
@@ -5,17 +5,19 @@
 
 attack_status_t can_attack(position_t white_queen, position_t black_queen)
 {
-   if ((white_queen.column == black_queen.column && white_queen.row == black_queen.row) ||
-       white_queen.row >= ROW_COUNT ||
-       white_queen.column >= COLUMN_COUNT ||
-       black_queen.row >= ROW_COUNT || black_queen.column >= COLUMN_COUNT) {
+   if ((white_queen.column == black_queen.column
+        && white_queen.row == black_queen.row) || white_queen.row >= ROW_COUNT
+       || white_queen.column >= COLUMN_COUNT || black_queen.row >= ROW_COUNT
+       || black_queen.column >= COLUMN_COUNT) {
       return INVALID_POSITION;
    }
 
    if (white_queen.column == black_queen.column ||
        white_queen.row == black_queen.row ||
-       (white_queen.row - black_queen.row == white_queen.column - black_queen.column) ||
-       (white_queen.row - black_queen.row == black_queen.column - white_queen.column)) {
+       (white_queen.row - black_queen.row ==
+        white_queen.column - black_queen.column)
+       || (white_queen.row - black_queen.row ==
+           black_queen.column - white_queen.column)) {
       return CAN_ATTACK;
    }
 
