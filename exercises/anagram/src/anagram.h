15e07ea9fc4efcfbd7d8d79d82a0f315c5919b17
Mike Finch <mikefinch87@gmail.com>
2017-10-29 20:16:48 -0400

    Modified anagram to not require any memory management

--- exercises/anagram/src/anagram.h
+++ exercises/anagram/src/anagram.h
@@ -1,13 +1,33 @@
 #ifndef ANAGRAM_H
 #define ANAGRAM_H
 
-#define MAX_STR_LEN 20
-
-struct vector {
-   char (*vec)[MAX_STR_LEN];
-   int size;
+enum anagramStatus {
+   UNCHECKED = -1,
+   NOT_ANAGRAM,
+   IS_ANAGRAM
 };
 
-struct vector anagrams_for(char *, struct vector);
+struct candidate {
+   enum anagramStatus isAnagram;
+   char *candidate;
+};
+
+struct candidates {
+   struct candidate *candidate;
+   int size;
+};
+/**
+ * @description - determines if any of the words in candidate are anagrams
+ *                for word.  Word buffer and candidate structures may be modified.
+ *
+ * @param - word, buffer containing a word used to look for an anagram in  candidates.
+ *          may be modified before function returns.
+ *
+ * @param - candidates, list of potential anagrams as an array of struct candidate.
+ *          Words in candidate structure may be modifies before function returns.
+ *          candidate.isAnagram will be set to IS_ANAGRAM if the candidate is an
+ *          anagram for word.
+ */
+void anagrams_for(char word[], struct candidates *candidates);
 
 #endif
--- exercises/anagram/src/example.c
+++ exercises/anagram/src/example.c
@@ -5,18 +5,13 @@
 
 #include "anagram.h"
 
-char *lower_case(char *p)
+void to_lower_case(char p[])
 {
-   int len = strlen(p) + 1;
-   char *lower = (char *)malloc(len);
-   memcpy(lower, p, len);
-   p = lower;
-   while (*p) {
-      *p = tolower(*p);
-      p++;
+   char *tmp = p;
+   while (*tmp) {
+      *tmp = tolower(*tmp);
+      tmp++;
    }
-
-   return lower;
 }
 
 int compare(const void *a, const void *b)
@@ -24,37 +19,25 @@ int compare(const void *a, const void *b)
    return *(const char *)a - *(const char *)b;
 }
 
-struct vector anagrams_for(char *in, struct vector vin)
+void anagrams_for(char word[], struct candidates *candidates)
 {
-   struct vector vout = {
-      malloc(MAX_STR_LEN * sizeof(char *)),
-      0
-   };
-   char (*vout_vecp)[MAX_STR_LEN] = vout.vec;
-
-   char *lower = lower_case(in);
-   char *sorted = malloc(strlen(in) + 1);
-   memcpy(sorted, lower, strlen(in) + 1);
-   qsort(sorted, strlen(sorted), 1, compare);
-
-   char (*vecp)[MAX_STR_LEN] = vin.vec;
-   int x;
-   for (x = 0; x < vin.size; x++) {
-      char *input_lower = lower_case((char *)vecp);
-      if (strcmp(input_lower, lower) != 0) {
-         char *input_sorted = input_lower;
-         qsort(input_sorted, strlen(input_sorted), 1, compare);
-         if (strcmp(sorted, input_sorted) == 0) {
-            strcpy(*vout_vecp, *vecp);
-            vout_vecp++;
-            vout.size++;
+   to_lower_case(word);
+   for (int x = 0; x < candidates->size; x++) {
+      to_lower_case(candidates->candidate[x].candidate);
+      if (strcmp(word, candidates->candidate[x].candidate) == 0) {
+         candidates->candidate[x].isAnagram = NOT_ANAGRAM;
+      }
+   }
+   qsort(word, strlen(word), 1, compare);
+   for (int x = 0; x < candidates->size; x++) {
+      if (candidates->candidate[x].isAnagram == UNCHECKED) {
+         qsort(candidates->candidate[x].candidate,
+               strlen(candidates->candidate[x].candidate), 1, compare);
+         if (strcmp(word, candidates->candidate[x].candidate) == 0) {
+            candidates->candidate[x].isAnagram = IS_ANAGRAM;
+         } else {
+            candidates->candidate[x].isAnagram = NOT_ANAGRAM;
          }
       }
-      free(input_lower);
-      vecp++;
    }
-
-   free(lower);
-   free(sorted);
-   return vout;
 }
