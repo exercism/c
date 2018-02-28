3e6f59f740f641dea81cd290ad79ee5d948e7bc8
Vladimir <jelezarov.vladimir@gmail.com>
2018-02-28 06:53:19 +0100

    improve readability

--- exercises/pig-latin/src/example.c
+++ exercises/pig-latin/src/example.c
@@ -5,14 +5,19 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-static bool is_vowel(const char a)
+static bool is_vowel(const char letter)
 {
-   return a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u';
+   return letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o'
+       || letter == 'u';
 }
 
-static bool starts_with_vowel(const char a, const char b)
+static bool starts_with_vowel_sound(const char *input)
 {
-   return is_vowel(a) || (a == 'x' && b == 'r') || (a == 'y' && b == 't');
+   const char first_letter = tolower(input[0]);
+   const char sec_letter = tolower(input[1]);
+
+   return is_vowel(first_letter) || (first_letter == 'x' && sec_letter == 'r')
+       || (first_letter == 'y' && sec_letter == 't');
 }
 
 static void count_words_and_chars(const char *input, size_t * words,
@@ -30,7 +35,7 @@ static void count_words_and_chars(const char *input, size_t * words,
    }
 }
 
-static void mstrncpy(char *dst, const char *src, size_t n)
+static void strncpy_tolower(char *dst, const char *src, size_t n)
 {
    while ((n--) && (*dst++ = tolower(*src++))) ;
 }
@@ -41,39 +46,43 @@ static void append_ay(char *res)
    *(res + 1) = 'y';
 }
 
+static bool starts_with(const char *input, const char *start_seq)
+{
+   while (*start_seq)
+      if (tolower(*input++) != *start_seq++)
+         return false;
+
+   return true;
+}
+
 static void process_pig_logic(char *dst, const char *src, size_t word_len)
 {
    append_ay(dst + word_len);
 
-   const char a = tolower(*src);
-   const char b = a ? tolower(*(src + 1)) : 0;
-
-   if (starts_with_vowel(a, b)) {
-      mstrncpy(dst, src, word_len);
+   if (starts_with_vowel_sound(src)) {
+      strncpy_tolower(dst, src, word_len);
       return;
    }
 
    size_t i = 0;
    while (src[i]) {
-      const char a = tolower(src[i]);
-      const char b = tolower(src[i + 1]);
-
-      if (is_vowel(a))
+      if (is_vowel(tolower(src[i])))
          break;
 
-      if (b == 'y') {
+      if (starts_with(&src[i + 1], "y")) {
          i++;
          break;
       }
 
-      if (a == 'q' && b == 'u') {
+      if (starts_with(&src[i], "qu")) {
          i += 2;
          break;
       }
       i++;
    }
-   mstrncpy(dst, src + i, word_len - i);
-   mstrncpy(dst + (word_len - i), src, i);
+
+   strncpy_tolower(dst, src + i, word_len - i);
+   strncpy_tolower(dst + (word_len - i), src, i);
 }
 
 char *translate(const char *phrase)
@@ -83,7 +92,6 @@ char *translate(const char *phrase)
 
    size_t words = 0, chars = 0;
    count_words_and_chars(phrase, &words, &chars);
-
    if (chars == 0)
       return NULL;
 
@@ -101,7 +109,6 @@ char *translate(const char *phrase)
 
    char *tmp = res;
    while (words--) {
-
       /* just copy non-alphas */
       while (*phrase && !isalpha(*phrase))
          *tmp++ = *phrase++;
