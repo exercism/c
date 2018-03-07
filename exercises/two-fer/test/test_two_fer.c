b26234fb336233f71c5898a978d06fe76b54e9fe
Vladimir <jelezarov.vladimir@gmail.com>
2018-03-07 10:44:35 +0100

    Implement exercise 'two-fer'

---
+++ exercises/two-fer/test/test_two_fer.c
@@ -0,0 +1,51 @@
+#include "vendor/unity.h"
+#include "../src/two_fer.h"
+#include <stddef.h>
+
+#define BUFFER_SIZE (100)
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_two_fer_no_name_given(void)
+{
+   char response[BUFFER_SIZE];
+   const char *expected = "One for you, one for me.";
+   two_fer(response, NULL);
+   TEST_ASSERT_EQUAL_STRING(expected, response);
+}
+
+void test_two_fer_a_name_given(void)
+{
+   TEST_IGNORE();               // delete this line to run test
+   char response[BUFFER_SIZE];
+   const char *expected = "One for Alice, one for me.";
+   two_fer(response, "Alice");
+   TEST_ASSERT_EQUAL_STRING(expected, response);
+}
+
+void test_two_fer_another_name_given(void)
+{
+   TEST_IGNORE();
+   char response[BUFFER_SIZE];
+   const char *expected = "One for Bob, one for me.";
+   two_fer(response, "Bob");
+   TEST_ASSERT_EQUAL_STRING(expected, response);
+}
+
+int main(void)
+{
+   UnityBegin("test/test_two_fer.c");
+
+   RUN_TEST(test_two_fer_no_name_given);
+   RUN_TEST(test_two_fer_a_name_given);
+   RUN_TEST(test_two_fer_another_name_given);
+
+   UnityEnd();
+   return 0;
+}
