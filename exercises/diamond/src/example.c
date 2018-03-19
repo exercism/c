db994caaec4c27dd3a495245cb4e40791e70731a
Vladimir Zhelezarov <jelezarov.vladimir@gmail.com>
2018-03-19 13:13:22 +0100

    Implement exercise 'diamond' (#285)
    * Implement exercise 'diamond'
    
    * fix function names in the tests

---
+++ exercises/diamond/src/example.c
@@ -0,0 +1,47 @@
+#include "diamond.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+char **make_diamond(const char letter)
+{
+   if (!isalpha(letter))
+      return NULL;
+
+   const char letter_upper = toupper(letter);
+   const size_t row_count = 2 * (letter_upper - 'A' + 1) - 1;
+   const size_t col_count = row_count + 1;
+
+   char **diamond = malloc(sizeof(char *) * row_count);
+   if (diamond == NULL) {
+      fprintf(stderr, "Memory error!\n");
+      return NULL;
+   }
+
+   char *all_rows = malloc(row_count * col_count);
+   if (all_rows == NULL) {
+      fprintf(stderr, "Memory error!\n");
+      free(diamond);
+      return NULL;
+   }
+
+   memset(all_rows, ' ', row_count * col_count);
+
+   const size_t middle = row_count / 2;
+   char curr_char = 'A';
+   int advance = 1;
+   for (size_t row = 0; row < row_count; row++) {
+      diamond[row] = &all_rows[row * col_count];
+      diamond[row][col_count - 1] = '\0';
+
+      diamond[row][middle + (curr_char - 'A')] = curr_char;
+      diamond[row][middle - (curr_char - 'A')] = curr_char;
+
+      if (curr_char == letter_upper)
+         advance = -1;
+      curr_char += advance;
+   }
+
+   return diamond;
+}
---
+++ exercises/diamond/test/test_diamond.c
@@ -0,0 +1,158 @@
+#include "vendor/unity.h"
+#include "../src/diamond.h"
+#include <stdlib.h>
+
+#define ARRAY_SIZE(arr) (sizeof(arr)/sizeof(arr[0]))
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+static void free_all(char **diamond)
+{
+   free(diamond[0]);
+   free(diamond);
+}
+
+void test_rows_degenerate_case_with_a_single_a_row(void)
+{
+   const char letter = 'A';
+   const char *expected[] = {
+      "A"
+   };
+   char **diamond = make_diamond(letter);
+   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, diamond, ARRAY_SIZE(expected));
+   free_all(diamond);
+}
+
+void
+test_rows_degenerate_case_with_no_row_with_3_distinct_groups_of_spaces(void)
+{
+   TEST_IGNORE();               // delete this line to run test
+   const char letter = 'B';
+   const char *expected[] = {
+      " A ",
+      "B B",
+      " A "
+   };
+   char **diamond = make_diamond(letter);
+   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, diamond, ARRAY_SIZE(expected));
+   free_all(diamond);
+}
+
+void test_rows_smallest_non_degenerate_case_with_odd_diamond_side_length(void)
+{
+   TEST_IGNORE();
+   const char letter = 'C';
+   const char *expected[] = {
+      "  A  ",
+      " B B ",
+      "C   C",
+      " B B ",
+      "  A  "
+   };
+   char **diamond = make_diamond(letter);
+   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, diamond, ARRAY_SIZE(expected));
+   free_all(diamond);
+}
+
+void test_rows_smallest_non_degenerate_case_with_even_diamond_side_length(void)
+{
+   TEST_IGNORE();
+   const char letter = 'D';
+   const char *expected[] = {
+      "   A   ",
+      "  B B  ",
+      " C   C ",
+      "D     D",
+      " C   C ",
+      "  B B  ",
+      "   A   "
+   };
+   char **diamond = make_diamond(letter);
+   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, diamond, ARRAY_SIZE(expected));
+   free_all(diamond);
+}
+
+void test_rows_largest_possible_diamond(void)
+{
+   TEST_IGNORE();
+   const char letter = 'Z';
+   const char *expected[] = {
+      "                         A                         ",
+      "                        B B                        ",
+      "                       C   C                       ",
+      "                      D     D                      ",
+      "                     E       E                     ",
+      "                    F         F                    ",
+      "                   G           G                   ",
+      "                  H             H                  ",
+      "                 I               I                 ",
+      "                J                 J                ",
+      "               K                   K               ",
+      "              L                     L              ",
+      "             M                       M             ",
+      "            N                         N            ",
+      "           O                           O           ",
+      "          P                             P          ",
+      "         Q                               Q         ",
+      "        R                                 R        ",
+      "       S                                   S       ",
+      "      T                                     T      ",
+      "     U                                       U     ",
+      "    V                                         V    ",
+      "   W                                           W   ",
+      "  X                                             X  ",
+      " Y                                               Y ",
+      "Z                                                 Z",
+      " Y                                               Y ",
+      "  X                                             X  ",
+      "   W                                           W   ",
+      "    V                                         V    ",
+      "     U                                       U     ",
+      "      T                                     T      ",
+      "       S                                   S       ",
+      "        R                                 R        ",
+      "         Q                               Q         ",
+      "          P                             P          ",
+      "           O                           O           ",
+      "            N                         N            ",
+      "             M                       M             ",
+      "              L                     L              ",
+      "               K                   K               ",
+      "                J                 J                ",
+      "                 I               I                 ",
+      "                  H             H                  ",
+      "                   G           G                   ",
+      "                    F         F                    ",
+      "                     E       E                     ",
+      "                      D     D                      ",
+      "                       C   C                       ",
+      "                        B B                        ",
+      "                         A                         "
+   };
+   char **diamond = make_diamond(letter);
+   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, diamond, ARRAY_SIZE(expected));
+   free_all(diamond);
+}
+
+int main(void)
+{
+   UnityBegin("test/test_diamond.c");
+
+   RUN_TEST(test_rows_degenerate_case_with_a_single_a_row);
+   RUN_TEST
+       (test_rows_degenerate_case_with_no_row_with_3_distinct_groups_of_spaces);
+   RUN_TEST
+       (test_rows_smallest_non_degenerate_case_with_odd_diamond_side_length);
+   RUN_TEST
+       (test_rows_smallest_non_degenerate_case_with_even_diamond_side_length);
+   RUN_TEST(test_rows_largest_possible_diamond);
+
+   UnityEnd();
+   return 0;
+}
