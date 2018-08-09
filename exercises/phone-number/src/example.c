e0de4450fb1e2a9ea632fbec54586f0c28f4c153
Mike Walker <mhwalker@gmail.com>
2018-08-08 18:18:17 -0700

    update test cases to match specifications

--- exercises/phone-number/src/example.c
+++ exercises/phone-number/src/example.c
@@ -27,19 +27,23 @@ char *phone_number_clean(const char *input)
 {
    char *output = calloc(VALID_NUMBER_LENGTH + 2, sizeof(char));
    size_t j = 0;
-
+   
    for (size_t i = 0; i < strlen(input); i++) {
-      if (isdigit(input[i])) {
+    
+      if (isdigit(input[i]) || (i == 0 && input[0] == '+')) {
+      
          if (j > VALID_NUMBER_LENGTH + 1) {
             break;
          }
-         output[j++] = input[i];
+         if (isdigit(input[i]))
+            output[j++] = input[i];
+         
       } else if (strchr(VALID_NON_DIGIT_CHARS, input[i]) == NULL) {
          strcpy(output, INVALID_NUMBER_RESULT);
          return output;
       }
    }
-
+   
    if (j > 11 || j < 10) {
       strcpy(output, INVALID_NUMBER_RESULT);
    } else if (j == 11) {
@@ -49,23 +53,10 @@ char *phone_number_clean(const char *input)
          strcpy(output, INVALID_NUMBER_RESULT);
       }
    }
-   return output;
-}
-
-char *phone_number_get_area_code(const char *input)
-{
-   char *output = phone_number_clean(input);
-   output[3] = '\0';
-   return output;
-}
-
-char *phone_number_format(const char *input)
-{
-   char *cleaned_input = phone_number_clean(input);
-   char *output = calloc(FORMATTED_LENGTH + 1, sizeof(char));
-
-   sprintf(output, "(%.3s) %.3s-%.4s", cleaned_input,
-           &cleaned_input[AREA_CODE_LENGTH], &cleaned_input[EXTENSION_OFFSET]);
-   free(cleaned_input);
+   
+   if (strcmp(output, INVALID_NUMBER_RESULT) != 0)
+      if (output[0] < '2' || output[3] < '2')
+         strcpy(output, INVALID_NUMBER_RESULT);
+   
    return output;
 }
