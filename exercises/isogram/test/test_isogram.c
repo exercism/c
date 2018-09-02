636d136aedefa91a4f72a62d075b02b98b90ca56
Frosh_VII <35707740+FroshVII@users.noreply.github.com>
2018-09-02 11:16:08 -0500

    Extend C Isogram Test Suite
    - Added test for NULL input
    - Added test for duplicated letters on the ends of character arrays
    - Added test for terminating as soon as a duplicate is found

--- exercises/isogram/test/test_isogram.c
+++ exercises/isogram/test/test_isogram.c
@@ -15,6 +15,11 @@ void test_empty_string(void)
    TEST_ASSERT_TRUE(is_isogram(""));
 }
 
+void test_null(void)
+{
+   TEST_ASSERT_TRUE(is_isogram(NULL));
+}
+
 void test_lower_case_only(void)
 {
    TEST_IGNORE();               // delete this line to run test
@@ -37,6 +42,11 @@ void test_longest_known_isogram(void)
 {
    TEST_IGNORE();
    TEST_ASSERT_TRUE(is_isogram("subdermatoglyphic"));
+}
+
+void test_duplicated_on_ends(void)
+{
+   TEST_ASSERT_FALSE(is_isogram("oreo"));
 }
 
 void test_duplicated_letter_mixed_case(void)
@@ -69,13 +79,37 @@ void test_duplicated_letter_within_word(void)
    TEST_ASSERT_FALSE(is_isogram("accentor"));
 }
 
+void test_quit_early(void)
+{
+   clock_t a, b;
+   double small, big;
+   const char small_word[] = "aa";
+   const char big_word[10000] = { 'a' };
+   
+   a = clock();
+   is_isogram(small_word);
+   b = clock();
+   
+   small = (double)(b - a);
+   
+   a = clock();
+   is_isogram(big_word);
+   b = clock();
+   
+   big = (double)(b - a);
+
+   TEST_ASSERT_FLOAT_WITHIN(small, big, small);
+}
+
 int main(void)
 {
    UnityBegin("test/test_isogram.c");
 
    RUN_TEST(test_empty_string);
+   RUN_TEST(test_null);
    RUN_TEST(test_lower_case_only);
    RUN_TEST(test_duplicated_letter);
+   RUN_TEST(test_duplicated_on_ends);
    RUN_TEST(test_duplicated_letter_from_end_of_alphabet);
    RUN_TEST(test_longest_known_isogram);
    RUN_TEST(test_duplicated_letter_mixed_case);
@@ -83,6 +117,7 @@ int main(void)
    RUN_TEST(test_duplicated_non_letter_char);
    RUN_TEST(test_multiple_whitespace);
    RUN_TEST(test_duplicated_letter_within_word);
+   RUN_TEST(test_quit_early);
 
    UnityEnd();
    return 0;
