a6e146a60a8d522338981b5ac31384574cd76ef1
Patrick Jackson <PatrickSJackson@gmail.com>
2019-03-31 17:40:31 -0700

    Removing pointer compare in binary-search. (#371)

--- exercises/binary-search/test/test_binary_search.c
+++ exercises/binary-search/test/test_binary_search.c
@@ -13,7 +13,7 @@ void test_single_element(void)
 {
    int arr[] = { 6 };
    size_t length = sizeof(arr) / sizeof(arr[0]);
-   TEST_ASSERT_EQUAL_PTR(&arr[0], binary_search(6, arr, length));
+   TEST_ASSERT(&arr[0] == binary_search(6, arr, length));
 }
 
 void test_value_in_middle(void)
@@ -21,7 +21,7 @@ void test_value_in_middle(void)
    TEST_IGNORE();               // delete this line to run test
    int arr[] = { 1, 3, 4, 6, 8, 9, 11 };
    size_t length = sizeof(arr) / sizeof(arr[0]);
-   TEST_ASSERT_EQUAL_PTR(&arr[3], binary_search(6, arr, length));
+   TEST_ASSERT(&arr[3] == binary_search(6, arr, length));
 }
 
 void test_value_at_beginning(void)
@@ -29,7 +29,7 @@ void test_value_at_beginning(void)
    TEST_IGNORE();
    int arr[] = { 1, 3, 4, 6, 8, 9, 11 };
    size_t length = sizeof(arr) / sizeof(arr[0]);
-   TEST_ASSERT_EQUAL_PTR(&arr[0], binary_search(1, arr, length));
+   TEST_ASSERT(&arr[0] == binary_search(1, arr, length));
 }
 
 void test_value_at_end(void)
@@ -37,7 +37,7 @@ void test_value_at_end(void)
    TEST_IGNORE();
    int arr[] = { 1, 3, 4, 6, 8, 9, 11 };
    size_t length = sizeof(arr) / sizeof(arr[0]);
-   TEST_ASSERT_EQUAL_PTR(&arr[6], binary_search(11, arr, length));
+   TEST_ASSERT(&arr[6] == binary_search(11, arr, length));
 }
 
 void test_find_value_with_odd_length(void)
@@ -45,7 +45,7 @@ void test_find_value_with_odd_length(void)
    TEST_IGNORE();
    int arr[] = { 1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634 };
    size_t length = sizeof(arr) / sizeof(arr[0]);
-   TEST_ASSERT_EQUAL_PTR(&arr[9], binary_search(144, arr, length));
+   TEST_ASSERT(&arr[9] == binary_search(144, arr, length));
 }
 
 void test_find_value_with_even_length(void)
@@ -53,7 +53,7 @@ void test_find_value_with_even_length(void)
    TEST_IGNORE();
    int arr[] = { 1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377 };
    size_t length = sizeof(arr) / sizeof(arr[0]);
-   TEST_ASSERT_EQUAL_PTR(&arr[5], binary_search(21, arr, length));
+   TEST_ASSERT(&arr[5] == binary_search(21, arr, length));
 }
 
 void test_identify_missing_value(void)
@@ -61,7 +61,7 @@ void test_identify_missing_value(void)
    TEST_IGNORE();
    int arr[] = { 1, 3, 4, 6, 8, 9, 11 };
    size_t length = sizeof(arr) / sizeof(arr[0]);
-   TEST_ASSERT_EQUAL_PTR(NULL, binary_search(7, arr, length));
+   TEST_ASSERT(NULL == binary_search(7, arr, length));
 }
 
 void test_value_smaller_than_everything(void)
@@ -69,7 +69,7 @@ void test_value_smaller_than_everything(void)
    TEST_IGNORE();
    int arr[] = { 1, 3, 4, 6, 8, 9, 11 };
    size_t length = sizeof(arr) / sizeof(arr[0]);
-   TEST_ASSERT_EQUAL_PTR(NULL, binary_search(0, arr, length));
+   TEST_ASSERT(NULL == binary_search(0, arr, length));
 }
 
 void test_value_larger_than_everything(void)
@@ -77,7 +77,7 @@ void test_value_larger_than_everything(void)
    TEST_IGNORE();
    int arr[] = { 1, 3, 4, 6, 8, 9, 11 };
    size_t length = sizeof(arr) / sizeof(arr[0]);
-   TEST_ASSERT_EQUAL_PTR(NULL, binary_search(13, arr, length));
+   TEST_ASSERT(NULL == binary_search(13, arr, length));
 }
 
 void test_empty_array(void)
@@ -85,7 +85,7 @@ void test_empty_array(void)
    TEST_IGNORE();
    int *arr = NULL;
    size_t length = 0;
-   TEST_ASSERT_EQUAL_PTR(NULL, binary_search(1, arr, length));
+   TEST_ASSERT(NULL == binary_search(1, arr, length));
 }
 
 void test_zero_length_array(void)
@@ -93,7 +93,7 @@ void test_zero_length_array(void)
    TEST_IGNORE();
    int arr[] = { 1 };
    size_t length = 0;
-   TEST_ASSERT_EQUAL_PTR(NULL, binary_search(1, arr, length));
+   TEST_ASSERT(NULL == binary_search(1, arr, length));
 }
 
 int main(void)
