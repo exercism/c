b67320947286ebd5f1c34bb3497cdd0b12d66616
Patrick Jackson <PatrickSJackson@gmail.com>
2019-04-01 21:14:22 -0700

    Fixing ShellCheck errors.

--- indent.sh
+++ indent.sh
@@ -7,8 +7,8 @@ INDENT=""
 
 if [ -x "$(command -v gindent)" ]; then
   INDENT=gindent
-elif [ "$OSTYPE" = "darwin"* ]; then
-  echo "ERROR:  Install gnu-indent using home brew to run this script."
+elif [[ "$OSTYPE" = "darwin"* ]]; then
+  echo "ERROR:  Install gnu-indent using Homebrew to run this script."
   exit 126
 elif [ -x "$(command -v indent)" ]; then
   INDENT=indent
@@ -17,7 +17,9 @@ else
   exit 126
 fi
 
-for f in $(find exercises/ -not -path '*/vendor/*' -name '*.c' -or -not -path '*/vendor/*' -name '*.h'); do
-  $INDENT -nbad -bap -nbc -bbo -hnl -br -brs -c33 -cd33 -ncdb -ce -ci4 -cli0 -d0 -di1 -nfc1 -i3 -nut -ip0 -l80 -lp -npcs -nprs -npsl -sai -saf -saw -ncs -nsc -sob -nfca -cp33 -ss -il1 $f
-  rm $f~ 2> /dev/null
-done
+# Run indent on every file
+# shellcheck disable=SC2046
+$INDENT -nbad -bap -nbc -bbo -hnl -br -brs -c33 -cd33 -ncdb -ce -ci4 -cli0 -d0 -di1 -nfc1 -i3 -nut -ip0 -l80 -lp -npcs -nprs -npsl -sai -saf -saw -ncs -nsc -sob -nfca -cp33 -ss -il1 $(find exercises/ -not -path '*/vendor/*' -name '*.c' -or -not -path '*/vendor/*' -name '*.h')
+
+# Remove all the backup files created by indent
+find . -name '*~' -delete
