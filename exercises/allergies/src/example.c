6b75f5dd46ea34dc24b2272c86dfd0be43eae1d8
Patrick Jackson <PatrickSJackson@gmail.com>
2019-06-02 09:50:00 -0700

    Fixing struct initialization for non-conforming clang.

--- exercises/allergies/src/example.c
+++ exercises/allergies/src/example.c
@@ -8,7 +8,10 @@ bool is_allergic_to(allergen_t allergen, unsigned int score)
 
 allergen_list_t get_allergens(unsigned int score)
 {
-   allergen_list_t list = { 0, };
+   allergen_list_t list = {
+      .count = 0,
+      .allergens = {0},
+   };
 
    for (allergen_t allergen = 0; allergen < ALLERGEN_COUNT; ++allergen) {
       if (is_allergic_to(allergen, score)) {
