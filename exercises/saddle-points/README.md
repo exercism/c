1d3bc06b55e84c6f4fc15e3fe6ed792f1e51fcd2
wolf99 <wolf99@users.noreply.github.com>
2019-08-29 01:46:28 +0100

    saddle-points: add exercise (#404)
    * Add saddle-points exercise
    
    * Remove always-null function parameter

---
+++ exercises/saddle-points/README.md
@@ -0,0 +1,67 @@
+# Saddle Points
+
+Detect saddle points in a matrix.
+
+So say you have a matrix like so:
+
+```text
+    1  2  3
+  |---------
+1 | 9  8  7
+2 | 5  3  2     <--- saddle point at column 1, row 2, with value 5
+3 | 6  6  7
+```
+
+It has a saddle point at column 1, row 2.
+
+It's called a "saddle point" because it is greater than or equal to
+every element in its row and less than or equal to every element in
+its column.
+
+A matrix may have zero or more saddle points.
+
+Your code should be able to provide the (possibly empty) list of all the
+saddle points for any given matrix.
+
+The matrix can have a different number of rows and columns (Non square).
+
+Note that you may find other definitions of matrix saddle points online,
+but the tests for this exercise follow the above unambiguous definition.
+
+## Getting Started
+
+Make sure you have read the "Guides" section of the
+[C track](https://exercism.io/my/tracks/c) on the Exercism site. This covers
+the basic information on setting up the development environment expected
+by the exercises.
+
+
+## Passing the Tests
+
+Get the first test compiling, linking and passing by following the [three
+rules of test-driven development][3-tdd-rules].
+
+The included makefile can be used to create and run the tests using the `test`
+task.
+
+    make test
+
+Create just the functions you need to satisfy any compiler errors and get the
+test to fail. Then write just enough code to get the test to pass. Once you've
+done that, move onto the next test.
+
+[3-tdd-rules]: http://butunclebob.com/ArticleS.UncleBob.TheThreeRulesOfTdd
+
+As you progress through the tests, take the time to refactor your
+implementation for readability and expressiveness and then go on to the next
+test.
+
+Try to use standard C99 facilities in preference to writing your own
+low-level algorithms or facilities by hand.
+
+## Source
+
+J Dalbey's Programming Practice problems [http://users.csc.calpoly.edu/~jdalbey/103/Projects/ProgrammingPractice.html](http://users.csc.calpoly.edu/~jdalbey/103/Projects/ProgrammingPractice.html)
+
+## Submitting Incomplete Solutions
+It's possible to submit an incomplete solution so you can see how others have completed the exercise.
---
+++ exercises/saddle-points/test/test_saddle_points.c
@@ -0,0 +1,196 @@
+#include "vendor/unity.h"
+#include "../src/saddle_points.h"
+#include <stdlib.h>
+#include <stdbool.h>
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+static void check_count(size_t expected, size_t actual)
+{
+   TEST_ASSERT_EQUAL_UINT64(expected, actual);
+}
+
+static void check_points(size_t count, saddle_point_t expected[],
+                         saddle_point_t actual[])
+{
+   // assumes array counts match
+   for (size_t i = 0; i < count; ++i) {
+      bool point_found = false;
+      for (size_t j = 0; j < count; ++j) {
+         if (expected[i].row == actual[j].row
+             && expected[i].column == actual[j].column) {
+            point_found = true;
+            break;
+         }
+      }
+      if (!point_found)
+         TEST_FAIL();
+   }
+}
+
+static void test_single_saddle_point(void)
+{
+   uint8_t matrix[3][3] = {
+      {9, 8, 7},
+      {5, 3, 2},
+      {6, 6, 7}
+   };
+
+   size_t expected_count = 1;
+   saddle_point_t expected_points[] = { {2, 1} };
+   saddle_points_t *actual = saddlePoints(3, 3, matrix);
+   check_count(expected_count, actual->count);
+   check_points(expected_count, expected_points, actual->points);
+   free(actual);
+}
+
+static void test_empty_matrix_has_no_saddle_points(void)
+{
+   TEST_IGNORE();               // delete this line to run test
+   size_t expected_count = 0;
+   saddle_points_t *actual = saddlePoints(0, 0, NULL);
+   check_count(expected_count, actual->count);
+   free(actual);
+}
+
+static void test_lack_of_saddle_points_when_there_are_none(void)
+{
+   TEST_IGNORE();
+   uint8_t matrix[3][3] = {
+      {1, 2, 3},
+      {3, 1, 2},
+      {2, 3, 1}
+   };
+
+   size_t expected_count = 0;
+   saddle_points_t *actual = saddlePoints(3, 3, matrix);
+   check_count(expected_count, actual->count);
+   free(actual);
+}
+
+static void test_multiple_saddle_points_in_a_column(void)
+{
+   TEST_IGNORE();
+   uint8_t matrix[3][3] = {
+      {4, 5, 4},
+      {3, 5, 5},
+      {1, 5, 4}
+   };
+
+   size_t expected_count = 3;
+   saddle_point_t expected_points[] = { {1, 2}, {2, 2}, {3, 2} };
+   saddle_points_t *actual = saddlePoints(3, 3, matrix);
+   check_count(expected_count, actual->count);
+   check_points(expected_count, expected_points, actual->points);
+   free(actual);
+}
+
+static void test_multiple_saddle_points_in_a_row(void)
+{
+   TEST_IGNORE();
+   uint8_t matrix[3][3] = {
+      {6, 7, 8},
+      {5, 5, 5},
+      {7, 5, 6}
+   };
+
+   size_t expected_count = 3;
+   saddle_point_t expected_points[] = { {2, 1}, {2, 2}, {2, 3} };
+   saddle_points_t *actual = saddlePoints(3, 3, matrix);
+   check_count(expected_count, actual->count);
+   check_points(expected_count, expected_points, actual->points);
+   free(actual);
+}
+
+static void test_saddle_point_in_bottom_right_corner(void)
+{
+   TEST_IGNORE();
+   uint8_t matrix[3][3] = {
+      {8, 7, 9},
+      {6, 7, 6},
+      {3, 2, 5}
+   };
+
+   size_t expected_count = 1;
+   saddle_point_t expected_points[] = { {3, 3} };
+   saddle_points_t *actual = saddlePoints(3, 3, matrix);
+   check_count(expected_count, actual->count);
+   check_points(expected_count, expected_points, actual->points);
+   free(actual);
+}
+
+static void test_saddle_points_in_non_square_matrix(void)
+{
+   TEST_IGNORE();
+   uint8_t matrix[2][3] = {
+      {3, 1, 3},
+      {3, 2, 4}
+   };
+
+   size_t expected_count = 2;
+   saddle_point_t expected_points[] = { {1, 3}, {1, 1} };
+   saddle_points_t *actual = saddlePoints(2, 3, matrix);
+   check_count(expected_count, actual->count);
+   check_points(expected_count, expected_points, actual->points);
+   free(actual);
+}
+
+static void
+    test_saddle_points_in_single_column_matrix_are_those_with_the_minimum_value
+    (void) {
+   TEST_IGNORE();
+   uint8_t matrix[4][1] = {
+      {2},
+      {1},
+      {4},
+      {1}
+   };
+
+   size_t expected_count = 2;
+   saddle_point_t expected_points[] = { {2, 1}, {4, 1} };
+   saddle_points_t *actual = saddlePoints(4, 1, matrix);
+   check_count(expected_count, actual->count);
+   check_points(expected_count, expected_points, actual->points);
+   free(actual);
+}
+
+static void
+test_saddle_points_in_single_row_matrix_are_those_with_the_maximum_value(void)
+{
+   TEST_IGNORE();
+   uint8_t matrix[1][4] = {
+      {2, 5, 3, 5}
+   };
+
+   size_t expected_count = 2;
+   saddle_point_t expected_points[] = { {1, 2}, {1, 4} };
+   saddle_points_t *actual = saddlePoints(1, 4, matrix);
+   check_count(expected_count, actual->count);
+   check_points(expected_count, expected_points, actual->points);
+   free(actual);
+}
+
+int main(void)
+{
+   UnityBegin("test/test_saddle_points.c");
+
+   RUN_TEST(test_single_saddle_point);
+   RUN_TEST(test_empty_matrix_has_no_saddle_points);
+   RUN_TEST(test_lack_of_saddle_points_when_there_are_none);
+   RUN_TEST(test_multiple_saddle_points_in_a_column);
+   RUN_TEST(test_multiple_saddle_points_in_a_row);
+   RUN_TEST(test_saddle_point_in_bottom_right_corner);
+   RUN_TEST(test_saddle_points_in_non_square_matrix);
+   RUN_TEST
+       (test_saddle_points_in_single_column_matrix_are_those_with_the_minimum_value);
+   RUN_TEST
+       (test_saddle_points_in_single_row_matrix_are_those_with_the_maximum_value);
+
+   return UnityEnd();
+}
