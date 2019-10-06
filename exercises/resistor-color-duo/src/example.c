a3b4cea51ced46a3d8ffd90a6378eb5239746fbf
wolf99 <wol99@users.noreply.github.com>
2019-10-06 21:48:07 +0100

    Update resistor-color-duo

--- exercises/resistor-color-duo/src/example.c
+++ exercises/resistor-color-duo/src/example.c
@@ -1,6 +1,6 @@
 #include "resistor_color_duo.h"
 
-uint16_t colorCode(resistor_band_t colors[])
+uint16_t color_code(resistor_band_t colors[])
 {
    return (uint16_t) (colors[0] * 10 + colors[1]);
 }
--- exercises/resistor-color-duo/test/test_resistor_color_duo.c
+++ exercises/resistor-color-duo/test/test_resistor_color_duo.c
@@ -11,35 +11,35 @@ void tearDown(void)
 
 static void test_brown_and_black(void)
 {
-   uint16_t actual = colorCode((resistor_band_t[]){ BROWN, BLACK });
+   uint16_t actual = color_code((resistor_band_t[]){ BROWN, BLACK });
    TEST_ASSERT_EQUAL_UINT16(10, actual);
 }
 
 static void test_blue_and_grey(void)
 {
    TEST_IGNORE();               // delete this line to run test
-   uint16_t actual = colorCode((resistor_band_t[]){ BLUE, GREY });
+   uint16_t actual = color_code((resistor_band_t[]){ BLUE, GREY });
    TEST_ASSERT_EQUAL_UINT16(68, actual);
 }
 
 static void test_yellow_and_violet(void)
 {
    TEST_IGNORE();
-   uint16_t actual = colorCode((resistor_band_t[]){ YELLOW, VIOLET });
+   uint16_t actual = color_code((resistor_band_t[]){ YELLOW, VIOLET });
    TEST_ASSERT_EQUAL_UINT16(47, actual);
 }
 
 static void test_orange_and_orange(void)
 {
    TEST_IGNORE();
-   uint16_t actual = colorCode((resistor_band_t[]){ ORANGE, ORANGE });
+   uint16_t actual = color_code((resistor_band_t[]){ ORANGE, ORANGE });
    TEST_ASSERT_EQUAL_UINT16(33, actual);
 }
 
 static void test_ignore_additional_colors(void)
 {
    TEST_IGNORE();
-   uint16_t actual = colorCode((resistor_band_t[]){ GREEN, BROWN, ORANGE });
+   uint16_t actual = color_code((resistor_band_t[]){ GREEN, BROWN, ORANGE });
    TEST_ASSERT_EQUAL_UINT16(51, actual);
 }
 
