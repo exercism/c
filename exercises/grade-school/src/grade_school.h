38176e058758675c090c468005e613997992702f
wolf99 <281700+wolf99@users.noreply.github.com>
2020-04-10 20:29:40 +0100

    Add stubs for grade-school (#442)
    * Add stubs for grade-school
    
    * Update grade_school.c
    
    Co-authored-by: wolf99 <wol99@users.noreply.github.com>
    Co-authored-by: Ryan Hartlage <ryanplusplus@gmail.com>

--- exercises/grade-school/src/grade_school.h
+++ exercises/grade-school/src/grade_school.h
@@ -1,4 +1,20 @@
 #ifndef GRADE_SCHOOL_H
 #define GRADE_SCHOOL_H
 
+#include <stddef.h>
+#include <stdint.h>
+
+#define MAX_NAME_LENGTH 20
+#define MAX_STUDENTS 20
+
+typedef struct {
+   uint8_t grade;
+   char *name;
+} student_t;
+
+typedef struct {
+   size_t count;
+   student_t students[MAX_STUDENTS];
+} roster_t;
+
 #endif
