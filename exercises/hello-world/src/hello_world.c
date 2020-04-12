09ee55dea892a1175d224629e7be2d592d2c0cfb
wolf99 <wol99@users.noreply.github.com>
2020-04-12 12:33:58 +0100

    Add explanatory comments

--- exercises/hello-world/src/hello_world.c
+++ exercises/hello-world/src/hello_world.c
@@ -1,5 +1,14 @@
+// Include the standard definitions header from the standard library, so that we
+// have access to `NULL`. This can be removed if your changes remove the need
+// for NULL.
+#include <stddef.h>
+
 #include "hello_world.h"
 
+// Define the function itself.
 const char *hello(void)
 {
+    // To fix this function, change the return statement here to instead return
+    // a string equivelent to the string expected by the failing test.
+    return NULL;
 }
--- exercises/hello-world/test/test_hello_world.c
+++ exercises/hello-world/test/test_hello_world.c
@@ -1,20 +1,30 @@
-#include <stddef.h>
+// Include the test framework.
 #include "vendor/unity.h"
+
+// Include the header file with the declarations of the functions you create.
 #include "../src/hello_world.h"
 
+// Runs before every test.
 void setUp(void)
 {
 }
 
+// Runs after every test.
 void tearDown(void)
 {
 }
 
+// Defines a single test.
 static void test_hello(void)
 {
+   // Check if hello() function returns "Hello, World!"
+   // This test is expected to fail after first downloading this exercise
+   // To make this test pass, fix the hello() function in the src/hello_world.c
+   // source file.
    TEST_ASSERT_EQUAL_STRING("Hello, World!", hello());
 }
 
+// Runs the test(s)
 int main(void)
 {
    UnityBegin("test/test_hello_world.c");
