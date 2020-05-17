9ab1a027b48fec0577d5f93d851c22f001856bb4
Lorraine Lee <n8chz@yahoo.ca>
2020-05-17 12:53:04 -0400

    Update indent.sh, after having verbosed it in hopes of getting some clue as to what was going on.
    Co-authored-by: wolf99 <281700+wolf99@users.noreply.github.com>

--- indent.sh
+++ indent.sh
@@ -19,7 +19,7 @@ fi
 
 # Run indent on every file
 # shellcheck disable=SC2046
-$INDENT -v -nbad -bap -nbc -bbo -hnl -br -brs -c33 -cd33 -ncdb -ce -ci4 -cli0 -d0 -di1 -nfc1 -i3 -nut -ip0 -l80 -lp -npcs -nprs -npsl -sai -saf -saw -ncs -nsc -sob -nfca -cp33 -ss -il1 $(find exercises/ -not -path '*/vendor/*' -name '*.c' -or -not -path '*/vendor/*' -name '*.h')
+$INDENT -nbad -bap -nbc -bbo -hnl -br -brs -c33 -cd33 -ncdb -ce -ci4 -cli0 -d0 -di1 -nfc1 -i3 -nut -ip0 -l80 -lp -npcs -nprs -npsl -sai -saf -saw -ncs -nsc -sob -nfca -cp33 -ss -il1 $(find exercises/ -not -path '*/vendor/*' -name '*.c' -or -not -path '*/vendor/*' -name '*.h')
 
 # Remove all the backup files created by indent
 find . -name '*~' -delete
