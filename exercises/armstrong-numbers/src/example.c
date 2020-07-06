8fdfae9749a6e427a9c516458710cb4976102a4f
elyashiv <elyashi.horovitz@gmail.com>
2020-07-06 15:17:15 +0300

    Use stdbool for armstrongnumbers (#457)
    * use stdbool for return value
    
    * use the same signature in the example
    
    Co-authored-by: Elyashiv <>

--- exercises/armstrong-numbers/src/example.c
+++ exercises/armstrong-numbers/src/example.c
@@ -1,4 +1,6 @@
 #include "armstrong_numbers.h"
+
+#include <stdint.h>
 #include <math.h>
 
 static uint16_t get_digit_count(uint32_t candidate)
@@ -11,14 +13,14 @@ static uint16_t get_digit_count(uint32_t candidate)
    return digit_count;
 }
 
-bool is_armstrong_number(uint32_t candidate)
+bool is_armstrong_number(int candidate)
 {
    bool result = false;
    if (candidate < 10) {
       result = true;
    } else if (candidate >= 100) {
       uint16_t digit_count = get_digit_count(candidate);
-      uint32_t sum = 0, n = candidate;
+      int32_t sum = 0, n = candidate;
       while (n > 0 && sum <= candidate) {
          uint16_t r = n % 10;
          sum += pow(r, digit_count);
