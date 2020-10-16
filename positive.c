da718a6cf421322c78f473ac46b7d2772429922f
sandeepdixit0401 <72873808+sandeepdixit0401@users.noreply.github.com>
2020-10-16 05:58:06 -1200

    positive.c
    find positive and negative in given no.

---
+++ positive.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+void main()
+{
+    int num;
+ 
+    printf("Enter a number: \n");
+    scanf("%d", &num);
+    if (num > 0)
+        printf("%d is a positive number \n", num);
+    else if (num < 0)
+        printf("%d is a negative number \n", num);
+    else
+        printf("0 is neither positive nor negative");
+}
