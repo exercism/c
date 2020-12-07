3e733d18fa4291fdb053394c278a7fc04b8cd674
Ryan Hartlage <ryanplusplus@gmail.com>
2020-12-07 08:37:34 -0500

    Add two-bucket exercise (#542)
    * Added two-bucket
    
    * Addressed review comments
    
    * Addressed more review comments

---
+++ exercises/two-bucket/src/two_bucket.h
@@ -0,0 +1,24 @@
+#ifndef TWO_BUCKET_H
+#define TWO_BUCKET_H
+
+#include <stdbool.h>
+
+typedef enum {
+   BUCKET_ID_1,
+   BUCKET_ID_2
+} bucket_id_t;
+
+typedef unsigned int bucket_liters_t;
+
+typedef struct {
+   bool possible;
+   int move_count;
+   bucket_id_t goal_bucket;
+   bucket_liters_t other_bucket_liters;
+} bucket_result_t;
+
+bucket_result_t measure(bucket_liters_t bucket_1_size,
+                        bucket_liters_t bucket_2_size,
+                        bucket_liters_t goal_volume, bucket_id_t start_bucket);
+
+#endif
