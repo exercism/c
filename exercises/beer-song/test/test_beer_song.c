82eec946a19cacd9ad98e741b0d0dec55bfadad2
wolf99 <281700+wolf99@users.noreply.github.com>
2020-12-27 21:08:51 +0000

    beer-song: align with specification per #497

--- exercises/beer-song/test/test_beer_song.c
+++ exercises/beer-song/test/test_beer_song.c
@@ -11,18 +11,29 @@ void tearDown(void)
 {
 }
 
-static void test_handles_arbitrary_verse(void)
+static void test_first_generic_verse(void)
 {
    char response[BUFFER_SIZE];
    const char expected[BUFFER_SIZE] =
-       "84 bottles of beer on the wall, 84 bottles of beer.\n"
-       "Take one down and pass it around, 83 bottles of beer on the wall.\n";
+       "99 bottles of beer on the wall, 99 bottles of beer.\n"
+       "Take one down and pass it around, 98 bottles of beer on the wall.\n";
 
-   verse(response, 84);
+   recite(99, 1, response);
    TEST_ASSERT_EQUAL_STRING(expected, response);
 }
 
-static void test_handles_2_bottles(void)
+static void test_last_generic_verse(void)
+{
+   char response[BUFFER_SIZE];
+   const char expected[BUFFER_SIZE] =
+       "3 bottles of beer on the wall, 3 bottles of beer.\n"
+       "Take one down and pass it around, 2 bottles of beer on the wall.\n";
+
+   recite(3, 1, response);
+   TEST_ASSERT_EQUAL_STRING(expected, response);
+}
+
+static void test_verse_with_2_bottles(void)
 {
    TEST_IGNORE();               // delete this line to run test
    char response[BUFFER_SIZE];
@@ -30,11 +41,11 @@ static void test_handles_2_bottles(void)
        "2 bottles of beer on the wall, 2 bottles of beer.\n"
        "Take one down and pass it around, 1 bottle of beer on the wall.\n";
 
-   verse(response, 2);
+   recite(2, 1, response);
    TEST_ASSERT_EQUAL_STRING(expected, response);
 }
 
-static void test_handles_1_bottle(void)
+static void test_verse_with_1_bottle(void)
 {
    TEST_IGNORE();
    char response[BUFFER_SIZE];
@@ -42,11 +53,11 @@ static void test_handles_1_bottle(void)
        "1 bottle of beer on the wall, 1 bottle of beer.\n"
        "Take it down and pass it around, no more bottles of beer on the wall.\n";
 
-   verse(response, 1);
+   recite(1, 1, response);
    TEST_ASSERT_EQUAL_STRING(expected, response);
 }
 
-static void test_handles_0_bottles(void)
+static void test_verse_with_0_bottles(void)
 {
    TEST_IGNORE();
    char response[BUFFER_SIZE];
@@ -54,36 +65,30 @@ static void test_handles_0_bottles(void)
        "No more bottles of beer on the wall, no more bottles of beer.\n"
        "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
 
-   verse(response, 0);
+   recite(0, 1, response);
    TEST_ASSERT_EQUAL_STRING(expected, response);
 }
 
-static void test_sings_several_verses(void)
+static void test_first_two_verses(void)
 {
    TEST_IGNORE();
    char response[BUFFER_SIZE];
    const char expected[BUFFER_SIZE] =
-       "8 bottles of beer on the wall, 8 bottles of beer.\n"
-       "Take one down and pass it around, 7 bottles of beer on the wall.\n"
+       "99 bottles of beer on the wall, 99 bottles of beer.\n"
+       "Take one down and pass it around, 98 bottles of beer on the wall.\n"
        "\n"
-       "7 bottles of beer on the wall, 7 bottles of beer.\n"
-       "Take one down and pass it around, 6 bottles of beer on the wall.\n"
-       "\n"
-       "6 bottles of beer on the wall, 6 bottles of beer.\n"
-       "Take one down and pass it around, 5 bottles of beer on the wall.\n";
+       "98 bottles of beer on the wall, 98 bottles of beer.\n"
+       "Take one down and pass it around, 97 bottles of beer on the wall.\n";
 
-   sing(response, 8, 6);
+   recite(99, 2, response);
    TEST_ASSERT_EQUAL_STRING(expected, response);
 }
 
-static void test_sings_the_last_4_verses(void)
+static void test_last_three_verses(void)
 {
    TEST_IGNORE();
    char response[BUFFER_SIZE];
    const char expected[BUFFER_SIZE] =
-       "3 bottles of beer on the wall, 3 bottles of beer.\n"
-       "Take one down and pass it around, 2 bottles of beer on the wall.\n"
-       "\n"
        "2 bottles of beer on the wall, 2 bottles of beer.\n"
        "Take one down and pass it around, 1 bottle of beer on the wall.\n"
        "\n"
@@ -93,7 +98,7 @@ static void test_sings_the_last_4_verses(void)
        "No more bottles of beer on the wall, no more bottles of beer.\n"
        "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
 
-   sing(response, 3, 0);
+   recite(2, 3, response);
    TEST_ASSERT_EQUAL_STRING(expected, response);
 }
 
@@ -101,12 +106,13 @@ int main(void)
 {
    UnityBegin("test/test_beer_song.c");
 
-   RUN_TEST(test_handles_arbitrary_verse);
-   RUN_TEST(test_handles_2_bottles);
-   RUN_TEST(test_handles_1_bottle);
-   RUN_TEST(test_handles_0_bottles);
-   RUN_TEST(test_sings_several_verses);
-   RUN_TEST(test_sings_the_last_4_verses);
+   RUN_TEST(test_first_generic_verse);
+   RUN_TEST(test_last_generic_verse);
+   RUN_TEST(test_verse_with_2_bottles);
+   RUN_TEST(test_verse_with_1_bottle);
+   RUN_TEST(test_verse_with_0_bottles);
+   RUN_TEST(test_first_two_verses);
+   RUN_TEST(test_last_three_verses);
 
    return UnityEnd();
 }
