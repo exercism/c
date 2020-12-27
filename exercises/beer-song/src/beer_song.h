a0230d3b2c1580855334a8006c5789b60556e1a9
wolf99 <281700+wolf99@users.noreply.github.com>
2020-12-27 21:08:51 +0000

    beer-song: align with specification per #497

--- exercises/beer-song/src/beer_song.h
+++ exercises/beer-song/src/beer_song.h
@@ -1,8 +1,6 @@
 #ifndef BEER_SONG_H
 #define BEER_SONG_H
 
-void verse(char *buffer, unsigned int verse_number);
-void sing(char *buffer, unsigned int starting_verse_number,
-          unsigned int ending_verse_number);
+void recite(unsigned int start_bottles, unsigned int take_down, char *buffer);
 
 #endif
--- exercises/beer-song/src/example.c
+++ exercises/beer-song/src/example.c
@@ -2,49 +2,42 @@
 #include <stdio.h>
 #include <string.h>
 
-static unsigned int get_verse(char *buffer, unsigned int verse_number)
+static unsigned int get_verse(unsigned int bottles, char *buffer)
 {
    unsigned int bytes_written = 0;
 
-   if (verse_number == 0) {
+   if (bottles == 0) {
       bytes_written =
           sprintf(buffer,
                   "No more bottles of beer on the wall, no more bottles of beer.\n"
                   "Go to the store and buy some more, 99 bottles of beer on the wall.\n");
-   } else if (verse_number == 1) {
+   } else if (bottles == 1) {
       bytes_written =
           sprintf(buffer,
                   "1 bottle of beer on the wall, 1 bottle of beer.\n"
                   "Take it down and pass it around, no more bottles of beer on the wall.\n");
-   } else if (verse_number == 2) {
+   } else if (bottles == 2) {
       bytes_written =
           sprintf(buffer,
                   "2 bottles of beer on the wall, 2 bottles of beer.\n"
                   "Take one down and pass it around, 1 bottle of beer on the wall.\n");
-   } else if (verse_number <= 99) {
+   } else if (bottles <= 99) {
       bytes_written =
           sprintf(buffer,
                   "%u bottles of beer on the wall, %u bottles of beer.\n"
                   "Take one down and pass it around, %u bottles of beer on the wall.\n",
-                  verse_number, verse_number, verse_number - 1);
+                  bottles, bottles, bottles - 1);
    }
 
    return bytes_written;
 }
 
-void verse(char *buffer, unsigned int verse_number)
-{
-   (void)get_verse(buffer, verse_number);
-}
-
-void sing(char *buffer, unsigned int starting_verse_number,
-          unsigned int ending_verse_number)
+void recite(unsigned int start_bottles, unsigned int take_down, char *buffer)
 {
    char *current_position = buffer;
 
-   for (unsigned int i = starting_verse_number + 1; i > ending_verse_number;
-        i--) {
-      current_position += get_verse(current_position, i - 1);
+   while (take_down-- > 0) {
+      current_position += get_verse(start_bottles--, current_position);
       strcpy(current_position, "\n");
       current_position++;
    }
