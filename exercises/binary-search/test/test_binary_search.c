eeb266668591491974a6a70732a33f550268e725
wolf99 <281700+wolf99@users.noreply.github.com>
2020-12-28 18:10:21 +0000

    binary-search: align with specification per #499

--- exercises/binary-search/test/test_binary_search.c
+++ exercises/binary-search/test/test_binary_search.c
@@ -9,14 +9,14 @@ void tearDown(void)
 {
 }
 
-static void test_single_element(void)
+static void test_finds_a_value_in_an_array_with_one_element(void)
 {
    int arr[] = { 6 };
    size_t length = sizeof(arr) / sizeof(arr[0]);
    TEST_ASSERT(&arr[0] == binary_search(6, arr, length));
 }
 
-static void test_value_in_middle(void)
+static void test_a_value_in_the_middle_of_an_array(void)
 {
    TEST_IGNORE();               // delete this line to run test
    int arr[] = { 1, 3, 4, 6, 8, 9, 11 };
@@ -24,7 +24,7 @@ static void test_value_in_middle(void)
    TEST_ASSERT(&arr[3] == binary_search(6, arr, length));
 }
 
-static void test_value_at_beginning(void)
+static void test_finds_a_value_at_the_beginning_of_an_array(void)
 {
    TEST_IGNORE();
    int arr[] = { 1, 3, 4, 6, 8, 9, 11 };
@@ -32,7 +32,7 @@ static void test_value_at_beginning(void)
    TEST_ASSERT(&arr[0] == binary_search(1, arr, length));
 }
 
-static void test_value_at_end(void)
+static void test_finds_a_value_at_the_end_of_an_array(void)
 {
    TEST_IGNORE();
    int arr[] = { 1, 3, 4, 6, 8, 9, 11 };
@@ -40,7 +40,7 @@ static void test_value_at_end(void)
    TEST_ASSERT(&arr[6] == binary_search(11, arr, length));
 }
 
-static void test_find_value_with_odd_length(void)
+static void test_finds_a_value_in_an_array_of_odd_length(void)
 {
    TEST_IGNORE();
    int arr[] = { 1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634 };
@@ -48,7 +48,7 @@ static void test_find_value_with_odd_length(void)
    TEST_ASSERT(&arr[9] == binary_search(144, arr, length));
 }
 
-static void test_find_value_with_even_length(void)
+static void test_finds_a_value_in_an_array_of_even_length(void)
 {
    TEST_IGNORE();
    int arr[] = { 1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377 };
@@ -56,7 +56,7 @@ static void test_find_value_with_even_length(void)
    TEST_ASSERT(&arr[5] == binary_search(21, arr, length));
 }
 
-static void test_identify_missing_value(void)
+static void test_identifies_that_a_value_is_not_included_in_the_array(void)
 {
    TEST_IGNORE();
    int arr[] = { 1, 3, 4, 6, 8, 9, 11 };
@@ -64,7 +64,8 @@ static void test_identify_missing_value(void)
    TEST_ASSERT(NULL == binary_search(7, arr, length));
 }
 
-static void test_value_smaller_than_everything(void)
+static void
+test_a_value_smaller_than_the_arrays_smallest_value_is_not_found(void)
 {
    TEST_IGNORE();
    int arr[] = { 1, 3, 4, 6, 8, 9, 11 };
@@ -72,7 +73,7 @@ static void test_value_smaller_than_everything(void)
    TEST_ASSERT(NULL == binary_search(0, arr, length));
 }
 
-static void test_value_larger_than_everything(void)
+static void test_a_value_larger_than_the_arrays_largest_value_is_not_found(void)
 {
    TEST_IGNORE();
    int arr[] = { 1, 3, 4, 6, 8, 9, 11 };
@@ -80,18 +81,26 @@ static void test_value_larger_than_everything(void)
    TEST_ASSERT(NULL == binary_search(13, arr, length));
 }
 
-static void test_empty_array(void)
+static void test_nothing_is_found_in_an_empty_array(void)
 {
    TEST_IGNORE();
-   int *arr = NULL;
+   int arr[] = { 1 };
    size_t length = 0;
    TEST_ASSERT(NULL == binary_search(1, arr, length));
 }
 
-static void test_zero_length_array(void)
+static void test_nothing_is_found_when_the_left_and_right_bounds_cross(void)
 {
    TEST_IGNORE();
-   int arr[] = { 1 };
+   int arr[] = { 1, 2 };
+   size_t length = sizeof(arr) / sizeof(arr[0]);
+   TEST_ASSERT(NULL == binary_search(0, arr, length))
+}
+
+static void test_nothing_is_found_if_array_is_null(void)
+{
+   TEST_IGNORE();
+   int *arr = NULL;
    size_t length = 0;
    TEST_ASSERT(NULL == binary_search(1, arr, length));
 }
@@ -100,17 +109,18 @@ int main(void)
 {
    UnityBegin("test/test_binary_search.c");
 
-   RUN_TEST(test_single_element);
-   RUN_TEST(test_value_in_middle);
-   RUN_TEST(test_value_at_beginning);
-   RUN_TEST(test_value_at_end);
-   RUN_TEST(test_find_value_with_odd_length);
-   RUN_TEST(test_find_value_with_even_length);
-   RUN_TEST(test_identify_missing_value);
-   RUN_TEST(test_value_smaller_than_everything);
-   RUN_TEST(test_value_larger_than_everything);
-   RUN_TEST(test_empty_array);
-   RUN_TEST(test_zero_length_array);
+   RUN_TEST(test_finds_a_value_in_an_array_with_one_element);
+   RUN_TEST(test_a_value_in_the_middle_of_an_array);
+   RUN_TEST(test_finds_a_value_at_the_beginning_of_an_array);
+   RUN_TEST(test_finds_a_value_at_the_end_of_an_array);
+   RUN_TEST(test_finds_a_value_in_an_array_of_odd_length);
+   RUN_TEST(test_finds_a_value_in_an_array_of_even_length);
+   RUN_TEST(test_identifies_that_a_value_is_not_included_in_the_array);
+   RUN_TEST(test_a_value_smaller_than_the_arrays_smallest_value_is_not_found);
+   RUN_TEST(test_a_value_larger_than_the_arrays_largest_value_is_not_found);
+   RUN_TEST(test_nothing_is_found_in_an_empty_array);
+   RUN_TEST(test_nothing_is_found_when_the_left_and_right_bounds_cross);
+   RUN_TEST(test_nothing_is_found_if_array_is_null);
 
    return UnityEnd();
 }
