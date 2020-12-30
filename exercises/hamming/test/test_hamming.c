e0da781683228484560f552b6846805e409147c7
wolf99 <281700+wolf99@users.noreply.github.com>
2020-12-30 22:46:30 +0000

    hamming: align with specification per #504 (#556)
    * hamming: align with specification per #504
    
    * Remove tests that don't do what is intended

--- exercises/hamming/test/test_hamming.c
+++ exercises/hamming/test/test_hamming.c
@@ -14,76 +14,40 @@ static void test_empty_strands(void)
    TEST_ASSERT_EQUAL(0, compute("", ""));
 }
 
-static void test_rejects_null_strand(void)
+static void test_single_identical_strands(void)
 {
    TEST_IGNORE();               // delete this line to run test
-   TEST_ASSERT_EQUAL(-1, compute(NULL, "A"));
-}
-
-static void test_rejects_other_null_strand(void)
-{
-   TEST_IGNORE();
-   TEST_ASSERT_EQUAL(-1, compute("A", NULL));
-}
-
-static void test_no_difference_between_identical_strands(void)
-{
-   TEST_IGNORE();
    TEST_ASSERT_EQUAL(0, compute("A", "A"));
 }
 
-static void test_identical_long_strands(void)
+static void test_single_letter_different_strands(void)
 {
    TEST_IGNORE();
-   TEST_ASSERT_EQUAL(0, compute("GGACTGA", "GGACTGA"));
+   TEST_ASSERT_EQUAL(1, compute("G", "T"));
 }
 
-static void test_hamming_distance_for_single_nucleotide_strand(void)
+static void test_long_identical_strands(void)
 {
    TEST_IGNORE();
-   TEST_ASSERT_EQUAL(1, compute("A", "G"));
+   TEST_ASSERT_EQUAL(0, compute("GGACTGAAATCTG", "GGACTGAAATCTG"));
 }
 
-static void test_complete_hamming_distance_for_small_strand(void)
-{
-   TEST_IGNORE();
-   TEST_ASSERT_EQUAL(2, compute("AG", "CT"));
-}
-
-static void test_small_hamming_distance(void)
-{
-   TEST_IGNORE();
-   TEST_ASSERT_EQUAL(1, compute("AT", "CT"));
-}
-
-static void test_small_hamming_distance_in_longer_strand(void)
-{
-   TEST_IGNORE();
-   TEST_ASSERT_EQUAL(1, compute("GGACG", "GGTCG"));
-}
-
-static void test_rejects_extra_length_on_first_strand_when_longer(void)
-{
-   TEST_IGNORE();
-   TEST_ASSERT_EQUAL(-1, compute("AAAG", "AAA"));
-}
-
-static void test_rejects_extra_length_on_other_strand_when_longer(void)
-{
-   TEST_IGNORE();
-   TEST_ASSERT_EQUAL(-1, compute("AAA", "AAAG"));
-}
-
-static void test_large_hamming_distance(void)
-{
-   TEST_IGNORE();
-   TEST_ASSERT_EQUAL(4, compute("GATACA", "GCATAA"));
-}
-
-static void test_hamming_distance_in_very_long_strand(void)
+static void test_long_different_strands(void)
 {
    TEST_IGNORE();
    TEST_ASSERT_EQUAL(9, compute("GGACGGATTCTG", "AGGACGGATTCT"));
+}
+
+static void test_disallow_first_strand_when_longer(void)
+{
+   TEST_IGNORE();
+   TEST_ASSERT_EQUAL(-1, compute("AATG", "AAA"));
+}
+
+static void test_disallow_second_strand_when_longer(void)
+{
+   TEST_IGNORE();
+   TEST_ASSERT_EQUAL(-1, compute("ATA", "AGTG"));
 }
 
 int main(void)
@@ -91,18 +55,12 @@ int main(void)
    UnityBegin("test/test_hamming.c");
 
    RUN_TEST(test_empty_strands);
-   RUN_TEST(test_no_difference_between_identical_strands);
-   RUN_TEST(test_rejects_null_strand);
-   RUN_TEST(test_rejects_other_null_strand);
-   RUN_TEST(test_identical_long_strands);
-   RUN_TEST(test_hamming_distance_for_single_nucleotide_strand);
-   RUN_TEST(test_complete_hamming_distance_for_small_strand);
-   RUN_TEST(test_small_hamming_distance);
-   RUN_TEST(test_small_hamming_distance_in_longer_strand);
-   RUN_TEST(test_rejects_extra_length_on_first_strand_when_longer);
-   RUN_TEST(test_rejects_extra_length_on_other_strand_when_longer);
-   RUN_TEST(test_large_hamming_distance);
-   RUN_TEST(test_hamming_distance_in_very_long_strand);
+   RUN_TEST(test_single_identical_strands);
+   RUN_TEST(test_single_letter_different_strands);
+   RUN_TEST(test_long_identical_strands);
+   RUN_TEST(test_long_different_strands);
+   RUN_TEST(test_disallow_first_strand_when_longer);
+   RUN_TEST(test_disallow_second_strand_when_longer);
 
    return UnityEnd();
 }
