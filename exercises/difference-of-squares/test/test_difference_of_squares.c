412684a8f3ac3ae07b1913d49b540f03e26d7ee0
wolf99 <281700+wolf99@users.noreply.github.com>
2020-12-30 15:30:11 +0000

    difference-of-squares: align with specification per #502 (#554)

--- exercises/difference-of-squares/test/test_difference_of_squares.c
+++ exercises/difference-of-squares/test/test_difference_of_squares.c
@@ -9,60 +9,54 @@ void tearDown(void)
 {
 }
 
-static void test_square_of_sum_up_to_5(void)
+static void test_square_of_sum_1(void)
 {
+   TEST_ASSERT_EQUAL(1, square_of_sum(1));
+}
+
+static void test_square_of_sum_5(void)
+{
+   TEST_IGNORE();               // delete this line to run test
    TEST_ASSERT_EQUAL(225, square_of_sum(5));
 }
 
-static void test_square_of_sum_up_to_10(void)
-{
-   TEST_IGNORE();               // delete this line to run test
-   TEST_ASSERT_EQUAL(3025, square_of_sum(10));
-}
-
-static void test_square_of_sum_up_to_100(void)
+static void test_square_of_sum_100(void)
 {
    TEST_IGNORE();
    TEST_ASSERT_EQUAL(25502500, square_of_sum(100));
 }
 
-static void test_sum_of_squares_up_to_5(void)
+static void test_sum_of_squares_1(void)
+{
+   TEST_IGNORE();
+   TEST_ASSERT_EQUAL(1, sum_of_squares(1));
+}
+
+static void test_sum_of_squares_5(void)
 {
    TEST_IGNORE();
    TEST_ASSERT_EQUAL(55, sum_of_squares(5));
 }
 
-static void test_sum_of_squares_up_to_10(void)
-{
-   TEST_IGNORE();
-   TEST_ASSERT_EQUAL(385, sum_of_squares(10));
-}
-
-static void test_sum_of_squares_up_to_100(void)
+static void test_sum_of_squares_100(void)
 {
    TEST_IGNORE();
    TEST_ASSERT_EQUAL(338350, sum_of_squares(100));
 }
 
-static void test_difference_of_squares_up_to_0(void)
+static void test_difference_of_squares_1(void)
 {
    TEST_IGNORE();
-   TEST_ASSERT_EQUAL(0, difference_of_squares(0));
+   TEST_ASSERT_EQUAL(0, difference_of_squares(1));
 }
 
-static void test_difference_of_squares_up_to_5(void)
+static void test_difference_of_squares_5(void)
 {
    TEST_IGNORE();
    TEST_ASSERT_EQUAL(170, difference_of_squares(5));
 }
 
-static void test_difference_of_squares_up_to_10(void)
-{
-   TEST_IGNORE();
-   TEST_ASSERT_EQUAL(2640, difference_of_squares(10));
-}
-
-static void test_difference_of_squares_up_to_100(void)
+static void test_difference_of_squares_100(void)
 {
    TEST_IGNORE();
    TEST_ASSERT_EQUAL(25164150, difference_of_squares(100));
@@ -72,16 +66,15 @@ int main(void)
 {
    UnityBegin("test/test_difference_of_squares.c");
 
-   RUN_TEST(test_square_of_sum_up_to_5);
-   RUN_TEST(test_square_of_sum_up_to_10);
-   RUN_TEST(test_square_of_sum_up_to_100);
-   RUN_TEST(test_sum_of_squares_up_to_5);
-   RUN_TEST(test_sum_of_squares_up_to_10);
-   RUN_TEST(test_sum_of_squares_up_to_100);
-   RUN_TEST(test_difference_of_squares_up_to_0);
-   RUN_TEST(test_difference_of_squares_up_to_5);
-   RUN_TEST(test_difference_of_squares_up_to_10);
-   RUN_TEST(test_difference_of_squares_up_to_100);
+   RUN_TEST(test_square_of_sum_1);
+   RUN_TEST(test_square_of_sum_5);
+   RUN_TEST(test_square_of_sum_100);
+   RUN_TEST(test_sum_of_squares_1);
+   RUN_TEST(test_sum_of_squares_5);
+   RUN_TEST(test_sum_of_squares_100);
+   RUN_TEST(test_difference_of_squares_1);
+   RUN_TEST(test_difference_of_squares_5);
+   RUN_TEST(test_difference_of_squares_100);
 
    return UnityEnd();
 }
