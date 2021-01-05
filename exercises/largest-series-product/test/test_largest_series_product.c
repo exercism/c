af42c5ad9d143b81c07309622a52187862fb2806
wolf99 <281700+wolf99@users.noreply.github.com>
2021-01-05 21:17:18 +0000

    Revert UUT name to match exercise name

--- exercises/largest-series-product/test/test_largest_series_product.c
+++ exercises/largest-series-product/test/test_largest_series_product.c
@@ -11,93 +11,93 @@ void tearDown(void)
 
 static void test_finds_the_largest_product_if_span_equals_length(void)
 {
-   TEST_ASSERT_EQUAL(18, largest_product("29", 2));
+   TEST_ASSERT_EQUAL(18, largest_series_product("29", 2));
 }
 
 static void test_can_find_the_largest_product_of_2_with_numbers_in_order(void)
 {
    TEST_IGNORE();               // delete this line to run test
-   TEST_ASSERT_EQUAL(72, largest_product("0123456789", 2));
+   TEST_ASSERT_EQUAL(72, largest_series_product("0123456789", 2));
 }
 
 static void test_can_find_the_largest_product_of_2(void)
 {
    TEST_IGNORE();
-   TEST_ASSERT_EQUAL(48, largest_product("576802143", 2));
+   TEST_ASSERT_EQUAL(48, largest_series_product("576802143", 2));
 }
 
 static void test_can_find_the_largest_product_of_3_with_numbers_in_order(void)
 {
    TEST_IGNORE();
-   TEST_ASSERT_EQUAL(504, largest_product("0123456789", 3));
+   TEST_ASSERT_EQUAL(504, largest_series_product("0123456789", 3));
 }
 
 static void test_can_find_the_largest_product_of_3(void)
 {
    TEST_IGNORE();
-   TEST_ASSERT_EQUAL(270, largest_product("1027839564", 3));
+   TEST_ASSERT_EQUAL(270, largest_series_product("1027839564", 3));
 }
 
 static void test_can_find_the_largest_product_of_5_with_numbers_in_order(void)
 {
    TEST_IGNORE();
-   TEST_ASSERT_EQUAL(15120, largest_product("0123456789", 5));
+   TEST_ASSERT_EQUAL(15120, largest_series_product("0123456789", 5));
 }
 
 static void test_can_get_the_largest_product_of_a_big_number(void)
 {
    TEST_IGNORE();
    TEST_ASSERT_EQUAL(23520,
-                     largest_product
+                     largest_series_product
                      ("73167176531330624919225119674426574742355349194934", 6));
 }
 
 static void test_reports_zero_if_the_only_digits_are_zero(void)
 {
    TEST_IGNORE();
-   TEST_ASSERT_EQUAL(0, largest_product("0000", 2));
+   TEST_ASSERT_EQUAL(0, largest_series_product("0000", 2));
 }
 
 static void test_reports_zero_if_all_spans_include_zero(void)
 {
    TEST_IGNORE();
-   TEST_ASSERT_EQUAL(0, largest_product("99099", 3));
+   TEST_ASSERT_EQUAL(0, largest_series_product("99099", 3));
 }
 
 static void test_rejects_span_longer_than_string_length(void)
 {
    TEST_IGNORE();
-   TEST_ASSERT_EQUAL(-1, largest_product("123", 4));
+   TEST_ASSERT_EQUAL(-1, largest_series_product("123", 4));
 }
 
 static void test_reports_1_for_empty_string_and_empty_product(void)
 {
    TEST_IGNORE();
-   TEST_ASSERT_EQUAL(1, largest_product("", 0));
+   TEST_ASSERT_EQUAL(1, largest_series_product("", 0));
 }
 
 static void test_reports_1_for_nonempty_string_and_empty_product(void)
 {
    TEST_IGNORE();
-   TEST_ASSERT_EQUAL(1, largest_product("123", 0));
+   TEST_ASSERT_EQUAL(1, largest_series_product("123", 0));
 }
 
 static void test_rejects_empty_string_and_nonzero_span(void)
 {
    TEST_IGNORE();
-   TEST_ASSERT_EQUAL(-1, largest_product("", 1));
+   TEST_ASSERT_EQUAL(-1, largest_series_product("", 1));
 }
 
 static void test_rejects_invalid_character_in_digits(void)
 {
    TEST_IGNORE();
-   TEST_ASSERT_EQUAL(-1, largest_product("123a5", 2));
+   TEST_ASSERT_EQUAL(-1, largest_series_product("123a5", 2));
 }
 
 static void test_rejects_negative_span(void)
 {
    TEST_IGNORE();
-   TEST_ASSERT_EQUAL(-1, largest_product("12345", -1));
+   TEST_ASSERT_EQUAL(-1, largest_series_product("12345", -1));
 }
 
 int main(void)
