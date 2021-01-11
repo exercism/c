46d62ea4d1abe96af450c0112417ab923d5c7fad
wolf99 <281700+wolf99@users.noreply.github.com>
2021-01-11 00:18:16 +0000

    List ops update (#560)
    * list-ops: align with specification per #509
    
    * tidy up test helper usage
    
    Removes the need for a list for the expected values
    
    * [CI] Format C code
    
    * Trigger build
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- exercises/list-ops/src/list_ops.h
+++ exercises/list-ops/src/list_ops.h
@@ -8,34 +8,34 @@ typedef int list_value_t;
 
 typedef struct {
    size_t length;
-   list_value_t values[];
+   list_element_t elements[];
 } list_t;
 
 // constructs a new list
-list_t *new_list(size_t length, list_value_t values[]);
+list_t *new_list(size_t length, list_element_t elements[]);
 
 // append entries to a list and return the new list
 list_t *append_list(list_t * list1, list_t * list2);
 
 // filter list returning only values that satisfy the filter function
-list_t *filter_list(list_t * list, bool(*filter) (list_value_t value));
+list_t *filter_list(list_t * list, bool(*filter) (list_element_t));
 
 // returns the length of the list
 size_t length_list(list_t * list);
 
 // return a list of elements whose values equal the list value transformed by
 // the mapping function
-list_t *map_list(list_t * list, list_value_t(*map) (list_value_t value));
+list_t *map_list(list_t * list, list_element_t(*map) (list_element_t));
 
 // folds (reduces) the given list from the left with a function
-list_value_t foldl_list(list_t * list, list_value_t initial,
-                        list_value_t(*foldl) (list_value_t value,
-                                              list_value_t initial));
+list_element_t foldl_list(list_t * list, list_element_t initial,
+                          list_element_t(*foldl) (list_element_t,
+                                                  list_element_t));
 
 // folds (reduces) the given list from the right with a function
-list_value_t foldr_list(list_t * list, list_value_t initial,
-                        list_value_t(*foldr) (list_value_t value,
-                                              list_value_t initial));
+list_element_t foldr_list(list_t * list, list_element_t initial,
+                          list_element_t(*foldr) (list_element_t,
+                                                  list_element_t));
 
 // reverse the elements of the list
 list_t *reverse_list(list_t * list);
--- exercises/list-ops/test/test_list_ops.c
+++ exercises/list-ops/test/test_list_ops.c
@@ -11,13 +11,13 @@ void tearDown(void)
 {
 }
 
-static char *print_list(list_t * list)
+static char *print_elements(size_t length, list_element_t list_elements[])
 {
    char *array = malloc(MAX_STRING_LEN * sizeof(char));
    char *ptr = array;
-   for (size_t i = 0; i < list->length; i++) {
+   for (size_t i = 0; i < length; i++) {
       int printed = snprintf(ptr, MAX_STRING_LEN - (ptr - array), " %d ",
-                             (list->values[i]));
+                             (list_elements[i]));
       ptr += printed;
       if (ptr - array > MAX_STRING_LEN) {
          break;
@@ -26,11 +26,13 @@ static char *print_list(list_t * list)
    return array;
 }
 
-static char *create_error_message(list_t * expected, list_t * actual)
+static char *create_error_message(size_t length,
+                                  list_element_t expected_elements[],
+                                  list_element_t actual_elements[])
 {
    char *message = malloc(MAX_STRING_LEN * sizeof(char));
-   char *expected_string = print_list(expected);
-   char *actual_string = print_list(actual);
+   char *expected_string = print_elements(length, expected_elements);
+   char *actual_string = print_elements(length, actual_elements);
    snprintf(message, MAX_STRING_LEN, "[%s] != [%s]", expected_string,
             actual_string);
    free(expected_string);
@@ -38,70 +40,68 @@ static char *create_error_message(list_t * expected, list_t * actual)
    return message;
 }
 
-static void check_list_contents(list_value_t expected[], list_value_t actual[],
-                                size_t expected_length)
+static void check_lists_match(size_t expected_length,
+                              list_element_t expected_elements[],
+                              list_t * actual)
 {
-   TEST_ASSERT_EQUAL_MEMORY_ARRAY(expected, actual,
-                                  sizeof(list_value_t), expected_length);
-}
-
-static void check_lists_match(list_t * expected, list_t * actual)
-{
-   // check inputs to be valid lists
-   TEST_ASSERT_NOT_NULL(expected);
+   // check actual list is a valid list
    TEST_ASSERT_NOT_NULL(actual);
 
    // check lengths match
-   TEST_ASSERT_EQUAL_MESSAGE(expected->length, actual->length,
+   TEST_ASSERT_EQUAL_MESSAGE(expected_length, actual->length,
                              "List lengths differ");
 
-   // check values match in non-zero length lists
-   if (expected->length) {
-      char *error_message = create_error_message(expected, actual);
-      TEST_ASSERT_EQUAL_MEMORY_ARRAY_MESSAGE(expected->values, actual->values,
-                                             sizeof(list_value_t),
-                                             expected->length, error_message);
+   // check elements match in non-zero length list
+   if (expected_length) {
+      char *error_message =
+          create_error_message(expected_length, expected_elements,
+                               actual->elements);
+      TEST_ASSERT_EQUAL_MEMORY_ARRAY_MESSAGE(expected_elements,
+                                             actual->elements,
+                                             sizeof(list_element_t),
+                                             expected_length, error_message);
       free(error_message);
    }
 }
 
-static bool filter_modulo(list_value_t value)
+static bool filter_modulo(list_element_t element)
 {
-   return (value % 2 == 1);
+   return (element % 2 == 1);
 }
 
-static list_value_t map_increment(list_value_t value)
+static list_element_t map_increment(list_element_t element)
 {
-   return value + 1;
+   return element + 1;
 }
 
-static list_value_t fold_multiply(list_value_t value, list_value_t initial)
+static list_element_t fold_multiply(list_element_t element,
+                                    list_element_t accumulator)
 {
-   return value * initial;
+   return element * accumulator;
 }
 
-static list_value_t fold_add(list_value_t value, list_value_t initial)
+static list_element_t fold_add(list_element_t element,
+                               list_element_t accumulator)
 {
-   return value + initial;
+   return element + accumulator;
 }
 
-static list_value_t fold_divide(list_value_t value, list_value_t initial)
+static list_element_t fold_divide(list_element_t element,
+                                  list_element_t accumulator)
 {
-   return (initial == 0) ? 0 : value / initial;
+   return (accumulator == 0) ? 0 : element / accumulator;
 }
 
 static void test_append_empty_lists(void)
 {
    list_t *list1 = new_list(0, NULL);
    list_t *list2 = new_list(0, NULL);
-   list_t *expected = new_list(0, NULL);
 
    list_t *actual = append_list(list1, list2);
-   check_lists_match(expected, actual);
+   check_lists_match(0, NULL, actual);
 
    delete_list(list1);
    delete_list(list2);
-   delete_list(expected);
    delete_list(actual);
 }
 
@@ -109,56 +109,47 @@ static void test_append_list_to_empty_list(void)
 {
    TEST_IGNORE();               // delete this line to run test
    list_t *list1 = new_list(0, NULL);
-   list_t *list2 = new_list(3, (list_value_t[]){ 1, 3, 4 });
-   size_t expected_len = 3;
-   list_value_t expected_values[] = { 1, 3, 4 };
-   list_t *expected = new_list(expected_len, expected_values);
+   list_t *list2 = new_list(3, (list_element_t[]){ 1, 3, 4 });
+   size_t expected_length = 3;
+   list_element_t expected_elements[] = { 1, 3, 4 };
 
    list_t *actual = append_list(list1, list2);
-   check_lists_match(expected, actual);
-   check_list_contents(expected_values, actual->values, expected_len);
+   check_lists_match(expected_length, expected_elements, actual);
 
    delete_list(list1);
    delete_list(list2);
-   delete_list(expected);
    delete_list(actual);
 }
 
 static void test_append_empty_list_to_list(void)
 {
    TEST_IGNORE();
-   list_t *list1 = new_list(4, (list_value_t[]){ 1, 2, 3, 4 });
+   list_t *list1 = new_list(4, (list_element_t[]){ 1, 2, 3, 4 });
    list_t *list2 = new_list(0, NULL);
-   size_t expected_len = 4;
-   list_value_t expected_values[] = { 1, 2, 3, 4 };
-   list_t *expected = new_list(expected_len, expected_values);
+   size_t expected_length = 4;
+   list_element_t expected_elements[] = { 1, 2, 3, 4 };
 
    list_t *actual = append_list(list1, list2);
-   check_lists_match(expected, actual);
-   check_list_contents(expected_values, actual->values, expected_len);
+   check_lists_match(expected_length, expected_elements, actual);
 
    delete_list(list1);
    delete_list(list2);
-   delete_list(expected);
    delete_list(actual);
 }
 
 static void test_append_non_empty_lists(void)
 {
    TEST_IGNORE();
-   list_t *list1 = new_list(2, (list_value_t[]){ 1, 2 });
-   list_t *list2 = new_list(4, (list_value_t[]){ 2, 3, 4, 5 });
-   size_t expected_len = 6;
-   list_value_t expected_values[] = { 1, 2, 2, 3, 4, 5 };
-   list_t *expected = new_list(expected_len, expected_values);
+   list_t *list1 = new_list(2, (list_element_t[]){ 1, 2 });
+   list_t *list2 = new_list(4, (list_element_t[]){ 2, 3, 4, 5 });
+   size_t expected_length = 6;
+   list_element_t expected_elements[] = { 1, 2, 2, 3, 4, 5 };
 
    list_t *actual = append_list(list1, list2);
-   check_lists_match(expected, actual);
-   check_list_contents(expected_values, actual->values, expected_len);
+   check_lists_match(expected_length, expected_elements, actual);
 
    delete_list(list1);
    delete_list(list2);
-   delete_list(expected);
    delete_list(actual);
 }
 
@@ -166,30 +157,25 @@ static void test_filter_empty_list(void)
 {
    TEST_IGNORE();
    list_t *list = new_list(0, NULL);
-   list_t *expected = new_list(0, NULL);
 
    list_t *actual = filter_list(list, filter_modulo);
-   check_lists_match(expected, actual);
+   check_lists_match(0, NULL, actual);
 
    delete_list(list);
-   delete_list(expected);
    delete_list(actual);
 }
 
 static void test_filter_non_empty_list(void)
 {
    TEST_IGNORE();
-   list_t *list = new_list(5, (list_value_t[]){ 1, 2, 3, 4, 5 });
-   size_t expected_len = 3;
-   list_value_t expected_values[] = { 1, 3, 5 };
-   list_t *expected = new_list(expected_len, expected_values);
+   list_t *list = new_list(5, (list_element_t[]){ 1, 2, 3, 4, 5 });
+   size_t expected_length = 3;
+   list_element_t expected_elements[] = { 1, 3, 5 };
 
    list_t *actual = filter_list(list, filter_modulo);
-   check_lists_match(expected, actual);
-   check_list_contents(expected_values, actual->values, expected_len);
+   check_lists_match(expected_length, expected_elements, actual);
 
    delete_list(list);
-   delete_list(expected);
    delete_list(actual);
 }
 
@@ -208,7 +194,7 @@ static void test_length_empty_list(void)
 static void test_length_non_empty_list(void)
 {
    TEST_IGNORE();
-   list_t *list = new_list(4, (list_value_t[]){ 1, 2, 3, 4 });
+   list_t *list = new_list(4, (list_element_t[]){ 1, 2, 3, 4 });
    size_t expected = 4;
 
    size_t actual = length_list(list);
@@ -221,13 +207,11 @@ static void test_map_empty_list(void)
 {
    TEST_IGNORE();
    list_t *list = new_list(0, NULL);
-   list_t *expected = new_list(0, NULL);
 
    list_t *actual = map_list(list, map_increment);
-   check_lists_match(expected, actual);
+   check_lists_match(0, NULL, actual);
 
    delete_list(list);
-   delete_list(expected);
    delete_list(actual);
 }
 
@@ -235,17 +219,14 @@ static void test_map_non_empty_list(void)
 {
 
    TEST_IGNORE();
-   list_t *list = new_list(4, (list_value_t[]){ 1, 3, 5, 7 });
-   size_t expected_len = 4;
-   list_value_t expected_values[] = { 2, 4, 6, 8 };
-   list_t *expected = new_list(expected_len, expected_values);
+   list_t *list = new_list(4, (list_element_t[]){ 1, 3, 5, 7 });
+   size_t expected_length = 4;
+   list_element_t expected_elements[] = { 2, 4, 6, 8 };
 
    list_t *actual = map_list(list, map_increment);
-   check_lists_match(expected, actual);
-   check_list_contents(expected_values, actual->values, expected_len);
+   check_lists_match(expected_length, expected_elements, actual);
 
    delete_list(list);
-   delete_list(expected);
    delete_list(actual);
 }
 
@@ -253,36 +234,38 @@ static void test_foldl_empty_list(void)
 {
    TEST_IGNORE();
    list_t *list = new_list(0, NULL);
-   list_value_t initial = 2;
-   list_value_t expected = 2;
+   list_element_t initial = 2;
+   list_element_t expected = 2;
 
-   list_value_t actual = foldl_list(list, initial, fold_multiply);
+   list_element_t actual = foldl_list(list, initial, fold_divide);
    TEST_ASSERT_EQUAL(expected, actual);
 
    delete_list(list);
 }
 
-static void test_foldl_direction_independent(void)
+static void
+test_foldl_direction_independent_function_applied_to_non_empty_list(void)
 {
    TEST_IGNORE();
-   list_t *list = new_list(4, (list_value_t[]){ 1, 2, 3, 4 });
-   list_value_t initial = 5;
-   list_value_t expected = 15;
+   list_t *list = new_list(4, (list_element_t[]){ 1, 2, 3, 4 });
+   list_element_t initial = 5;
+   list_element_t expected = 15;
 
-   list_value_t actual = foldl_list(list, initial, fold_add);
+   list_element_t actual = foldl_list(list, initial, fold_add);
    TEST_ASSERT_EQUAL(expected, actual);
 
    delete_list(list);
 }
 
-static void test_foldl_direction_dependent(void)
+static void
+test_foldl_direction_dependent_function_applied_to_non_empty_list(void)
 {
    TEST_IGNORE();
-   list_t *list = new_list(2, (list_value_t[]){ 2, 5 });
-   list_value_t initial = 5;
-   list_value_t expected = 0;
+   list_t *list = new_list(2, (list_element_t[]){ 2, 5 });
+   list_element_t initial = 5;
+   list_element_t expected = 0;
 
-   list_value_t actual = foldl_list(list, initial, fold_divide);
+   list_element_t actual = foldl_list(list, initial, fold_divide);
    TEST_ASSERT_EQUAL(expected, actual);
 
    delete_list(list);
@@ -292,36 +275,38 @@ static void test_foldr_empty_list(void)
 {
    TEST_IGNORE();
    list_t *list = new_list(0, NULL);
-   list_value_t initial = 2;
-   list_value_t expected = 2;
+   list_element_t initial = 2;
+   list_element_t expected = 2;
 
-   list_value_t actual = foldr_list(list, initial, fold_multiply);
+   list_element_t actual = foldr_list(list, initial, fold_multiply);
    TEST_ASSERT_EQUAL(expected, actual);
 
    delete_list(list);
 }
 
-static void test_foldr_direction_independent(void)
+static void
+test_foldr_direction_independent_function_applied_to_non_empty_list(void)
 {
    TEST_IGNORE();
-   list_t *list = new_list(4, (list_value_t[]){ 1, 2, 3, 4 });
-   list_value_t initial = 5;
-   list_value_t expected = 15;
+   list_t *list = new_list(4, (list_element_t[]){ 1, 2, 3, 4 });
+   list_element_t initial = 5;
+   list_element_t expected = 15;
 
-   list_value_t actual = foldr_list(list, initial, fold_add);
+   list_element_t actual = foldr_list(list, initial, fold_add);
    TEST_ASSERT_EQUAL(expected, actual);
 
    delete_list(list);
 }
 
-static void test_foldr_direction_dependent(void)
+static void
+test_foldr_direction_dependent_function_applied_to_non_empty_list(void)
 {
    TEST_IGNORE();
-   list_t *list = new_list(2, (list_value_t[]){ 2, 5 });
-   list_value_t initial = 5;
-   list_value_t expected = 2;
+   list_t *list = new_list(2, (list_element_t[]){ 2, 5 });
+   list_element_t initial = 5;
+   list_element_t expected = 2;
 
-   list_value_t actual = foldr_list(list, initial, fold_divide);
+   list_element_t actual = foldr_list(list, initial, fold_divide);
    TEST_ASSERT_EQUAL(expected, actual);
 
    delete_list(list);
@@ -331,30 +316,25 @@ static void test_reverse_empty_list(void)
 {
    TEST_IGNORE();
    list_t *list = new_list(0, NULL);
-   list_t *expected = new_list(0, NULL);
 
    list_t *actual = reverse_list(list);
-   check_lists_match(expected, actual);
+   check_lists_match(0, NULL, actual);
 
    delete_list(list);
-   delete_list(expected);
    delete_list(actual);
 }
 
 static void test_reverse_non_empty_list(void)
 {
    TEST_IGNORE();
-   list_t *list = new_list(4, (list_value_t[]){ 1, 3, 5, 7 });
-   size_t expected_len = 4;
-   list_value_t expected_values[] = { 7, 5, 3, 1 };
-   list_t *expected = new_list(expected_len, expected_values);
+   list_t *list = new_list(4, (list_element_t[]){ 1, 3, 5, 7 });
+   size_t expected_length = 4;
+   list_element_t expected_elements[] = { 7, 5, 3, 1 };
 
    list_t *actual = reverse_list(list);
-   check_lists_match(expected, actual);
-   check_list_contents(expected_values, actual->values, expected_len);
+   check_lists_match(expected_length, expected_elements, actual);
 
    delete_list(list);
-   delete_list(expected);
    delete_list(actual);
 }
 
@@ -373,11 +353,13 @@ int main(void)
    RUN_TEST(test_map_empty_list);
    RUN_TEST(test_map_non_empty_list);
    RUN_TEST(test_foldl_empty_list);
-   RUN_TEST(test_foldl_direction_independent);
-   RUN_TEST(test_foldl_direction_dependent);
+   RUN_TEST
+       (test_foldl_direction_independent_function_applied_to_non_empty_list);
+   RUN_TEST(test_foldl_direction_dependent_function_applied_to_non_empty_list);
    RUN_TEST(test_foldr_empty_list);
-   RUN_TEST(test_foldr_direction_independent);
-   RUN_TEST(test_foldr_direction_dependent);
+   RUN_TEST
+       (test_foldr_direction_independent_function_applied_to_non_empty_list);
+   RUN_TEST(test_foldr_direction_dependent_function_applied_to_non_empty_list);
    RUN_TEST(test_reverse_empty_list);
    RUN_TEST(test_reverse_non_empty_list);
 
