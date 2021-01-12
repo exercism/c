74e48089576021977a3f4ae7becfb3eafc47827b
wolf99 <281700+wolf99@users.noreply.github.com>
2021-01-12 02:26:34 +0000

    luhn: align with specification per #510 (#561)

--- exercises/luhn/test/test_luhn.c
+++ exercises/luhn/test/test_luhn.c
@@ -50,10 +50,29 @@ static void test_invalid_credit_card(void)
    TEST_ASSERT_FALSE(luhn("8273 1232 7352 0569"));
 }
 
-static void test_valid_strings_with_a_non_digit_included_become_invalid(void)
+static void test_invalid_long_number_with_an_even_remainder(void)
 {
    TEST_IGNORE();
-   TEST_ASSERT_FALSE(luhn("055a 444 285"));
+   TEST_ASSERT_FALSE(luhn("1 2345 6789 1234 5678 9012"));
+}
+
+static void test_valid_number_with_an_even_number_of_digits(void)
+{
+   TEST_IGNORE();
+   TEST_ASSERT_TRUE(luhn("095 245 88"));
+}
+
+static void test_valid_number_with_an_odd_number_of_spaces(void)
+{
+   TEST_IGNORE();
+   TEST_ASSERT_TRUE(luhn("234 567 891 234"));
+}
+
+static void
+test_valid_string_with_a_non_digit_added_at_the_end_to_become_invalid(void)
+{
+   TEST_IGNORE();
+   TEST_ASSERT_FALSE(luhn("059a"));
 }
 
 static void test_valid_strings_with_punctuation_included_become_invalid(void)
@@ -86,6 +105,18 @@ static void test_input_digit_9_is_correctly_converted_to_output_digit_9(void)
    TEST_ASSERT_TRUE(luhn("091"));
 }
 
+static void test_ascii_value_for_non_doubled_non_digit_isnt_allowed(void)
+{
+   TEST_IGNORE();
+   TEST_ASSERT_FALSE(luhn("055b 444 285"));
+}
+
+static void test_ascii_value_for_doubled_non_digit_isnt_allowed(void)
+{
+   TEST_IGNORE();
+   TEST_ASSERT_FALSE(luhn(":9"));
+}
+
 int main(void)
 {
    UnityBegin("test/test_luhn.c");
@@ -97,12 +128,18 @@ int main(void)
    RUN_TEST(test_a_valid_Canadian_SIN);
    RUN_TEST(test_invalid_Canadian_SIN);
    RUN_TEST(test_invalid_credit_card);
-   RUN_TEST(test_valid_strings_with_a_non_digit_included_become_invalid);
+   RUN_TEST(test_invalid_long_number_with_an_even_remainder);
+   RUN_TEST(test_valid_number_with_an_even_number_of_digits);
+   RUN_TEST(test_valid_number_with_an_odd_number_of_spaces);
+   RUN_TEST
+       (test_valid_string_with_a_non_digit_added_at_the_end_to_become_invalid);
    RUN_TEST(test_valid_strings_with_punctuation_included_become_invalid);
    RUN_TEST(test_valid_strings_with_symbols_included_become_invalid);
    RUN_TEST(test_single_zero_with_space_is_invalid);
    RUN_TEST(test_more_than_a_single_zero_is_valid);
    RUN_TEST(test_input_digit_9_is_correctly_converted_to_output_digit_9);
+   RUN_TEST(test_ascii_value_for_non_doubled_non_digit_isnt_allowed);
+   RUN_TEST(test_ascii_value_for_doubled_non_digit_isnt_allowed);
 
    return UnityEnd();
 }
