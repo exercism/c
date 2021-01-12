b1dee340144f07bfac2252c5d69c02ec4bcb17aa
wolf99 <281700+wolf99@users.noreply.github.com>
2021-01-12 21:28:47 +0000

    Align names with track conventions

--- exercises/pascals-triangle/test/test_pascals_triangle.c
+++ exercises/pascals-triangle/test/test_pascals_triangle.c
@@ -10,25 +10,23 @@ void tearDown(void)
 {
 }
 
-static bool check(size_t count, size_t expected[][count], size_t ** result)
+static bool check_triangle(size_t count, size_t expected[][count],
+                           size_t **actual)
 {
-   size_t i, j;
-   for (i = 0; i < count; i++) {
-      for (j = 0; j <= i; j++) {
-         if (expected[i][j] != result[i][j]) {
-            return 0;
-         }
-      }
-   }
-   return 1;
+   for (size_t i = 0; i < count; i++)
+      for (size_t j = 0; j <= i; j++)
+         if (expected[i][j] != actual[i][j])
+            return false;
+
+   return true;
 }
 
 static void test_no_rows(void)
 {
    size_t expected[1][1] = { {0} };
-   size_t **r = create_triangle(0);
-   TEST_ASSERT_TRUE(check(1, expected, r));
-   free_triangle(r, 1);
+   size_t **actual = create_triangle(0);
+   TEST_ASSERT_TRUE(check_triangle(1, expected, actual));
+   free_triangle(actual, 1);
 }
 
 static void test_single_row(void)
@@ -37,9 +35,9 @@ static void test_single_row(void)
    size_t expected[1][1] = {
       {1}
    };
-   size_t **r = create_triangle(1);
-   TEST_ASSERT_TRUE(check(1, expected, r));
-   free_triangle(r, 1);
+   size_t **actual = create_triangle(1);
+   TEST_ASSERT_TRUE(check_triangle(1, expected, actual));
+   free_triangle(actual, 1);
 }
 
 static void test_two_rows(void)
@@ -49,9 +47,9 @@ static void test_two_rows(void)
       {1, 0},
       {1, 1}
    };
-   size_t **r = create_triangle(2);
-   TEST_ASSERT_TRUE(check(2, expected, r));
-   free_triangle(r, 2);
+   size_t **actual = create_triangle(2);
+   TEST_ASSERT_TRUE(check_triangle(2, expected, actual));
+   free_triangle(actual, 2);
 }
 
 static void test_three_rows(void)
@@ -62,9 +60,9 @@ static void test_three_rows(void)
       {1, 1, 0},
       {1, 2, 1}
    };
-   size_t **r = create_triangle(3);
-   TEST_ASSERT_TRUE(check(3, expected, r));
-   free_triangle(r, 3);
+   size_t **actual = create_triangle(3);
+   TEST_ASSERT_TRUE(check_triangle(3, expected, actual));
+   free_triangle(actual, 3);
 }
 
 static void test_four_rows(void)
@@ -76,9 +74,9 @@ static void test_four_rows(void)
       {1, 2, 1, 0},
       {1, 3, 3, 1}
    };
-   size_t **r = create_triangle(4);
-   TEST_ASSERT_TRUE(check(4, expected, r));
-   free_triangle(r, 4);
+   size_t **actual = create_triangle(4);
+   TEST_ASSERT_TRUE(check_triangle(4, expected, actual));
+   free_triangle(actual, 4);
 }
 
 static void test_five_rows(void)
@@ -91,45 +89,45 @@ static void test_five_rows(void)
       {1, 3, 3, 1, 0},
       {1, 4, 6, 4, 1}
    };
-   size_t **r = create_triangle(5);
-   TEST_ASSERT_TRUE(check(5, expected, r));
-   free_triangle(r, 5);
+   size_t **actual = create_triangle(5);
+   TEST_ASSERT_TRUE(check_triangle(5, expected, actual));
+   free_triangle(actual, 5);
 }
 
 static void test_six_rows(void)
 {
    TEST_IGNORE();
    size_t expected[6][6] = {
-      {1, 0,  0,  0, 0, 0},
-      {1, 1,  0,  0, 0, 0},
-      {1, 2,  1,  0, 0, 0},
-      {1, 3,  3,  1, 0, 0},
-      {1, 4,  6,  4, 1, 0},
+      {1, 0, 0, 0, 0, 0},
+      {1, 1, 0, 0, 0, 0},
+      {1, 2, 1, 0, 0, 0},
+      {1, 3, 3, 1, 0, 0},
+      {1, 4, 6, 4, 1, 0},
       {1, 5, 10, 10, 5, 1},
    };
-   size_t **r = create_triangle(6);
-   TEST_ASSERT_TRUE(check(6, expected, r));
-   free_triangle(r, 6);
+   size_t **actual = create_triangle(6);
+   TEST_ASSERT_TRUE(check_triangle(6, expected, actual));
+   free_triangle(actual, 6);
 }
 
 static void test_ten_rows(void)
 {
    TEST_IGNORE();
    size_t expected[10][10] = {
-      {1, 0,  0,  0,   0,   0,  0,  0, 0, 0},
-      {1, 1,  0,  0,   0,   0,  0,  0, 0, 0},
-      {1, 2,  1,  0,   0,   0,  0,  0, 0, 0},
-      {1, 3,  3,  1,   0,   0,  0,  0, 0, 0},
-      {1, 4,  6,  4,   1,   0,  0,  0, 0, 0},
-      {1, 5, 10, 10,   5,   1,  0,  0, 0, 0},
-      {1, 6, 15, 20,  15,   6,  1,  0, 0, 0},
-      {1, 7, 21, 35,  35,  21,  7,  1, 0, 0},
-      {1, 8, 28, 56,  70,  56, 28,  8, 1, 0},
+      {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+      {1, 2, 1, 0, 0, 0, 0, 0, 0, 0},
+      {1, 3, 3, 1, 0, 0, 0, 0, 0, 0},
+      {1, 4, 6, 4, 1, 0, 0, 0, 0, 0},
+      {1, 5, 10, 10, 5, 1, 0, 0, 0, 0},
+      {1, 6, 15, 20, 15, 6, 1, 0, 0, 0},
+      {1, 7, 21, 35, 35, 21, 7, 1, 0, 0},
+      {1, 8, 28, 56, 70, 56, 28, 8, 1, 0},
       {1, 9, 36, 84, 126, 126, 84, 36, 9, 1}
    };
-   size_t **r = create_triangle(10);
-   TEST_ASSERT_TRUE(check(10, expected, r));
-   free_triangle(r, 10);
+   size_t **actual = create_triangle(10);
+   TEST_ASSERT_TRUE(check_triangle(10, expected, actual));
+   free_triangle(actual, 10);
 }
 
 int main(void)
