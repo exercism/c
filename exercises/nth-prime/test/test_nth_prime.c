587e49728d2762dbaa43ad636a747b3372041027
wolf99 <281700+wolf99@users.noreply.github.com>
2021-01-12 20:57:18 +0000

    nth-prime: align with specification per #513

--- exercises/nth-prime/test/test_nth_prime.c
+++ exercises/nth-prime/test/test_nth_prime.c
@@ -26,13 +26,13 @@ static void test_sixth_prime(void)
    TEST_ASSERT_EQUAL_UINT32(13, nth(6));
 }
 
-static void test_large_prime(void)
+static void test_big_prime(void)
 {
    TEST_IGNORE();
    TEST_ASSERT_EQUAL_UINT32(104743, nth(10001));
 }
 
-static void test_weird_case(void)
+static void test_there_is_no_zeroth_prime(void)
 {
    TEST_IGNORE();
    TEST_ASSERT_EQUAL_UINT32(0, nth(0));
@@ -45,8 +45,8 @@ int main(void)
    RUN_TEST(test_first_prime);
    RUN_TEST(test_second_prime);
    RUN_TEST(test_sixth_prime);
-   RUN_TEST(test_large_prime);
-   RUN_TEST(test_weird_case);
+   RUN_TEST(test_big_prime);
+   RUN_TEST(test_there_is_no_zeroth_prime);
 
    return UnityEnd();
 }
