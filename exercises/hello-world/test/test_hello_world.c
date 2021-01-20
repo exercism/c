c8a67b2abfd1b565176e2dbd81e58288acc73b1b
wolf99 <281700+wolf99@users.noreply.github.com>
2021-01-20 20:09:41 +0000

    hello-world: align with specification per #505

--- exercises/hello-world/test/test_hello_world.c
+++ exercises/hello-world/test/test_hello_world.c
@@ -15,7 +15,7 @@ void tearDown(void)
 }
 
 // Defines a single test.
-static void test_hello(void)
+static void test_say_hi(void)
 {
    // Check if the 'hello()' function returns "Hello, World!"
    // This test is expected to fail after first downloading this exercise.
