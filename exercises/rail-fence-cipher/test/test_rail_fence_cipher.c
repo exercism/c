624b903c8acbac67546a36da9af0a5306308d8fb
wolf99 <281700+wolf99@users.noreply.github.com>
2021-01-20 23:12:01 +0000

    rail-fence-cipher: align with specification per #519 (#568)
    - Refactor remove helper function that has only one line
    
    - Match `msg` argument name to spec

--- exercises/rail-fence-cipher/test/test_rail_fence_cipher.c
+++ exercises/rail-fence-cipher/test/test_rail_fence_cipher.c
@@ -10,113 +10,67 @@ void tearDown(void)
 {
 }
 
-static void test_expected_value(char *actual, char *expected)
-{
-   TEST_ASSERT_EQUAL_STRING(actual, expected);
-}
-
-static void test_encode_with_empty_string(void)
-{
-   char *res = encode("", 4);
-   test_expected_value("", res);
-   free(res);
-}
-
-static void test_encode_with_one_rail(void)
-{
-   TEST_IGNORE();
-   char *res = encode("One rail, only one rail", 1);
-   test_expected_value("One rail, only one rail", res);
-   free(res);
-}
-
 static void test_encode_with_two_rails(void)
 {
    TEST_IGNORE();
-   char *res = encode("XOXOXOXOXOXOXOXOXO", 2);
-   test_expected_value("XXXXXXXXXOOOOOOOOO", res);
-   free(res);
+   char *actual = encode("XOXOXOXOXOXOXOXOXO", 2);
+   TEST_ASSERT_EQUAL_STRING("XXXXXXXXXOOOOOOOOO", actual);
+   free(actual);
 }
 
 static void test_encode_with_three_rails(void)
 {
    TEST_IGNORE();
-   char *res = encode("WEAREDISCOVEREDFLEEATONCE", 3);
-   test_expected_value("WECRLTEERDSOEEFEAOCAIVDEN", res);
-   free(res);
+   char *actual = encode("WEAREDISCOVEREDFLEEATONCE", 3);
+   TEST_ASSERT_EQUAL_STRING("WECRLTEERDSOEEFEAOCAIVDEN", actual);
+   free(actual);
 }
 
 static void test_encode_with_ending_in_the_middle(void)
 {
    TEST_IGNORE();
-   char *res = encode("EXERCISES", 4);
-   test_expected_value("ESXIEECSR", res);
-   free(res);
-}
-
-static void test_encode_with_less_letters_than_rails(void)
-{
-   TEST_IGNORE();
-   char *res = encode("More rails than letters", 24);
-   test_expected_value("More rails than letters", res);
-   free(res);
-}
-
-static void test_decode_with_empty_string(void)
-{
-   TEST_IGNORE();
-   char *res = decode("", 4);
-   test_expected_value("", res);
-   free(res);
-}
-
-static void test_decode_with_one_rail(void)
-{
-   TEST_IGNORE();
-   char *res = decode("ABCDEFGHIJKLMNOP", 1);
-   test_expected_value("ABCDEFGHIJKLMNOP", res);
-   free(res);
-}
-
-static void test_decode_with_two_rails(void)
-{
-   TEST_IGNORE();
-   char *res = decode("XXXXXXXXXOOOOOOOOO", 2);
-   test_expected_value("XOXOXOXOXOXOXOXOXO", res);
-   free(res);
+   char *actual = encode("EXERCISES", 4);
+   TEST_ASSERT_EQUAL_STRING("ESXIEECSR", actual);
+   free(actual);
 }
 
 static void test_decode_with_three_rails(void)
 {
    TEST_IGNORE();
-   char *res = decode("TEITELHDVLSNHDTISEIIEA", 3);
-   test_expected_value("THEDEVILISINTHEDETAILS", res);
-   free(res);
+   char *actual = decode("TEITELHDVLSNHDTISEIIEA", 3);
+   TEST_ASSERT_EQUAL_STRING("THEDEVILISINTHEDETAILS", actual);
+   free(actual);
 }
 
-static void test_decode_with_four_rails(void)
+static void test_decode_with_five_rails(void)
 {
    TEST_IGNORE();
-   char *res = decode("TSENEHINVRAYREEOONRWERGOH", 4);
-   test_expected_value("THEREISNOGOVERNORANYWHERE", res);
-   free(res);
+   char *actual = decode("EIEXMSMESAORIWSCE", 5);
+   TEST_ASSERT_EQUAL_STRING("EXERCISMISAWESOME", actual);
+   free(actual);
+}
+
+static void test_decode_with_six_rails(void)
+{
+   TEST_IGNORE();
+   char *actual =
+       decode("133714114238148966225439541018335470986172518171757571896261",
+              6);
+   TEST_ASSERT_EQUAL_STRING
+       ("112358132134558914423337761098715972584418167651094617711286", actual);
+   free(actual);
 }
 
 int main(void)
 {
    UnityBegin("test/test_rail_fence_cipher.c");
 
-   RUN_TEST(test_encode_with_empty_string);
-   RUN_TEST(test_encode_with_one_rail);
    RUN_TEST(test_encode_with_two_rails);
    RUN_TEST(test_encode_with_three_rails);
    RUN_TEST(test_encode_with_ending_in_the_middle);
-   RUN_TEST(test_encode_with_less_letters_than_rails);
-   RUN_TEST(test_decode_with_empty_string);
-   RUN_TEST(test_decode_with_one_rail);
-   RUN_TEST(test_decode_with_two_rails);
    RUN_TEST(test_decode_with_three_rails);
-   RUN_TEST(test_decode_with_four_rails);
+   RUN_TEST(test_decode_with_five_rails);
+   RUN_TEST(test_decode_with_six_rails);
 
    return UnityEnd();
 }
