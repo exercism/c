0915cf4445e98ec6e585f2625614cd089e556c9a
wolf99 <281700+wolf99@users.noreply.github.com>
2021-01-20 01:53:51 +0000

    pascals-triangle: align with specification per #516 (#565)
    * pascals-triangle: align with specification per #516
    
    * Align names with track conventions
    
    * [CI] Format C code
    
    * Trigger build
    
    * Refactor types, names, add error-checking
    
    * Check for NULL in test helper function
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- exercises/pascals-triangle/src/example.h
+++ exercises/pascals-triangle/src/example.h
@@ -1,8 +1,10 @@
 #ifndef PASCALS_TRIANGLE_H
 #define PASCALS_TRIANGLE_H
-#include <stdlib.h>
 
-void free_triangle(size_t **, size_t);
-size_t **create_triangle(int);
+#include <stddef.h>
+#include <stdint.h>
+
+void free_triangle(uint8_t ** triangle, size_t rows);
+uint8_t **create_triangle(size_t rows);
 
 #endif
