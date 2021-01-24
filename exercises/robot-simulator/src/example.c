ad5f1c4eab7fc25b73b8416425ec684d6182dc26
wolf99 <281700+wolf99@users.noreply.github.com>
2021-01-24 19:16:13 +0000

    Robot simulator update (#574)
    * roman-numerals: align with specification per #523
    
    * Simplify test helper function
    
    * Simplify interface, match names to spec
    
    * Run indent

--- exercises/robot-simulator/src/example.c
+++ exercises/robot-simulator/src/example.c
@@ -1,49 +1,49 @@
 #include <string.h>
 #include "robot_simulator.h"
 
-robot_grid_status_t robot_init(void)
-{
-   return (robot_init_with_position
-           (DEFAULT_BEARING, DEFAULT_X_COORDINATE, DEFAULT_Y_COORDINATE));
-}
+enum {
+   INSTRUCTION_LEFT = 'L',
+   INSTRUCTION_RIGHT = 'R',
+   INSTRUCTION_ADVANCE = 'A'
+};
 
-robot_grid_status_t robot_init_with_position(bearing_t bearing, int x, int y)
+robot_status_t robot_create(robot_direction_t direction, int x, int y)
 {
-   robot_grid_status_t robot = { bearing, {x, y} };
+   robot_status_t robot = { direction, {x, y} };
 
-   if ((bearing < HEADING_NORTH) || (bearing >= HEADING_MAX)) {
-      robot.bearing = DEFAULT_BEARING;
-   }
+   if ((direction < DIRECTION_NORTH) || (direction >= DIRECTION_MAX))
+      robot.direction = DIRECTION_DEFAULT;
+
    return robot;
 }
 
-void robot_turn_right(robot_grid_status_t * robot)
+static void robot_turn_right(robot_status_t * robot)
 {
-   robot->bearing = (robot->bearing + 1) % HEADING_MAX;
+   robot->direction = (robot->direction + 1) % DIRECTION_MAX;
 }
 
-void robot_turn_left(robot_grid_status_t * robot)
+static void robot_turn_left(robot_status_t * robot)
 {
-   robot->bearing = ((robot->bearing - 1) + HEADING_MAX) % HEADING_MAX;
+   robot->direction = ((robot->direction - 1) + DIRECTION_MAX) % DIRECTION_MAX;
 }
 
-void robot_advance(robot_grid_status_t * robot)
+static void robot_advance(robot_status_t * robot)
 {
-   switch (robot->bearing) {
-   case HEADING_NORTH:
-      robot->grid.y_position++;
+   switch (robot->direction) {
+   case DIRECTION_NORTH:
+      robot->position.y++;
       break;
 
-   case HEADING_EAST:
-      robot->grid.x_position++;
+   case DIRECTION_EAST:
+      robot->position.x++;
       break;
 
-   case HEADING_SOUTH:
-      robot->grid.y_position--;
+   case DIRECTION_SOUTH:
+      robot->position.y--;
       break;
 
-   case HEADING_WEST:
-      robot->grid.x_position--;
+   case DIRECTION_WEST:
+      robot->position.x--;
       break;
 
    default:
@@ -51,19 +51,19 @@ void robot_advance(robot_grid_status_t * robot)
    }
 }
 
-void robot_simulator(robot_grid_status_t * robot, const char *commands)
+void robot_move(robot_status_t * robot, const char *instructions)
 {
-   for (unsigned long index = 0; index < strlen(commands); index++) {
-      switch (commands[index]) {
-      case COMMAND_LEFT:
+   for (unsigned long index = 0; index < strlen(instructions); index++) {
+      switch (instructions[index]) {
+      case INSTRUCTION_LEFT:
          robot_turn_left(robot);
          break;
 
-      case COMMAND_RIGHT:
+      case INSTRUCTION_RIGHT:
          robot_turn_right(robot);
          break;
 
-      case COMMAND_ADVANCE:
+      case INSTRUCTION_ADVANCE:
          robot_advance(robot);
          break;
 
--- exercises/robot-simulator/test/test_robot_simulator.c
+++ exercises/robot-simulator/test/test_robot_simulator.c
@@ -9,175 +9,232 @@ void tearDown(void)
 {
 }
 
-// Test Helper Function
-static void confirm_position(robot_grid_status_t * expected,
-                             robot_grid_status_t * actual)
+static void check_position(robot_status_t expected, robot_status_t actual)
 {
-   TEST_ASSERT_EQUAL(expected->bearing, actual->bearing);
-   TEST_ASSERT_EQUAL(expected->grid.x_position, actual->grid.x_position);
-   TEST_ASSERT_EQUAL(expected->grid.y_position, actual->grid.y_position);
+   TEST_ASSERT_EQUAL(expected.direction, actual.direction);
+   TEST_ASSERT_EQUAL(expected.position.x, actual.position.x);
+   TEST_ASSERT_EQUAL(expected.position.y, actual.position.y);
 }
 
-// Tests...
-static void test_init(void)
+static void test_create_at_origin_facing_north(void)
 {
-   robot_grid_status_t expected =
-       { DEFAULT_BEARING, {DEFAULT_X_COORDINATE, DEFAULT_Y_COORDINATE} };
-   robot_grid_status_t actual = robot_init();
+   robot_status_t expected = { DIRECTION_NORTH, {0, 0} };
 
-   confirm_position(&expected, &actual);
+   robot_status_t actual = robot_create(DIRECTION_NORTH, 0, 0);
+
+   check_position(expected, actual);
 }
 
-static void test_invalid_initial_heading(void)
+static void test_create_at_negative_position_facing_south(void)
 {
    TEST_IGNORE();               // delete this line to run test
-   robot_grid_status_t expected =
-       { DEFAULT_BEARING, {DEFAULT_X_COORDINATE, DEFAULT_Y_COORDINATE} };
-   robot_grid_status_t actual =
-       robot_init_with_position(99, DEFAULT_X_COORDINATE, DEFAULT_Y_COORDINATE);
+   robot_status_t expected = { DIRECTION_SOUTH, {-1, -1} };
 
-   confirm_position(&expected, &actual);
+   robot_status_t actual = robot_create(DIRECTION_SOUTH, -1, -1);
+
+   check_position(expected, actual);
 }
 
-static void test_init_with_negative_positions(void)
+static void test_rotating_clockwise_changes_north_to_east(void)
 {
    TEST_IGNORE();
-   robot_grid_status_t expected = { HEADING_SOUTH, {-1, -1} };
-   robot_grid_status_t actual = robot_init_with_position(HEADING_SOUTH, -1, -1);
+   robot_status_t expected = { DIRECTION_EAST, {0, 0} };
 
-   confirm_position(&expected, &actual);
+   robot_status_t actual = { DIRECTION_NORTH, {0, 0} };
+   robot_move(&actual, "R");
+
+   check_position(expected, actual);
 }
 
-static void test_turn_right(void)
+static void test_rotating_clockwise_changes_east_to_south(void)
 {
    TEST_IGNORE();
-   robot_grid_status_t expected = { HEADING_EAST, {0, 0} };
-   robot_grid_status_t actual = robot_init();
+   robot_status_t expected = { DIRECTION_SOUTH, {0, 0} };
 
-   robot_turn_right(&actual);
-   confirm_position(&expected, &actual);
+   robot_status_t actual = { DIRECTION_EAST, {0, 0} };
+   robot_move(&actual, "R");
 
-   expected.bearing = HEADING_SOUTH;
-   robot_turn_right(&actual);
-   confirm_position(&expected, &actual);
-
-   expected.bearing = HEADING_WEST;
-   robot_turn_right(&actual);
-   confirm_position(&expected, &actual);
-
-   expected.bearing = HEADING_NORTH;
-   robot_turn_right(&actual);
-   confirm_position(&expected, &actual);
+   check_position(expected, actual);
 }
 
-static void test_turn_left(void)
+static void test_rotating_clockwise_changes_south_to_west(void)
 {
    TEST_IGNORE();
-   robot_grid_status_t expected = { HEADING_WEST, {0, 0} };
-   robot_grid_status_t actual = robot_init();
+   robot_status_t expected = { DIRECTION_WEST, {0, 0} };
 
-   robot_turn_left(&actual);
-   confirm_position(&expected, &actual);
+   robot_status_t actual = { DIRECTION_SOUTH, {0, 0} };
+   robot_move(&actual, "R");
 
-   expected.bearing = HEADING_SOUTH;
-   robot_turn_left(&actual);
-   confirm_position(&expected, &actual);
-
-   expected.bearing = HEADING_EAST;
-   robot_turn_left(&actual);
-   confirm_position(&expected, &actual);
-
-   expected.bearing = HEADING_NORTH;
-   robot_turn_left(&actual);
-   confirm_position(&expected, &actual);
+   check_position(expected, actual);
 }
 
-static void test_advance_positive_north(void)
+static void test_rotating_clockwise_changes_west_to_north(void)
 {
    TEST_IGNORE();
-   robot_grid_status_t expected = { HEADING_NORTH, {0, 1} };
-   robot_grid_status_t actual = robot_init();
+   robot_status_t expected = { DIRECTION_NORTH, {0, 0} };
 
-   robot_advance(&actual);
-   confirm_position(&expected, &actual);
+   robot_status_t actual = { DIRECTION_WEST, {0, 0} };
+   robot_move(&actual, "R");
+
+   check_position(expected, actual);
 }
 
-static void test_advance_positive_east(void)
+static void test_rotating_counter_clockwise_changes_north_to_west(void)
 {
    TEST_IGNORE();
-   robot_grid_status_t expected = { HEADING_EAST, {1, 0} };
-   robot_grid_status_t actual = robot_init_with_position(HEADING_EAST, 0, 0);
+   robot_status_t expected = { DIRECTION_WEST, {0, 0} };
 
-   robot_advance(&actual);
-   confirm_position(&expected, &actual);
+   robot_status_t actual = { DIRECTION_NORTH, {0, 0} };
+   robot_move(&actual, "L");
+
+   check_position(expected, actual);
 }
 
-static void test_advance_negative_south(void)
+static void test_rotating_counter_clockwise_changes_west_to_south(void)
 {
    TEST_IGNORE();
-   robot_grid_status_t expected = { HEADING_SOUTH, {0, -1} };
-   robot_grid_status_t actual = robot_init_with_position(HEADING_SOUTH, 0, 0);
+   robot_status_t expected = { DIRECTION_SOUTH, {0, 0} };
 
-   robot_advance(&actual);
-   confirm_position(&expected, &actual);
+   robot_status_t actual = { DIRECTION_WEST, {0, 0} };
+   robot_move(&actual, "L");
+
+   check_position(expected, actual);
 }
 
-static void test_advance_negative_west(void)
+static void test_rotating_counter_clockwise_changes_south_to_east(void)
 {
    TEST_IGNORE();
-   robot_grid_status_t expected = { HEADING_WEST, {-1, 0} };
-   robot_grid_status_t actual = robot_init_with_position(HEADING_WEST, 0, 0);
+   robot_status_t expected = { DIRECTION_EAST, {0, 0} };
 
-   robot_advance(&actual);
-   confirm_position(&expected, &actual);
+   robot_status_t actual = { DIRECTION_SOUTH, {0, 0} };
+   robot_move(&actual, "L");
+
+   check_position(expected, actual);
 }
 
-static void test_simulate_move_west_and_north(void)
+static void test_rotating_counter_clockwise_changes_east_to_north(void)
 {
    TEST_IGNORE();
-   robot_grid_status_t expected = { HEADING_WEST, {-4, 1} };
-   robot_grid_status_t actual = robot_init();
+   robot_status_t expected = { DIRECTION_NORTH, {0, 0} };
 
-   robot_simulator(&actual, "LAAARALA");
-   confirm_position(&expected, &actual);
+   robot_status_t actual = { DIRECTION_EAST, {0, 0} };
+   robot_move(&actual, "L");
+
+   check_position(expected, actual);
 }
 
-static void test_simulate_move_west_and_south(void)
+static void test_moving_forward_one_facing_north_increments_Y(void)
 {
    TEST_IGNORE();
-   robot_grid_status_t expected = { HEADING_SOUTH, {-3, -8} };
-   robot_grid_status_t actual = robot_init_with_position(HEADING_EAST, 2, -7);
+   robot_status_t expected = { DIRECTION_NORTH, {0, 1} };
 
-   robot_simulator(&actual, "RRAAAAALA");
-   confirm_position(&expected, &actual);
+   robot_status_t actual = { DIRECTION_NORTH, {0, 0} };
+   robot_move(&actual, "A");
+
+   check_position(expected, actual);
 }
 
-static void test_simulate_move_east_and_north(void)
+static void test_moving_forward_one_facing_south_decrements_Y(void)
 {
    TEST_IGNORE();
-   robot_grid_status_t expected = { HEADING_NORTH, {11, 5} };
-   robot_grid_status_t actual = robot_init_with_position(HEADING_SOUTH, 8, 4);
+   robot_status_t expected = { DIRECTION_SOUTH, {0, -1} };
 
-   robot_simulator(&actual, "LAAARRRALLLL");
-   confirm_position(&expected, &actual);
+   robot_status_t actual = { DIRECTION_SOUTH, {0, 0} };
+   robot_move(&actual, "A");
+
+   check_position(expected, actual);
+}
+
+static void test_moving_forward_one_facing_east_increments_X(void)
+{
+   TEST_IGNORE();
+   robot_status_t expected = { DIRECTION_EAST, {1, 0} };
+
+   robot_status_t actual = { DIRECTION_EAST, {0, 0} };
+   robot_move(&actual, "A");
+
+   check_position(expected, actual);
+}
+
+static void test_moving_forward_one_facing_west_decrements_X(void)
+{
+   TEST_IGNORE();
+   robot_status_t expected = { DIRECTION_WEST, {-1, 0} };
+
+   robot_status_t actual = { DIRECTION_WEST, {0, 0} };
+   robot_move(&actual, "A");
+
+   check_position(expected, actual);
+}
+
+static void
+test_follow_series_of_instructions_moving_east_and_north_from_README(void)
+{
+   TEST_IGNORE();
+   robot_status_t expected = { DIRECTION_WEST, {9, 4} };
+
+   robot_status_t actual = { DIRECTION_NORTH, {7, 3} };
+   robot_move(&actual, "RAALAL");
+
+   check_position(expected, actual);
+}
+
+static void test_follow_series_of_instructions_moving_west_and_north(void)
+{
+   TEST_IGNORE();
+   robot_status_t expected = { DIRECTION_WEST, {-4, 1} };
+
+   robot_status_t actual = { DIRECTION_NORTH, {0, 0} };
+   robot_move(&actual, "LAAARALA");
+
+   check_position(expected, actual);
+}
+
+static void test_follow_series_of_instructions_moving_west_and_south(void)
+{
+   TEST_IGNORE();
+   robot_status_t expected = { DIRECTION_SOUTH, {-3, -8} };
+
+   robot_status_t actual = { DIRECTION_EAST, {2, -7} };
+   robot_move(&actual, "RRAAAAALA");
+
+   check_position(expected, actual);
+}
+
+static void test_follow_series_of_instructions_moving_east_and_north(void)
+{
+   TEST_IGNORE();
+   robot_status_t expected = { DIRECTION_NORTH, {11, 5} };
+
+   robot_status_t actual = { DIRECTION_SOUTH, {8, 4} };
+   robot_move(&actual, "LAAARRRALLLL");
+
+   check_position(expected, actual);
 }
 
 int main(void)
 {
    UnityBegin("test/test_robot_simulator.c");
 
-   RUN_TEST(test_init);
-   RUN_TEST(test_invalid_initial_heading);
-   RUN_TEST(test_init_with_negative_positions);
-   RUN_TEST(test_turn_right);
-   RUN_TEST(test_turn_left);
-   RUN_TEST(test_advance_positive_north);
-   RUN_TEST(test_advance_positive_east);
-   RUN_TEST(test_advance_negative_south);
-   RUN_TEST(test_advance_negative_west);
-   RUN_TEST(test_simulate_move_west_and_north);
-   RUN_TEST(test_simulate_move_west_and_south);
-   RUN_TEST(test_simulate_move_east_and_north);
+   RUN_TEST(test_create_at_origin_facing_north);
+   RUN_TEST(test_create_at_negative_position_facing_south);
+   RUN_TEST(test_rotating_clockwise_changes_north_to_east);
+   RUN_TEST(test_rotating_clockwise_changes_east_to_south);
+   RUN_TEST(test_rotating_clockwise_changes_south_to_west);
+   RUN_TEST(test_rotating_clockwise_changes_west_to_north);
+   RUN_TEST(test_rotating_counter_clockwise_changes_north_to_west);
+   RUN_TEST(test_rotating_counter_clockwise_changes_west_to_south);
+   RUN_TEST(test_rotating_counter_clockwise_changes_south_to_east);
+   RUN_TEST(test_rotating_counter_clockwise_changes_east_to_north);
+   RUN_TEST(test_moving_forward_one_facing_north_increments_Y);
+   RUN_TEST(test_moving_forward_one_facing_south_decrements_Y);
+   RUN_TEST(test_moving_forward_one_facing_east_increments_X);
+   RUN_TEST(test_moving_forward_one_facing_west_decrements_X);
+   RUN_TEST
+       (test_follow_series_of_instructions_moving_east_and_north_from_README);
+   RUN_TEST(test_follow_series_of_instructions_moving_west_and_north);
+   RUN_TEST(test_follow_series_of_instructions_moving_west_and_south);
+   RUN_TEST(test_follow_series_of_instructions_moving_east_and_north);
 
    return UnityEnd();
 }
