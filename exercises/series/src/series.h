e663bb1077be4c2595a9f27f220bf3a62484b352
wolf99 <281700+wolf99@users.noreply.github.com>
2021-01-24 22:13:13 +0000

    series: align with specification per #525 (#575)
    * series: align with specification per #525
    
    * [CI] Format C code
    
    * Trigger build
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- exercises/series/src/series.h
+++ exercises/series/src/series.h
@@ -6,23 +6,23 @@
 #define MAX_SERIES_LENGTH (5)
 
 // results structure
-typedef struct series_results {
+typedef struct slices {
    unsigned int substring_count;
    char **substring;            // array of pointers of dimension substring_count
-} series_results_t;
+} slices_t;
 
-// series - routine to split up input text into consecutive substrings of text of a given substringLength
+// slices - routine to slice up input text into consecutive substrings of text of a given substring_length
 //
 // inputs:
-//    inputText - bounded to MAX_INPUT_TEXT_LENGTH
-//    substringLength - the desired length of substrings that are returned bounded to MAX_SERIES_LENGTH
+//    input_text - bounded to MAX_INPUT_TEXT_LENGTH
+//    substring_length - the desired length of substrings that are returned bounded to MAX_SERIES_LENGTH
 //
 // outputs:
-//    SeriesResult_t results - structure containing count of the substring and an array of
+//    slices_t results - structure containing count of the substrings and an array of
 //       pointers to the substrings.
 //
-//       calling routine will free memory associated with each of the substrings and
+//       Calling routine will free memory associated with each of the substrings and
 //       the pointers to the substrings.
-series_results_t series(char *input_text, unsigned int substring_length);
+slices_t slices(char *input_text, unsigned int substring_length);
 
 #endif
--- exercises/series/test/test_series.c
+++ exercises/series/test/test_series.c
@@ -11,8 +11,8 @@ void tearDown(void)
 }
 
 // helper routines to validate test results
-static void test_solution(series_results_t * expected_solution,
-                          series_results_t * actual_solution)
+static void test_solution(slices_t * expected_solution,
+                          slices_t * actual_solution)
 {
    // result count correct
    TEST_ASSERT_EQUAL(expected_solution->substring_count,
@@ -31,71 +31,94 @@ static void test_solution(series_results_t * expected_solution,
 }
 
 // Test cases
-static void test_slices_of_one(void)
+static void test_slices_of_one_from_one(void)
 {
-   char *substrings[] = { "0", "1", "2", "3", "4" };
-   series_results_t expected = { 5, &substrings[0] };
-   series_results_t actual = series("01234", 1);
+   char *substrings[] = { "1" };
+   slices_t expected = { 1, &substrings[0] };
+   slices_t actual = slices("1", 1);
+
+   test_solution(&expected, &actual);
+}
+
+static void test_slices_of_one_from_two(void)
+{
+   TEST_IGNORE();               // delete this line to run test
+   char *substrings[] = { "1", "2" };
+   slices_t expected = { 2, &substrings[0] };
+   slices_t actual = slices("12", 1);
 
    test_solution(&expected, &actual);
 }
 
 static void test_slices_of_two(void)
 {
-   TEST_IGNORE();               // delete this line to run test
-   char *substrings[] = { "97", "78", "86", "67", "75", "56", "64" };
-   series_results_t expected = { 7, &substrings[0] };
-   series_results_t actual = series("97867564", 2);
+   TEST_IGNORE();
+   char *substrings[] = { "35" };
+   slices_t expected = { 1, &substrings[0] };
+   slices_t actual = slices("35", 2);
 
    test_solution(&expected, &actual);
 }
 
-static void test_slices_of_three(void)
+static void test_slices_of_two_overlap(void)
 {
    TEST_IGNORE();
-   char *substrings[] = { "978", "786", "867", "675", "756", "564" };
-   series_results_t expected = { 6, &substrings[0] };
-   series_results_t actual = series("97867564", 3);
+   char *substrings[] = { "91", "14", "42" };
+   slices_t expected = { 3, &substrings[0] };
+   slices_t actual = slices("9142", 2);
 
    test_solution(&expected, &actual);
 }
 
-static void test_slices_of_four(void)
+static void test_slices_can_include_duplicates(void)
 {
    TEST_IGNORE();
-   char *substrings[] = { "0123", "1234" };
-   series_results_t expected = { 2, &substrings[0] };
-   series_results_t actual = series("01234", 4);
+   char *substrings[] = { "777", "777", "777", "777" };
+   slices_t expected = { 4, &substrings[0] };
+   slices_t actual = slices("777777", 3);
 
    test_solution(&expected, &actual);
 }
 
-static void test_slices_of_five(void)
+static void test_slices_of_a_long_series(void)
 {
    TEST_IGNORE();
-   char *substrings[] = { "01234" };
-   series_results_t expected = { 1, &substrings[0] };
-   series_results_t actual = series("01234", 5);
+   char *substrings[] =
+       { "91849", "18493", "84939", "49390", "93904", "39042", "90424",
+      "04243"
+   };
+   slices_t expected = { 8, &substrings[0] };
+   slices_t actual = slices("918493904243", 5);
 
    test_solution(&expected, &actual);
 }
 
-static void test_overly_long_slice(void)
-{
-   TEST_IGNORE();
-   char *substrings[] = { "" };
-   series_results_t expected = { 0, &substrings[0] };
-   series_results_t actual = series("012", 4);
-
-   test_solution(&expected, &actual);
-}
-
-static void test_overly_short_slice(void)
+static void test_slice_length_is_too_large(void)
 {
    TEST_IGNORE();
    char *substrings[] = { "" };
-   series_results_t expected = { 0, &substrings[0] };
-   series_results_t actual = series("01234", 0);
+   slices_t expected = { 0, &substrings[0] };
+   slices_t actual = slices("12345", 6);
+
+   test_solution(&expected, &actual);
+}
+
+static void test_slice_length_cannot_be_zero(void)
+{
+   TEST_IGNORE();
+   char *substrings[] = { "" };
+   slices_t expected = { 0, &substrings[0] };
+   slices_t actual = slices("12345", 0);
+
+   test_solution(&expected, &actual);
+}
+
+static void test_empty_series_is_invalid(void)
+{
+   TEST_IGNORE();
+   char *substrings[] = { "" };
+   slices_t expected = { 0, &substrings[0] };
+   slices_t actual = slices("", 1);
 
    test_solution(&expected, &actual);
 }
@@ -104,13 +127,15 @@ int main(void)
 {
    UnityBegin("test/test_series.c");
 
-   RUN_TEST(test_slices_of_one);
+   RUN_TEST(test_slices_of_one_from_one);
+   RUN_TEST(test_slices_of_one_from_two);
    RUN_TEST(test_slices_of_two);
-   RUN_TEST(test_slices_of_three);
-   RUN_TEST(test_slices_of_four);
-   RUN_TEST(test_slices_of_five);
-   RUN_TEST(test_overly_long_slice);
-   RUN_TEST(test_overly_short_slice);
+   RUN_TEST(test_slices_of_two_overlap);
+   RUN_TEST(test_slices_can_include_duplicates);
+   RUN_TEST(test_slices_of_a_long_series);
+   RUN_TEST(test_slice_length_is_too_large);
+   RUN_TEST(test_slice_length_cannot_be_zero);
+   RUN_TEST(test_empty_series_is_invalid);
 
    return UnityEnd();
 }
