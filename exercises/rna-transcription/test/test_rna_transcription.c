635e1cdf8fca19f483914b1b4d276776b0c9b4a4
wolf99 <281700+wolf99@users.noreply.github.com>
2021-01-24 15:05:18 +0000

    rna-transcription: align with specification per #522 (#572)

--- exercises/rna-transcription/test/test_rna_transcription.c
+++ exercises/rna-transcription/test/test_rna_transcription.c
@@ -10,77 +10,58 @@ void tearDown(void)
 {
 }
 
-static void test_transcription(const char *dna, const char *expected)
+static void check_transcription(const char *dna, const char *expected)
 {
    char *rna = to_rna(dna);
    TEST_ASSERT_EQUAL_STRING(expected, rna);
    free(rna);
 }
 
-static void test_failure(const char *dna)
+static void test_empty_rna_sequence(void)
 {
-   TEST_ASSERT_NULL(to_rna(dna));
+   check_transcription("", "");
 }
 
-static void test_transcribes_G_to_C(void)
-{
-   test_transcription("G", "C");
-}
-
-static void test_transcribes_C_to_G(void)
+static void test_rna_complement_of_cytosine_is_guanine(void)
 {
    TEST_IGNORE();               // delete this line to run test
-   test_transcription("C", "G");
+   check_transcription("C", "G");
 }
 
-static void test_transcribes_T_to_A(void)
+static void test_rna_complement_of_guanine_is_cytosine(void)
 {
    TEST_IGNORE();
-   test_transcription("T", "A");
+   check_transcription("G", "C");
 }
 
-static void test_transcribes_A_to_U(void)
+static void test_rna_complement_of_thymine_is_adenine(void)
 {
    TEST_IGNORE();
-   test_transcription("A", "U");
+   check_transcription("T", "A");
 }
 
-static void test_transcribes_all_occurrences(void)
+static void test_rna_complement_of_adenine_is_uracil(void)
 {
    TEST_IGNORE();
-   test_transcription("ACGTGGTCTTAA", "UGCACCAGAAUU");
+   check_transcription("A", "U");
 }
 
-static void test_handle_invalid_nucleotide(void)
+static void test_rna_complement(void)
 {
    TEST_IGNORE();
-   test_failure("U");
-}
-
-static void test_handle_completely_invalid_input(void)
-{
-   TEST_IGNORE();
-   test_failure("XXX");
-}
-
-static void test_handle_partially_invalid_input(void)
-{
-   TEST_IGNORE();
-   test_failure("ACGTXXXCTTAA");
+   check_transcription("ACGTGGTCTTAA", "UGCACCAGAAUU");
 }
 
 int main(void)
 {
    UnityBegin("test/test_rna_transcription.c");
 
-   RUN_TEST(test_transcribes_G_to_C);
-   RUN_TEST(test_transcribes_C_to_G);
-   RUN_TEST(test_transcribes_T_to_A);
-   RUN_TEST(test_transcribes_A_to_U);
-   RUN_TEST(test_transcribes_all_occurrences);
-   RUN_TEST(test_handle_invalid_nucleotide);
-   RUN_TEST(test_handle_completely_invalid_input);
-   RUN_TEST(test_handle_partially_invalid_input);
+   RUN_TEST(test_empty_rna_sequence);
+   RUN_TEST(test_rna_complement_of_cytosine_is_guanine);
+   RUN_TEST(test_rna_complement_of_guanine_is_cytosine);
+   RUN_TEST(test_rna_complement_of_thymine_is_adenine);
+   RUN_TEST(test_rna_complement_of_adenine_is_uracil);
+   RUN_TEST(test_rna_complement);
 
    return UnityEnd();
 }
