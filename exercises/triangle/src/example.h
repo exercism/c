f1ae01191041ea72e08055ad6bf4bc10e4d41dbc
wolf99 <281700+wolf99@users.noreply.github.com>
2021-01-26 02:00:29 +0000

    triangle: align with specification per #528 (#578)
    * triangle: align with specification per #528
    
    * Update names to match spec

--- exercises/triangle/src/example.h
+++ exercises/triangle/src/example.h
@@ -1,5 +1,6 @@
 #ifndef TRIANGLE_H
 #define TRIANGLE_H
+
 #include <stdbool.h>
 
 typedef struct {
@@ -8,8 +9,8 @@ typedef struct {
    double c;
 } triangle_t;
 
-bool is_equilateral(triangle_t input);
-bool is_isosceles(triangle_t input);
-bool is_scalene(triangle_t input);
+bool is_equilateral(triangle_t sides);
+bool is_isosceles(triangle_t sides);
+bool is_scalene(triangle_t sides);
 
 #endif
