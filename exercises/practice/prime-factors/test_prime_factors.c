47e5a9f6cbd5d451ac38d24c1e608ca75800baef
wolf99 <281700+wolf99@users.noreply.github.com>
2021-02-14 12:48:02 +0000

    prime-factors: update to new spec (#624)
    New tests have been recently added to the spec.
    
    This commit updates the tests to match.

--- exercises/practice/prime-factors/test_prime_factors.c
+++ exercises/practice/prime-factors/test_prime_factors.c
@@ -29,6 +29,16 @@ static void test_prime_number(void)
    TEST_ASSERT_EQUAL_INT64_ARRAY(expected_factors, buf, expected_len);
 }
 
+static void test_another_prime_number(void)
+{
+   TEST_IGNORE();
+   uint64_t expected_factors[] = { 3 };
+   size_t expected_len = 1;
+   size_t actual_len = find_factors(3, buf);
+   TEST_ASSERT_EQUAL_INT(expected_len, actual_len);
+   TEST_ASSERT_EQUAL_INT64_ARRAY(expected_factors, buf, expected_len);
+}
+
 static void test_square_of_a_prime(void)
 {
    TEST_IGNORE();
@@ -39,12 +49,52 @@ static void test_square_of_a_prime(void)
    TEST_ASSERT_EQUAL_INT64_ARRAY(expected_factors, buf, expected_len);
 }
 
+static void test_product_of_first_prime(void)
+{
+   TEST_IGNORE();
+   uint64_t expected_factors[] = { 2, 2 };
+   size_t expected_len = 2;
+   size_t actual_len = find_factors(4, buf);
+   TEST_ASSERT_EQUAL_INT(expected_len, actual_len);
+   TEST_ASSERT_EQUAL_INT64_ARRAY(expected_factors, buf, expected_len);
+}
+
 static void test_cube_of_a_prime(void)
 {
    TEST_IGNORE();
    uint64_t expected_factors[] = { 2, 2, 2 };
    size_t expected_len = 3;
    size_t actual_len = find_factors(8, buf);
+   TEST_ASSERT_EQUAL_INT(expected_len, actual_len);
+   TEST_ASSERT_EQUAL_INT64_ARRAY(expected_factors, buf, expected_len);
+}
+
+static void test_product_of_second_prime(void)
+{
+   TEST_IGNORE();
+   uint64_t expected_factors[] = { 3, 3, 3 };
+   size_t expected_len = 3;
+   size_t actual_len = find_factors(27, buf);
+   TEST_ASSERT_EQUAL_INT(expected_len, actual_len);
+   TEST_ASSERT_EQUAL_INT64_ARRAY(expected_factors, buf, expected_len);
+}
+
+static void test_product_of_third_prime(void)
+{
+   TEST_IGNORE();
+   uint64_t expected_factors[] = { 5, 5, 5, 5 };
+   size_t expected_len = 4;
+   size_t actual_len = find_factors(625, buf);
+   TEST_ASSERT_EQUAL_INT(expected_len, actual_len);
+   TEST_ASSERT_EQUAL_INT64_ARRAY(expected_factors, buf, expected_len);
+}
+
+static void test_product_of_first_and_second_prime(void)
+{
+   TEST_IGNORE();
+   uint64_t expected_factors[] = { 2, 3 };
+   size_t expected_len = 2;
+   size_t actual_len = find_factors(6, buf);
    TEST_ASSERT_EQUAL_INT(expected_len, actual_len);
    TEST_ASSERT_EQUAL_INT64_ARRAY(expected_factors, buf, expected_len);
 }
@@ -85,8 +135,13 @@ int main(void)
 
    RUN_TEST(test_no_factors);
    RUN_TEST(test_prime_number);
+   RUN_TEST(test_another_prime_number);
    RUN_TEST(test_square_of_a_prime);
+   RUN_TEST(test_product_of_first_prime);
    RUN_TEST(test_cube_of_a_prime);
+   RUN_TEST(test_product_of_second_prime);
+   RUN_TEST(test_product_of_third_prime);
+   RUN_TEST(test_product_of_first_and_second_prime);
    RUN_TEST(test_product_of_primes_and_non_primes);
    RUN_TEST(test_product_of_primes);
    RUN_TEST(test_factors_include_a_large_prime);
