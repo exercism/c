e4af5bf2059b5882c73e2e21713ae22673019664
wolf99 <281700+wolf99@users.noreply.github.com>
2021-05-12 19:21:59 +0100

    beer-song: update to latest spec
    fixes #549

--- exercises/practice/beer-song/.meta/example.c
+++ exercises/practice/beer-song/.meta/example.c
@@ -1,46 +1,31 @@
 #include "beer_song.h"
+#include <stdint.h>
 #include <stdio.h>
-#include <string.h>
 
-static unsigned int get_verse(unsigned int bottles, char *buffer)
+static uint16_t get_verse(uint8_t bottles, char **verse)
 {
-   unsigned int bytes_written = 0;
+   uint16_t lines_written = 0;
 
    if (bottles == 0) {
-      bytes_written =
-          sprintf(buffer,
-                  "No more bottles of beer on the wall, no more bottles of beer.\n"
-                  "Go to the store and buy some more, 99 bottles of beer on the wall.\n");
+      sprintf(*verse, "No more bottles of beer on the wall, no more bottles of beer.");
+      sprintf(*++verse, "Go to the store and buy some more, 99 bottles of beer on the wall.");
+      lines_written = 2;
    } else if (bottles == 1) {
-      bytes_written =
-          sprintf(buffer,
-                  "1 bottle of beer on the wall, 1 bottle of beer.\n"
-                  "Take it down and pass it around, no more bottles of beer on the wall.\n");
-   } else if (bottles == 2) {
-      bytes_written =
-          sprintf(buffer,
-                  "2 bottles of beer on the wall, 2 bottles of beer.\n"
-                  "Take one down and pass it around, 1 bottle of beer on the wall.\n");
-   } else if (bottles <= 99) {
-      bytes_written =
-          sprintf(buffer,
-                  "%u bottles of beer on the wall, %u bottles of beer.\n"
-                  "Take one down and pass it around, %u bottles of beer on the wall.\n",
-                  bottles, bottles, bottles - 1);
+      sprintf(*verse, "%u bottle of beer on the wall, %u bottle of beer.", bottles, bottles);
+      sprintf(*++verse, "Take it down and pass it around, no more bottles of beer on the wall.");
+      lines_written = 3;
+   } else {
+      sprintf(*verse, "%u bottles of beer on the wall, %u bottles of beer.", bottles, bottles);
+      sprintf(*++verse, "Take one down and pass it around, %u bottle%sof beer on the wall.", bottles - 1, bottles - 1 == 1 ? " " : "s ");
+      lines_written = 3;
    }
 
-   return bytes_written;
+   return lines_written;
 }
 
-void recite(unsigned int start_bottles, unsigned int take_down, char *buffer)
+void recite(uint8_t start_bottles, uint8_t take_down, char **song)
 {
-   char *current_position = buffer;
-
    while (take_down-- > 0) {
-      current_position += get_verse(start_bottles--, current_position);
-      strcpy(current_position, "\n");
-      current_position++;
+      song += get_verse(start_bottles--, song);
    }
-
-   strcpy(current_position - 1, "\0");
 }
