554714155f4eca2c86cb835e80a0b2517317d5c3
wolf99 <281700+wolf99@users.noreply.github.com>
2021-07-15 22:31:22 +0100

    Add new tests per fixed spec

--- exercises/practice/grade-school/test_grade_school.c
+++ exercises/practice/grade-school/test_grade_school.c
@@ -2,6 +2,7 @@
 #include "grade_school.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 void setUp(void)
 {
@@ -17,13 +18,13 @@ static void populate_roster(roster_t * expected)
    for (size_t i = 0; i < expected->count; ++i) {
       char name_buffer[MAX_NAME_LENGTH];
       strcpy(name_buffer, expected->students[i].name);
-      TEST_ASSERT_TRUE(add_student(name_buffer, expected->students[i].grade));
+      TEST_ASSERT_TRUE_MESSAGE(add_student(name_buffer, expected->students[i].grade), "Could not add student");
    }
 }
 
 static void check_roster_names(roster_t * expected, roster_t * actual)
 {
-   TEST_ASSERT_EQUAL(expected->count, actual->count);
+   TEST_ASSERT_EQUAL_size_t_MESSAGE(expected->count, actual->count, "Incorrect number of students");
 
    for (size_t i = 0; i < expected->count; ++i)
       TEST_ASSERT_EQUAL_STRING(expected->students[i].name,
@@ -54,21 +55,18 @@ static void test_adding_student_adds_to_the_sorted_roster(void)
    check_roster(&input, &expected);
 }
 
-static void test_a_student_cant_be_in_two_different_grades(void)
+static void test_student_can_only_be_added_to_the_same_grade_in_the_roster_once(void)
 {
    TEST_IGNORE();               // delete this line to run test
    roster_t input = {
       2, {
-          (student_t) {1, "Aimee"},
+          (student_t) {2, "Aimee"},
           (student_t) {2, "Aimee"}}
    };
-   uint8_t desired_grade = 2;
 
-   populate_roster(&input);
+   roster_t expected = { 1, {(student_t) {2, "Aimee"}} };
 
-   roster_t actual = get_grade(desired_grade);
-
-   TEST_ASSERT_EQUAL(0, actual.count);
+   check_roster(&input, &expected);
 }
 
 static void test_adding_more_students_adds_to_the_sorted_roster(void)
@@ -106,6 +104,21 @@ test_adding_students_to_different_grades_adds_to_the_same_sorted_roster(void)
           (student_t) {3, "Chelsea"},
           (student_t) {7, "Logan"}}
    };
+
+   check_roster(&input, &expected);
+}
+
+static void
+test_student_cannot_be_added_to_more_than_one_grade_in_the_sorted_roster (void)
+{
+   TEST_IGNORE();
+   roster_t input = {
+      2, {
+          (student_t) {2, "Aimee"},
+          (student_t) {1, "Aimee"}}
+   };
+
+   roster_t expected = { 1, {(student_t) {2, "Aimee"}} };
 
    check_roster(&input, &expected);
 }
@@ -189,10 +202,12 @@ int main(void)
    UnityBegin("test_grade_school.c");
 
    RUN_TEST(test_adding_student_adds_to_the_sorted_roster);
-   RUN_TEST(test_a_student_cant_be_in_two_different_grades);
+   RUN_TEST(test_student_can_only_be_added_to_the_same_grade_in_the_roster_once);
    RUN_TEST(test_adding_more_students_adds_to_the_sorted_roster);
    RUN_TEST
        (test_adding_students_to_different_grades_adds_to_the_same_sorted_roster);
+   RUN_TEST
+       (test_student_cannot_be_added_to_more_than_one_grade_in_the_sorted_roster);
    RUN_TEST
        (test_roster_returns_an_empty_list_if_there_are_no_students_enrolled);
    RUN_TEST(test_student_names_with_grades_displayed_in_the_same_sorted_roster);
