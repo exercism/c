bdc250579f413a28d6c9807f23a7a37c96e44bcd
Nerwosolek <subtraction_game@gieorgijewski.pl>
2021-09-21 18:29:42 +0200

    Modified example solution to reflect changes to tests.

--- exercises/practice/all-your-base/.meta/example.c
+++ exercises/practice/all-your-base/.meta/example.c
@@ -17,11 +17,8 @@ size_t rebase(int8_t digits[DIGITS_ARRAY_SIZE], int16_t from_base,
    for (size_t i = 0; i < num_digits && digits[i] == 0; ++i)
       ++first_non_zero;         /* skip leading zeros */
 
-   if (first_non_zero == num_digits)
-      return 0;                 /* check input isn't all zeros */
-
    /* convert to denary */
-   for (size_t i = 0; i < num_digits; ++i) {
+   for (size_t i = first_non_zero; i < num_digits; ++i) {
       if (digits[i] < 0)
          return 0;              /* negative digits */
       if (digits[i] >= from_base)
@@ -30,16 +27,17 @@ size_t rebase(int8_t digits[DIGITS_ARRAY_SIZE], int16_t from_base,
    }
 
    /* calculate number of new digits */
-   for (uint16_t j = denary; j > 0;) {
-      j /= to_base;
-      ++new_num_digits;
-   }
+   uint16_t j = denary;
+   do { // if proper input, there is at least 1-digit (zero included)
+        ++new_num_digits;
+    } while ((j /= to_base) != 0);
 
    /* calculate and store new digits */
+   digits[0] = 0;
    for (size_t i = new_num_digits - 1; denary > 0; --i) {
       digits[i] = denary % to_base;
       denary /= to_base;
-   }
+   }  
 
    /* ensure rest of array is zero */
    memset(&digits[new_num_digits], 0, DIGITS_ARRAY_SIZE - new_num_digits);
