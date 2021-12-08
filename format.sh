1ce63e7893aa0d371c966174ed5f455a31bc9438
wolf99 <281700+wolf99@users.noreply.github.com>
2021-12-08 22:02:02 +0000

    Remove comments

--- format.sh
+++ format.sh
@@ -3,6 +3,6 @@
 # Formats all .c and .h files in the project that are not vendor code (ie: Unity).
 #
 
-find exercises/practice/ -path '*/test-framework' -prune -o \
+find exercises/practice/ -path 'exercises/practice/*/test-framework' -prune -o \
   \( -iname '*.h' -o -iname '*.c' \) -print0 |\
   xargs --null clang-format -i
