123f8f457cbd9534db8297a8e2c46e8dca912c22
wolf99 <281700+wolf99@users.noreply.github.com>
2021-12-09 20:27:51 +0000

    Add check for clang-format presence

--- format.sh
+++ format.sh
@@ -3,6 +3,13 @@
 # Formats all .c and .h files in the project that are not vendor code (ie: Unity).
 #
 
+if ! [ -x "$(command -v clang-format)" ]; then
+  echo "ERROR:  Install clang-format version 10.0.0 or greater to run this script."
+  exit 126
+fi
+
+echo "INFO:  clang-format version 10.0.0 or greater is recommended to run this script"
+
 find exercises/practice/ -path 'exercises/practice/*/test-framework' -prune -o \
   \( -iname '*.h' -o -iname '*.c' \) -print0 |\
   xargs --null clang-format -i
