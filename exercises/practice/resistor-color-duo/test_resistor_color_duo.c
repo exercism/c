b6ab7ddb59c176737b06efde32d0852e9330651f
wolf99 <281700+wolf99@users.noreply.github.com>
2021-12-12 15:37:47 +0000

    Resistor-color-duo: update to latest spec (#731)

--- exercises/practice/resistor-color-duo/test_resistor_color_duo.c
+++ exercises/practice/resistor-color-duo/test_resistor_color_duo.c
@@ -29,6 +29,13 @@ static void test_yellow_and_violet(void)
    TEST_ASSERT_EQUAL_UINT16(47, actual);
 }
 
+static void test_white_and_red(void)
+{
+   TEST_IGNORE();
+   uint16_t actual = color_code((resistor_band_t[]){ WHITE, RED });
+   TEST_ASSERT_EQUAL_UINT16(92, actual);
+}
+
 static void test_orange_and_orange(void)
 {
    TEST_IGNORE();
@@ -43,6 +50,13 @@ static void test_ignore_additional_colors(void)
    TEST_ASSERT_EQUAL_UINT16(51, actual);
 }
 
+static void test_black_and_brown_one_digit(void)
+{
+   TEST_IGNORE();
+   uint16_t actual = color_code((resistor_band_t[]){ BLACK, BROWN });
+   TEST_ASSERT_EQUAL_UINT16(1, actual);
+}
+
 int main(void)
 {
    UnityBegin("test_resistor_color_duo.c");
@@ -50,8 +64,10 @@ int main(void)
    RUN_TEST(test_brown_and_black);
    RUN_TEST(test_blue_and_grey);
    RUN_TEST(test_yellow_and_violet);
+   RUN_TEST(test_white_and_red);
    RUN_TEST(test_orange_and_orange);
    RUN_TEST(test_ignore_additional_colors);
+   RUN_TEST(test_black_and_brown_one_digit);
 
    return UnityEnd();
 }
