8afc8054d02b2a52e2f335ce96a2e46161b752c4
wolf99 <281700+wolf99@users.noreply.github.com>
2021-12-12 15:58:12 +0000

    Improve naming

--- exercises/practice/space-age/.meta/example.c
+++ exercises/practice/space-age/.meta/example.c
@@ -2,21 +2,21 @@
 #include <stdbool.h>
 
 /* Number of seconds in a Earth year */
-#define EARTH_YR_IN_SECONDS 31557600
+#define EARTH_YEAR_IN_SECONDS 31557600
 
 /* Orbital period of each planet, in Earth years */
 static const float factors[8] = {
-   EARTH_YR_IN_SECONDS * 0.2408467,    // Mercury
-   EARTH_YR_IN_SECONDS * 0.61519726,   // Venus
-   EARTH_YR_IN_SECONDS * 1,            // Earth
-   EARTH_YR_IN_SECONDS * 1.8808158,    // Mars
-   EARTH_YR_IN_SECONDS * 11.862615,    // Jupiter
-   EARTH_YR_IN_SECONDS * 29.447498,    // Saturn
-   EARTH_YR_IN_SECONDS * 84.016846,    // Uranus
-   EARTH_YR_IN_SECONDS * 164.79132     // Neptune
+   EARTH_YEAR_IN_SECONDS * 0.2408467,    // Mercury
+   EARTH_YEAR_IN_SECONDS * 0.61519726,   // Venus
+   EARTH_YEAR_IN_SECONDS * 1,            // Earth
+   EARTH_YEAR_IN_SECONDS * 1.8808158,    // Mars
+   EARTH_YEAR_IN_SECONDS * 11.862615,    // Jupiter
+   EARTH_YEAR_IN_SECONDS * 29.447498,    // Saturn
+   EARTH_YEAR_IN_SECONDS * 84.016846,    // Uranus
+   EARTH_YEAR_IN_SECONDS * 164.79132     // Neptune
 };
 
-static bool isPlanet(planet_t planet)
+static bool is_planet(planet_t planet)
 {
    bool result = false;
 
@@ -37,5 +37,5 @@ static bool isPlanet(planet_t planet)
 
 float age(planet_t planet, int64_t seconds)
 {
-   return (isPlanet(planet)) ? seconds / factors[planet] : -1.0;
+   return (is_planet(planet)) ? seconds / factors[planet] : -1.0;
 }
