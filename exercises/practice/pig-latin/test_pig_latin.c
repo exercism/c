d8ab75deab5f635355482d895a2985391455e02c
Bob Hoeppner <32035397+bobahop@users.noreply.github.com>
2022-02-01 05:56:52 -0600

    Update test_pig_latin.c
    Formatting tweak

--- exercises/practice/pig-latin/test_pig_latin.c
+++ exercises/practice/pig-latin/test_pig_latin.c
@@ -27,7 +27,7 @@ static void test_word_beginning_with_a(void)
 
 static void test_word_beginning_with_e(void)
 {
-   TEST_IGNORE(); // delete this line to run test
+   TEST_IGNORE();   // delete this line to run test
    check_transate("ear", "earay");
 }
 
