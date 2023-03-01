64ee8b6e7468fe57f23e9b401df731de802dc6c2
Matthias <matthias@webding.de>
2023-03-01 02:55:29 +0100

    sieve: fix a small typo (#844)
    old: "the number of calculate primes"
    new: "the number of calculated primes"

--- exercises/practice/sieve/sieve.h
+++ exercises/practice/sieve/sieve.h
@@ -7,7 +7,7 @@
 /// Calculate at most `max_primes` prime numbers in the interval [2,limit]
 /// using the Sieve of Eratosthenes and store the prime numbers in `primes`
 /// in increasing order.
-/// The function returns the number of calculate primes.
+/// The function returns the number of calculated primes.
 uint32_t sieve(uint32_t limit, uint32_t *primes, size_t max_primes);
 
 #endif
