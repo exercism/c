ca88375a32faef126c4ca380d3738045ba7458ed
Jeremy Walker <jez.walker@gmail.com>
2023-08-17 20:01:30 +0100

    Hello World should start with the default stub

--- exercises/practice/hello-world/hello_world.c
+++ exercises/practice/hello-world/hello_world.c
@@ -1,14 +1,7 @@
-// Include the standard definitions header from the standard library, so that we
-// have access to 'NULL'. This can be removed if your changes remove the need
-// for 'NULL'.
-#include <stddef.h>
-
 #include "hello_world.h"
 
 // Define the function itself.
 const char *hello(void)
 {
-   // To fix this function, change the return statement here to instead return
-   // a string equivalent to the string expected by the failing test.
-   return NULL;
+   return "Goodbye, Mars!";
 }
