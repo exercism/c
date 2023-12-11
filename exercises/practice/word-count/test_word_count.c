f1ae583e894ca3464576213d3584b7d5331a92cc
LeakedMemory <elmq0022@umn.edu>
2023-12-10 20:12:43 -0600

    use clang-format

--- exercises/practice/word-count/test_word_count.c
+++ exercises/practice/word-count/test_word_count.c
@@ -17,10 +17,11 @@ void tearDown(void)
 {
 }
 
-static int cmp_word_count_word(const void *a, const void *b){
-    word_count_word_t *w1 = (word_count_word_t *)a;
-    word_count_word_t *w2 = (word_count_word_t *)b;
-    return strcmp(w1->text, w2->text);
+static int cmp_word_count_word(const void *a, const void *b)
+{
+   word_count_word_t *w1 = (word_count_word_t *)a;
+   word_count_word_t *w2 = (word_count_word_t *)b;
+   return strcmp(w1->text, w2->text);
 }
 
 static void check_solution(word_count_word_t *expected_solution,
@@ -32,8 +33,10 @@ static void check_solution(word_count_word_t *expected_solution,
    TEST_ASSERT_EQUAL(expected_word_count, actual_word_count);
 
    // Sort the words before comparing
-   qsort(expected_solution, expected_word_count, sizeof(word_count_word_t), cmp_word_count_word);
-   qsort(actual_solution, actual_word_count, sizeof(word_count_word_t), cmp_word_count_word);
+   qsort(expected_solution, expected_word_count, sizeof(word_count_word_t),
+         cmp_word_count_word);
+   qsort(actual_solution, actual_word_count, sizeof(word_count_word_t),
+         cmp_word_count_word);
 
    // now test the word count for the words...
    for (int index = 0; index < MAX_WORDS; index++) {
