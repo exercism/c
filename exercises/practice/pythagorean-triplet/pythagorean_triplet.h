f4fc7440f85c6b9108795d0359967c3e06726097
Vladimir Popov <6939832+vladimir-popov@users.noreply.github.com>
2024-01-21 15:12:05 +0400

    Add used types to the pythagorean_triplet.h

--- exercises/practice/pythagorean-triplet/pythagorean_triplet.h
+++ exercises/practice/pythagorean-triplet/pythagorean_triplet.h
@@ -1,4 +1,21 @@
 #ifndef PYTHAGOREAN_TRIPLET_H
 #define PYTHAGOREAN_TRIPLET_H
 
+#include <stdint.h>
+
+typedef struct
+{
+  int a, b, c;
+} triplet_t;
+
+typedef struct
+{
+  int count;
+  triplet_t *triplets;
+} triplets_t;
+
+triplets_t *triplets_with_sum (uint16_t sum);
+
+void free_triplets (triplets_t *pt);
+
 #endif
