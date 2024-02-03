962d4e4f46a0be6e04b58da170554135df517f91
Ryan Hartlage <ryanplusplus@gmail.com>
2024-02-03 08:44:06 -0500

    Return int in pop-count example (#949)

--- exercises/practice/eliuds-eggs/test_eliuds_eggs.c
+++ exercises/practice/eliuds-eggs/test_eliuds_eggs.c
@@ -12,28 +12,28 @@ void tearDown(void)
 static void test_0_eggs(void)
 {
    const unsigned int expected = 0;
-   TEST_ASSERT_EQUAL_INT(expected, egg_count(0));
+   TEST_ASSERT_EQUAL_UINT(expected, egg_count(0));
 }
 
 static void test_1_eggs(void)
 {
    TEST_IGNORE();
    const unsigned int expected = 1;
-   TEST_ASSERT_EQUAL_INT(expected, egg_count(16));
+   TEST_ASSERT_EQUAL_UINT(expected, egg_count(16));
 }
 
 static void test_4_eggs(void)
 {
    TEST_IGNORE();
    const unsigned int expected = 4;
-   TEST_ASSERT_EQUAL_INT(expected, egg_count(89));
+   TEST_ASSERT_EQUAL_UINT(expected, egg_count(89));
 }
 
 static void test_13_eggs(void)
 {
    TEST_IGNORE();
    const unsigned int expected = 13;
-   TEST_ASSERT_EQUAL_INT(expected, egg_count(2000000000));
+   TEST_ASSERT_EQUAL_UINT(expected, egg_count(2000000000));
 }
 
 int main(void)
