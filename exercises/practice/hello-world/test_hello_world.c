9c4a65200d69dcc4c12025ec25c28f47c6538f7b
Matthias Rahlf <matthias@webding.de>
2024-02-23 14:38:49 +0100

    Adjust locations of files in comments.

--- exercises/practice/hello-world/test_hello_world.c
+++ exercises/practice/hello-world/test_hello_world.c
@@ -20,7 +20,7 @@ static void test_say_hi(void)
    // Check if the 'hello()' function returns "Hello, World!"
    // This test is expected to fail after first downloading this exercise.
    // To make this test pass, fix the 'hello()' function definition in the
-   // source file src/hello_world.c.
+   // source file hello_world.c.
    TEST_ASSERT_EQUAL_STRING("Hello, World!", hello());
 }
 
