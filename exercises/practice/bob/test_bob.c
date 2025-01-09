c2095fcfe5aa3207f2d6e28f89128aa95fea72cd
jagdish-15 <jagadishdrp@gmail.com>
2025-01-10 00:24:00 +0530

    Adding tests to Bob exercise (#1030)
    * Adding tests to Bob exercise
    
    * Fixing newly added test
    
    * Formatting test file

--- exercises/practice/bob/test_bob.c
+++ exercises/practice/bob/test_bob.c
@@ -140,8 +140,7 @@ static void test_multiple_line_question(void)
 {
    TEST_IGNORE();
    TEST_ASSERT_EQUAL_STRING(
-       "Whatever.",
-       hey_bob("\nDoes this cryogenic chamber make me look fat?\nNo"));
+       "Sure.", hey_bob("\nDoes this cryogenic chamber make\n me look fat?"));
 }
 
 static void test_starting_with_whitespace(void)
