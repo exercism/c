74d024fffc8bfeea7e24cf8d9e2dd4c5f92d2f4b
jagdish-15 <jagadishdrp@gmail.com>
2025-01-12 20:03:52 +0530

    Updating new tests and rearranging tests for Roman-Numerals (#1035)
    * Updating new tests and rearranging tests for Roman-Numerals
    
    * Fix tests
    
    ---------
    
    Co-authored-by: Ryan Hartlage <ryanplusplus@gmail.com>

--- exercises/practice/roman-numerals/test_roman_numerals.c
+++ exercises/practice/roman-numerals/test_roman_numerals.c
@@ -62,6 +62,12 @@ static void test_9_is_IX(void)
    check_conversion(9, "IX");
 }
 
+static void test_16_is_XVI(void)
+{
+   TEST_IGNORE();
+   check_conversion(16, "XVI");
+}
+
 static void test_27_is_XXVII(void)
 {
    TEST_IGNORE();
@@ -86,6 +92,12 @@ static void test_59_is_LIX(void)
    check_conversion(59, "LIX");
 }
 
+static void test_66_is_LXVI(void)
+{
+   TEST_IGNORE();
+   check_conversion(66, "LXVI");
+}
+
 static void test_93_is_XCIII(void)
 {
    TEST_IGNORE();
@@ -104,6 +116,12 @@ static void test_163_is_CLXIII(void)
    check_conversion(163, "CLXIII");
 }
 
+static void test_166_is_CLXVI(void)
+{
+   TEST_IGNORE();
+   check_conversion(166, "CLXVI");
+}
+
 static void test_402_is_CDII(void)
 {
    TEST_IGNORE();
@@ -114,6 +132,12 @@ static void test_575_is_DLXXV(void)
 {
    TEST_IGNORE();
    check_conversion(575, "DLXXV");
+}
+
+static void test_666_is_DCLXVI(void)
+{
+   TEST_IGNORE();
+   check_conversion(666, "DCLXVI");
 }
 
 static void test_911_is_CMXI(void)
@@ -128,46 +152,28 @@ static void test_1024_is_MXXIV(void)
    check_conversion(1024, "MXXIV");
 }
 
-static void test_3000_is_MMM(void)
-{
-   TEST_IGNORE();
-   check_conversion(3000, "MMM");
-}
-
-static void test_16_is_XVI(void)
-{
-   TEST_IGNORE();
-   check_conversion(16, "XVI");
-}
-
-static void test_66_is_LXVI(void)
-{
-   TEST_IGNORE();
-   check_conversion(66, "LXVI");
-}
-
-static void test_166_is_CLXVI(void)
-{
-   TEST_IGNORE();
-   check_conversion(166, "CLXVI");
-}
-
-static void test_666_is_DCLXVI(void)
-{
-   TEST_IGNORE();
-   check_conversion(666, "DCLXVI");
-}
-
 static void test_1666_is_MDCLXVI(void)
 {
    TEST_IGNORE();
    check_conversion(1666, "MDCLXVI");
 }
 
+static void test_3000_is_MMM(void)
+{
+   TEST_IGNORE();
+   check_conversion(3000, "MMM");
+}
+
 static void test_3001_is_MMMI(void)
 {
    TEST_IGNORE();
    check_conversion(3001, "MMMI");
+}
+
+static void test_3888_is_MMMDCCCLXXXVIII(void)
+{
+   TEST_IGNORE();
+   check_conversion(3888, "MMMDCCCLXXXVIII");
 }
 
 static void test_3999_is_MMMCMXCIX(void)
@@ -187,24 +193,25 @@ int main(void)
    RUN_TEST(test_5_is_V);
    RUN_TEST(test_6_is_VI);
    RUN_TEST(test_9_is_IX);
+   RUN_TEST(test_16_is_XVI);
    RUN_TEST(test_27_is_XXVII);
    RUN_TEST(test_48_is_XLVIII);
    RUN_TEST(test_49_is_XLIX);
    RUN_TEST(test_59_is_LIX);
+   RUN_TEST(test_66_is_LXVI);
    RUN_TEST(test_93_is_XCIII);
    RUN_TEST(test_141_is_CXLI);
    RUN_TEST(test_163_is_CLXIII);
+   RUN_TEST(test_166_is_CLXVI);
    RUN_TEST(test_402_is_CDII);
    RUN_TEST(test_575_is_DLXXV);
+   RUN_TEST(test_666_is_DCLXVI);
    RUN_TEST(test_911_is_CMXI);
    RUN_TEST(test_1024_is_MXXIV);
-   RUN_TEST(test_3000_is_MMM);
-   RUN_TEST(test_16_is_XVI);
-   RUN_TEST(test_66_is_LXVI);
-   RUN_TEST(test_166_is_CLXVI);
-   RUN_TEST(test_666_is_DCLXVI);
    RUN_TEST(test_1666_is_MDCLXVI);
+   RUN_TEST(test_3000_is_MMM);
    RUN_TEST(test_3001_is_MMMI);
+   RUN_TEST(test_3888_is_MMMDCCCLXXXVIII);
    RUN_TEST(test_3999_is_MMMCMXCIX);
 
    return UNITY_END();
