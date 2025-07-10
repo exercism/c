f7b81ab6d726a3bf83a0ab22ceb2e5ccfae8ebf4
Shinichi Sakata <shinichi.sakata@gmail.com>
2025-07-10 16:03:59 -0700

    Secret Handshake: Reformat three C files with clang-format.

--- exercises/practice/secret-handshake/.meta/example.c
+++ exercises/practice/secret-handshake/.meta/example.c
@@ -2,10 +2,10 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-const char* actions[] = {"wink", "double blink", "close your eyes", "jump"};
+const char *actions[] = {"wink", "double blink", "close your eyes", "jump"};
 
-const char** commands(const size_t number) {
-  const char** action_sequence = (const char**)calloc(4, sizeof(char*));
+const char **commands(const size_t number) {
+  const char **action_sequence = (const char **)calloc(4, sizeof(char *));
   size_t k = 0;
   if (number & 16) {
     for (int i = 3; i >= 0; i--)
