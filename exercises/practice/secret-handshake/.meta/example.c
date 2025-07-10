d477f1a638f1b005ddca01bc59f9d2427eebf0da
Shinichi Sakata <shinichi.sakata@gmail.com>
2025-07-10 16:22:26 -0700

    Secret Handshake: Run format.sh for correcting the format of codes.

--- exercises/practice/secret-handshake/.meta/example.c
+++ exercises/practice/secret-handshake/.meta/example.c
@@ -2,19 +2,20 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-const char *actions[] = {"wink", "double blink", "close your eyes", "jump"};
+const char *actions[] = { "wink", "double blink", "close your eyes", "jump" };
 
-const char **commands(const size_t number) {
-  const char **action_sequence = calloc(4, sizeof(char *));
-  size_t k = 0;
-  if (number & 16) {
-    for (int i = 3; i >= 0; i--)
-      if (number & 1 << i)
-        action_sequence[k++] = actions[i];
-  } else {
-    for (int i = 0; i <= 3; i++)
-      if (number & 1 << i)
-        action_sequence[k++] = actions[i];
-  }
-  return action_sequence;
+const char **commands(const size_t number)
+{
+   const char **action_sequence = calloc(4, sizeof(char *));
+   size_t k = 0;
+   if (number & 16) {
+      for (int i = 3; i >= 0; i--)
+         if (number & 1 << i)
+            action_sequence[k++] = actions[i];
+   } else {
+      for (int i = 0; i <= 3; i++)
+         if (number & 1 << i)
+            action_sequence[k++] = actions[i];
+   }
+   return action_sequence;
 }
