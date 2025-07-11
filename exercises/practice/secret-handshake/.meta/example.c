bc0105cdc21f75b6c641c7a96d6f707d3af6400f
borderite <shinichi.sakata@gmail.com>
2025-07-10 17:32:36 -0700

    Secret Handshake: Revised the tests and .meta/example.c. (#1070)
    * Secret Handshake: Revised the tests and .meta/example.c.
    
    - The previous version of tests let solutions with extra actions pass.
    
    - The tests now require that each solution returns an array of size 4,
    and the unused slots of the array be filled with NULL.
    
    - .meta/example.c has been revised suitably so that it passes all
    revised tests.
    
    * Secret Handshake: Reformat three C files with clang-format.
    
    * Secret Handshake: Removed unnecessary type cast of calloc.
    
    * Secret Handshake: Run format.sh for correcting the format of codes.
    
    * Secret Handshake: Format fix of one more file.
    
    * Update exercises/practice/secret-handshake/test_secret_handshake.c
    
    Co-authored-by: Ryan Hartlage <2488333+ryanplusplus@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Ryan Hartlage <2488333+ryanplusplus@users.noreply.github.com>
    
    [no important files changed]

--- exercises/practice/secret-handshake/.meta/example.c
+++ exercises/practice/secret-handshake/.meta/example.c
@@ -1,60 +1,21 @@
 #include "secret_handshake.h"
+#include <stdint.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include <stdbool.h>
 
-#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
+const char *actions[] = { "wink", "double blink", "close your eyes", "jump" };
 
-typedef struct {
-   const char *cmd;
-   const size_t num;
-} cmd_t;
-
-static const cmd_t cmds[] = {
-   { "wink", 1 }, { "double blink", 2 }, { "close your eyes", 4 }, { "jump", 8 }
-};
-
-static const size_t CMD_COUNT = ARRAY_SIZE(cmds);
-static const size_t CMD_REVERSE = 16;
-
-/* bitcount: count the number of set bits in n */
-static size_t bitcount(size_t num)
+const char **commands(const size_t number)
 {
-   size_t count;
-   for (count = 0; num; num = num & (num - 1), count++)
-      ;
-   return count;
-}
-
-const char **commands(size_t number)
-{
-   if (number > 31)
-      number %= 32;
-
-   bool reverse = false;
-   if ((number & CMD_REVERSE) == CMD_REVERSE) {
-      number -= CMD_REVERSE;
-      reverse = true;
+   const char **action_sequence = calloc(4, sizeof(char *));
+   size_t k = 0;
+   if (number & 16) {
+      for (int i = 3; i >= 0; i--)
+         if (number & 1 << i)
+            action_sequence[k++] = actions[i];
+   } else {
+      for (int i = 0; i <= 3; i++)
+         if (number & 1 << i)
+            action_sequence[k++] = actions[i];
    }
-
-   const size_t len = bitcount(number);
-   const char **cmd_array = malloc(sizeof(char *) * (len ? len : 1));
-   if (cmd_array == NULL) {
-      fprintf(stderr, "Memory error!\n");
-      return NULL;
-   }
-
-   if (len == 0) {
-      cmd_array[0] = NULL;
-      return cmd_array;
-   }
-
-   size_t pos_cmd_array = 0;
-   for (size_t i = 0; i < CMD_COUNT; i++) {
-      size_t k = reverse ? (CMD_COUNT - 1 - i) : i;
-      if ((number & cmds[k].num) == cmds[k].num)
-         cmd_array[pos_cmd_array++] = cmds[k].cmd;
-   }
-
-   return cmd_array;
+   return action_sequence;
 }
--- exercises/practice/secret-handshake/test_secret_handshake.c
+++ exercises/practice/secret-handshake/test_secret_handshake.c
@@ -1,8 +1,11 @@
-#include "test-framework/unity.h"
 #include "secret_handshake.h"
+#include "test-framework/unity.h"
 #include <stdlib.h>
 
-#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
+#define ARRAY_SIZE 4
+
+#undef TEST_IGNORE
+#define TEST_IGNORE()
 
 void setUp(void)
 {
@@ -14,72 +17,72 @@ void tearDown(void)
 
 static void test_commands_wink_for_1(void)
 {
-   const char *expected[] = { "wink" };
+   const char *expected[] = { "wink", NULL, NULL, NULL };
    const char **actual = commands(1);
-   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, ARRAY_SIZE(expected));
+   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, ARRAY_SIZE);
    free(actual);
 }
 
 static void test_commands_double_blink_for_10(void)
 {
    TEST_IGNORE();   // delete this line to run test
-   const char *expected[] = { "double blink" };
+   const char *expected[] = { "double blink", NULL, NULL, NULL };
    const char **actual = commands(2);
-   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, ARRAY_SIZE(expected));
+   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, ARRAY_SIZE);
    free(actual);
 }
 
 static void test_commands_close_your_eyes_for_100(void)
 {
    TEST_IGNORE();
-   const char *expected[] = { "close your eyes" };
+   const char *expected[] = { "close your eyes", NULL, NULL, NULL };
    const char **actual = commands(4);
-   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, ARRAY_SIZE(expected));
+   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, ARRAY_SIZE);
    free(actual);
 }
 
 static void test_commands_jump_for_1000(void)
 {
    TEST_IGNORE();
-   const char *expected[] = { "jump" };
+   const char *expected[] = { "jump", NULL, NULL, NULL };
    const char **actual = commands(8);
-   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, ARRAY_SIZE(expected));
+   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, ARRAY_SIZE);
    free(actual);
 }
 
 static void test_commands_combine_two_actions(void)
 {
    TEST_IGNORE();
-   const char *expected[] = { "wink", "double blink" };
+   const char *expected[] = { "wink", "double blink", NULL, NULL };
    const char **actual = commands(3);
-   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, ARRAY_SIZE(expected));
+   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, ARRAY_SIZE);
    free(actual);
 }
 
 static void test_commands_reverse_two_actions(void)
 {
    TEST_IGNORE();
-   const char *expected[] = { "double blink", "wink" };
+   const char *expected[] = { "double blink", "wink", NULL, NULL };
    const char **actual = commands(19);
-   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, ARRAY_SIZE(expected));
+   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, ARRAY_SIZE);
    free(actual);
 }
 
 static void test_commands_reversing_one_action_gives_the_same_action(void)
 {
    TEST_IGNORE();
-   const char *expected[] = { "jump" };
+   const char *expected[] = { "jump", NULL, NULL, NULL };
    const char **actual = commands(24);
-   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, ARRAY_SIZE(expected));
+   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, ARRAY_SIZE);
    free(actual);
 }
 
 static void test_commands_reversing_no_actions_still_gives_no_actions(void)
 {
    TEST_IGNORE();
-   const char *expected[] = { NULL };
+   const char *expected[] = { NULL, NULL, NULL, NULL };
    const char **actual = commands(16);
-   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, ARRAY_SIZE(expected));
+   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, ARRAY_SIZE);
    free(actual);
 }
 
@@ -89,7 +92,7 @@ static void test_commands_all_possible_actions(void)
    const char *expected[] = { "wink", "double blink", "close your eyes",
                               "jump" };
    const char **actual = commands(15);
-   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, ARRAY_SIZE(expected));
+   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, ARRAY_SIZE);
    free(actual);
 }
 
@@ -99,16 +102,16 @@ static void test_commands_reverse_all_possible_actions(void)
    const char *expected[] = { "jump", "close your eyes", "double blink",
                               "wink" };
    const char **actual = commands(31);
-   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, ARRAY_SIZE(expected));
+   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, ARRAY_SIZE);
    free(actual);
 }
 
 static void test_commands_do_nothing_for_zero(void)
 {
    TEST_IGNORE();
-   const char *expected[] = { NULL };
+   const char *expected[] = { NULL, NULL, NULL, NULL };
    const char **actual = commands(0);
-   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, ARRAY_SIZE(expected));
+   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, ARRAY_SIZE);
    free(actual);
 }
 
