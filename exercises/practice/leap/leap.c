2453f5499a831bc29562a1e432482a46edbf1235
HarshaParamata <159015509+HarshaParamata@users.noreply.github.com>
2025-08-31 04:16:00 +0530

    Update leap.c
    test 3

--- exercises/practice/leap/leap.c
+++ exercises/practice/leap/leap.c
@@ -1,1 +1,15 @@
 #include "leap.h"
+#include <stdbool.h> 
+
+bool leap_year(int year)
+{
+  if(( year % 4 == 0 && year % 100 != 0) || ( year % 400 == 0 ))
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+
+}
