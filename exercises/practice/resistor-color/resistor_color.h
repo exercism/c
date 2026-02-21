6c62c384bbc7b52cada426b5e130f63c50dc0f28
prajwal <prajwalpoojari451@gmail.com>
2026-02-21 15:18:56 +0530

    Add missing function declarations to resistor_color.h

--- exercises/practice/resistor-color/resistor_color.h
+++ exercises/practice/resistor-color/resistor_color.h
@@ -2,7 +2,19 @@
 #define RESISTOR_COLOR_H
 
 typedef enum {
-
+    BLACK = 0,
+    BROWN,
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    BLUE,
+    VIOLET,
+    GREY,
+    WHITE
 } resistor_band_t;
+
+int color_code(resistor_band_t color);
+const char **colors(void);
 
 #endif
